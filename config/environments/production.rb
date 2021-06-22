e28b674cc4cde655b3513a799a1c1b5117ee69fa
Rémy Maucourt <remy.maucourt@yahoo.fr>
2021-06-22 17:57:17 +0200

    update mailer production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'mon-suivi-justice.incubateur.net' }
+  config.action_mailer.default_url_options = { host: ENV['HOST_URL'] }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
