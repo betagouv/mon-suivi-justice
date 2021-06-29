81f50c7fee51beb633b0d914426e6137eb3978d6
Rémy Maucourt <remy.maucourt@yahoo.fr>
2021-06-29 09:52:34 +0200

    allow cpip user to create convict

--- app/policies/convict_policy.rb
+++ app/policies/convict_policy.rb
@@ -12,7 +12,7 @@ class ConvictPolicy < ApplicationPolicy
   end
 
   def create?
-    user.admin? or user.bex?
+    true
   end
 
   def destroy?
--- spec/policies/convict_policy_spec.rb
+++ spec/policies/convict_policy_spec.rb
@@ -34,8 +34,8 @@ describe ConvictPolicy do
 
     it { is_expected.to permit_action(:show) }
     it { is_expected.to permit_action(:index) }
-    it { is_expected.to forbid_action(:new) }
-    it { is_expected.to forbid_action(:create) }
+    it { is_expected.to permit_action(:new) }
+    it { is_expected.to permit_action(:create) }
     it { is_expected.to forbid_action(:edit) }
     it { is_expected.to forbid_action(:update) }
     it { is_expected.to forbid_action(:destroy) }
