0ea9e226c5e7c65de362da1972c62b214e2f8348
Rémy Maucourt <remy.maucourt@yahoo.fr>
2021-06-30 11:05:52 +0200

    validates place phone number

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,6 @@
 class Place < ApplicationRecord
   validates :name, :adress, presence: true
+
+  validates :phone, format: { with: /[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}/,
+                              message: I18n.t('activerecord.errors.phone_format') }, allow_blank: true
 end
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 RSpec.describe Place, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:adress) }
+
+  it { should allow_value('0687549865').for(:phone) }
+  # it { should allow_value('06 87 54 98 65').for(:phone) }
+  it { should_not allow_value('06845').for(:phone) }
 end
