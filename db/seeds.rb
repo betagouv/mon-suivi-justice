34d0289aaa9ee20e8d1957af80b9e5df575667e8
Rémy Maucourt <remy.maucourt@yahoo.fr>
2021-08-11 11:16:48 +0200

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', role: :admin, first_name: 'Kevin', last_name: 'Mc Alistair')
+
+place = Place.create!(name: "Tribunal d'Ancenis", adress: "133 Av. Robert Schuman, 44150 Ancenis", place_type: :sap, phone: '0606060606')
+agenda = Agenda.create!(place: place, name: "Agenda tribunal Ancenis")
+
+appointment_type = AppointmentType.create!(name: 'RDV de test SAP', place_type: :spip)
+
+NotificationType.create!(appointment_type: appointment_type, role: :summon, template: "Vous êtes convoqué, merci de venir.")
+NotificationType.create!(appointment_type: appointment_type, role: :reminder, template: "RAPPEL Vous êtes convoqué, vraiment il faut venir.", reminder_period: :two_days)
+
+SlotType.create(appointment_type: appointment_type, week_day: :monday, starting_time: Time.new(2021, 6, 21, 10, 00, 0), duration: 60, capacity: 3)
+SlotType.create(appointment_type: appointment_type, week_day: :monday, starting_time: Time.new(2021, 6, 21, 11, 00, 0), duration: 60, capacity: 3)
+SlotType.create(appointment_type: appointment_type, week_day: :tuesday, starting_time: Time.new(2021, 6, 21, 10, 00, 0), duration: 60, capacity: 3)
+SlotType.create(appointment_type: appointment_type, week_day: :tuesday, starting_time: Time.new(2021, 6, 21, 11, 00, 0), duration: 60, capacity: 3)
+SlotType.create(appointment_type: appointment_type, week_day: :wednesday, starting_time: Time.new(2021, 6, 21, 10, 00, 0), duration: 60, capacity: 3)
+SlotType.create(appointment_type: appointment_type, week_day: :wednesday, starting_time: Time.new(2021, 6, 21, 11, 00, 0), duration: 60, capacity: 3)
+
+all_dates = []
+Date.today.upto(6.months.from_now.to_date) { |date| all_dates << date }
+
+appointment_type.slot_types.each do |slot_type|
+  dates = all_dates.select {|date| date.strftime("%A").downcase == slot_type.week_day }
+
+  dates.each do |date|
+    Slot.create!(
+      date: date,
+      agenda: agenda,
+      appointment_type: appointment_type,
+      starting_time: slot_type.starting_time,
+      duration: slot_type.duration,
+      capacity: slot_type.capacity,
+      available: true
+    )
+  end
+end
