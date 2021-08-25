249b06f63b71e5af48575e477f757bf742e01625
jeremira <jmiraille@gmail.com>
2021-08-25 16:06:43 +0200

    common concern spec

---
+++ spec/models/concerns/normalized_phone_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+shared_examples_for 'normalized_phone' do
+  let(:model) { described_class }
+  let(:model_factory) { model.to_s.underscore.to_sym }
+
+  describe 'Phone validations' do
+    it 'denies a non-digit number' do
+      expect(build(model_factory, phone: 'my phone number')).not_to be_valid
+    end
+    it 'denies a non-sense number' do
+      expect(build(model_factory, phone: 'zero six 56ù% abc')).not_to be_valid
+    end
+    it 'denies a invalid number' do
+      expect(build(model_factory, phone: '958748751245876321448')).not_to be_valid
+    end
+    it 'denies a too short number' do
+      expect(build(model_factory, phone: '1234')).not_to be_valid
+    end
+    it 'accepts a valid number with country prefix' do
+      expect(build(model_factory, phone: '+33561083731')).to be_valid
+    end
+    it 'accepts a valid number without country prefix' do
+      expect(build(model_factory, phone: '0561083731')).to be_valid
+    end
+    it 'accepts a valid number with space' do
+      expect(build(model_factory, phone: '05 61 08 37 31')).to be_valid
+    end
+    it 'accepts a valid number with dot' do
+      expect(build(model_factory, phone: '05.61.08.37.31')).to be_valid
+    end
+    it 'accepts a valid number with dash' do
+      expect(build(model_factory, phone: '05-61-08-37-31')).to be_valid
+    end
+  end
+
+  describe 'Phone normalization' do
+    it 'normalizes a valid number with country prefix' do
+      convict = create(model_factory, phone: '+33561083731')
+      expect(convict.reload.phone).to eq '+33561083731'
+    end
+    it 'normalizes a valid number without country prefix' do
+      convict = create(model_factory, phone: '0561083731')
+      expect(convict.reload.phone).to eq '+33561083731'
+    end
+    it 'normalizes a valid number with space' do
+      convict = create(model_factory, phone: '05 61 08 37 31')
+      expect(convict.reload.phone).to eq '+33561083731'
+    end
+    it 'normalizes a valid number with dot' do
+      convict = create(model_factory, phone: '05.61.08.37.31')
+      expect(convict.reload.phone).to eq '+33561083731'
+    end
+    it 'normalizes a valid number with dash' do
+      convict = create(model_factory, phone: '05-61-08-37-31')
+      expect(convict.reload.phone).to eq '+33561083731'
+    end
+  end
+
+  describe '#display_phone' do
+    it 'returns correct phone value with a french number' do
+      expect(create(model_factory, phone: '+33561083731').display_phone).to eq '05 61 08 37 31'
+    end
+    it 'returns correct phone value with a UK number' do
+      expect(create(model_factory, phone: '+443031237300').display_phone).to eq '0303 123 7300'
+    end
+  end
+end
--- spec/models/convict_spec.rb
+++ spec/models/convict_spec.rb
@@ -6,55 +6,9 @@ RSpec.describe Convict, type: :model do
   it { should validate_presence_of(:last_name) }
   it { should validate_presence_of(:title) }
   it { should define_enum_for(:title).with_values(%i[male female]) }
+  it_behaves_like 'normalized_phone'
 
-  describe 'Phone validations' do
-    it 'denies a non-digit number' do
-      expect(build(:convict, phone: 'my phone number')).not_to be_valid
-    end
-    it 'denies a non-sense number' do
-      expect(build(:convict, phone: 'zero six 56ù% abc')).not_to be_valid
-    end
-    it 'denies a invalid number' do
-      expect(build(:convict, phone: '958748751245876321448')).not_to be_valid
-    end
-    it 'denies a too short number' do
-      expect(build(:convict, phone: '1234')).not_to be_valid
-    end
-    it 'accepts a valid number with country prefix' do
-      expect(build(:convict, phone: '+33561083731')).to be_valid
-    end
-    it 'accepts a valid number without country prefix' do
-      expect(build(:convict, phone: '0561083731')).to be_valid
-    end
-    it 'accepts a valid number with space' do
-      expect(build(:convict, phone: '05 61 08 37 31')).to be_valid
-    end
-    it 'accepts a valid number with dot' do
-      expect(build(:convict, phone: '05.61.08.37.31')).to be_valid
-    end
-    it 'accepts a valid number with dash' do
-      expect(build(:convict, phone: '05-61-08-37-31')).to be_valid
-    end
-    it 'normalizes a valid number with country prefix' do
-      convict = create(:convict, phone: '+33561083731')
-      expect(convict.reload.phone).to eq '+33561083731'
-    end
-    it 'normalizes a valid number without country prefix' do
-      convict = create(:convict, phone: '0561083731')
-      expect(convict.reload.phone).to eq '+33561083731'
-    end
-    it 'normalizes a valid number with space' do
-      convict = create(:convict, phone: '05 61 08 37 31')
-      expect(convict.reload.phone).to eq '+33561083731'
-    end
-    it 'normalizes a valid number with dot' do
-      convict = create(:convict, phone: '05.61.08.37.31')
-      expect(convict.reload.phone).to eq '+33561083731'
-    end
-    it 'normalizes a valid number with dash' do
-      convict = create(:convict, phone: '05-61-08-37-31')
-      expect(convict.reload.phone).to eq '+33561083731'
-    end
+  describe 'Validations' do
     it 'requires a phone' do
       expect(build(:convict, phone: nil)).not_to be_valid
     end
@@ -66,16 +20,6 @@ RSpec.describe Convict, type: :model do
     end
     it 'accepts a blank phone with refused_phone option' do
       expect(build(:convict, phone: nil, refused_phone: true)).to be_valid
-    end
-  end
-
-  describe '#display_phone' do
-
-    it 'returns correct phone value with a french number' do
-      expect(create(:convict, phone: '+33561083731').display_phone).to eq '05 61 08 37 31'
-    end
-    it 'returns correct phone value with a UK number' do
-      expect(create(:convict, phone: '+443031237300').display_phone).to eq '0303 123 7300'
     end
   end
 end
