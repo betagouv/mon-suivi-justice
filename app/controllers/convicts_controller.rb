fa27a18af80fc0965ad1c873d6f4d39e1aed8dfa
jeremira <jmiraille@gmail.com>
2021-09-03 16:15:56 +0200

    cancel the cancelation

--- app/controllers/convicts_controller.rb
+++ app/controllers/convicts_controller.rb
@@ -60,7 +60,8 @@ class ConvictsController < ApplicationController
   private
 
   def convict_params
-    params.require(:convict).permit :first_name, :last_name, :title, :phone, :no_phone, :refused_phone, :place_id
+    params.require(:convict).permit(:first_name, :last_name, :title, :phone, :no_phone,
+                                    :refused_phone, :place_id, :prosecutor_number)
   end
 
   def select_path(params)
