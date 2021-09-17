b7975ff4b14b03e34d8a9427c36e654e3eccddf0
jeremira <jmiraille@gmail.com>
2021-09-17 09:19:04 +0200

    slot factory with slot type loop

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,6 +1,7 @@
 class Slot < ApplicationRecord
   has_paper_trail
 
+  belongs_to :agenda
   belongs_to :slot_type, optional: true
   belongs_to :appointment_type
   has_one :appointment, dependent: :destroy
--- app/models/slot_type.rb
+++ app/models/slot_type.rb
@@ -10,7 +10,7 @@ class SlotType < ApplicationRecord
   # When a SlotType is destroyed, we remove corresponding slots, as they are no more available
   # Slot already booked are preserved.
   #
-  before_destroy :destroy_slots_not_booked # TODO spec
+  before_destroy :destroy_slots_not_booked # TODO: spec
 
   validates :week_day, :duration, :starting_time, :capacity, presence: true
   enum week_day: %i[monday tuesday wednesday thursday friday]
