dca07cf31b350fc18739c51cdee539e15bc0c5d1
Rémy Maucourt <remy.maucourt@yahoo.fr>
2021-09-23 11:06:21 +0200

    display SMS content for no_show notification

--- app/jobs/get_sms_status_job.rb
+++ app/jobs/get_sms_status_job.rb
@@ -4,9 +4,10 @@ class GetSmsStatusJob < ApplicationJob
   queue_as :default
 
   def perform(notification_id)
+    notification = Notification.find(notification_id)
+
     return if %w[received failed].include?(notification.state)
 
-    notification = Notification.find(notification_id)
     SendinblueAdapter.new.get_sms_status(notification)
     GetSmsStatusJob.set(wait: 10.minutes).perform_later(notification.id)
   end
