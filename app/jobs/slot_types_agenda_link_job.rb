4f9ab792db2128bc6ffca3b0601b97ed27516b9a
jeremira <jmiraille@gmail.com>
2021-09-24 16:24:52 +0200

    remove useless job

--- app/jobs/slot_types_agenda_link_job.rb
+++
@@ -1,7 +0,0 @@
-class SlotTypesAgendaLinkJob < ApplicationJob
-  def perform
-    # Migration handled SlotType duplication for each agenda. All slots were destroyed in the process.
-    # Re-launch Slot creation factory to re-seed new slot :
-    SlotCreationJob.perform_later oneshot: true
-  end
-end
