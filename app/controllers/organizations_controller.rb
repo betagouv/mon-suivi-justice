ba5f3aa5559dd32f50cd944b208f19decf68e58c
jeremira <jmiraille@gmail.com>
2021-09-30 14:29:08 +0200

    sidebar and policy for orga controller

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,48 @@
+class OrganizationsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @organizations = Organization.all
+    authorize @organizations
+  end
+
+  def edit
+    @organization = Organization.find params[:id]
+    authorize @organization
+  end
+
+  def new
+    @organization = Organization.find params[:id]
+    authorize @organization
+  end
+
+  def update
+    organization = Organization.find params[:id]
+    authorize organization
+
+    organization.update organization_params
+    redirect_to organizations_path
+  end
+
+  def destroy
+    organization = Organization.find params[:id]
+    authorize organization
+
+    organization.destroy
+    redirect_to organizations_path
+  end
+
+  def create
+    organization = Organization.new organization_params
+    authorize organization
+
+    organization.save
+    redirect_to organizations_path
+  end
+
+  private
+
+  def organization_params
+    params.require(:organization).permit(:name)
+  end
+end
