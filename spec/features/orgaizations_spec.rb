4443f867b3cb7ea0f65ffe69a90e7833113cd022
jeremira <jmiraille@gmail.com>
2021-10-08 12:00:06 +0200

    spec for organization departement edit

--- spec/features/orgaizations_spec.rb
+++ spec/features/orgaizations_spec.rb
@@ -30,4 +30,19 @@ RSpec.feature 'Organizations', type: :feature do
     click_button 'Enregistrer'
     expect(page).to have_content('SPIP 75')
   end
+
+  scenario 'An admin attaches department to an organizatio' do
+    create :department, number: '09', name: 'Ariège'
+    create :organization, name: 'SPIP 92'
+    create_admin_user_and_login
+    visit organizations_path
+    within first('.organizations-item-container') do
+      click_link 'Modifier'
+    end
+    select 'Ariège', from: :areas_organizations_mapping_area_id
+    expect {click_button 'Ajouter'}.to change(AreasOrganizationsMapping, :count).from(0).to(1)
+    expect(page).to have_content("(09) Ariège")
+    expect {click_link 'Supprimer'}.to change(AreasOrganizationsMapping, :count).from(1).to(0)
+    expect(page).not_to have_content("(09) Ariège")
+  end
 end
