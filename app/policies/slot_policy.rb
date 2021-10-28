81345439a773f931423c61c8f03c2759ac8b6085
Rémy Maucourt <remy.maucourt@yahoo.fr>
2021-10-28 17:40:58 +0200

    add habilitation rules for slots

--- app/policies/slot_policy.rb
+++ app/policies/slot_policy.rb
@@ -1,22 +1,22 @@
 class SlotPolicy < ApplicationPolicy
   def index?
-    user.admin?
+    user.admin? || user.local_admin?
   end
 
   def update?
-    user.admin?
+    user.admin? || user.local_admin?
   end
 
   def show?
-    user.admin?
+    user.admin? || user.local_admin?
   end
 
   def create?
-    user.admin?
+    user.admin? || user.local_admin?
   end
 
   def destroy?
-    user.admin?
+    user.admin? || user.local_admin?
   end
 
   def select?
