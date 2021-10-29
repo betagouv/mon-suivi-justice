edab83d5e92e052f59d2a2fa018cc511f88c198a
Rémy Maucourt <remy.maucourt@yahoo.fr>
2021-10-29 10:33:38 +0200

    order roles in form

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,23 @@
 module UsersHelper
+  def ordered_user_roles
+    [
+      'admin',
+      'local_admin',
+      'jap',
+      'sap',
+      'bex',
+      'prosecutor',
+      'secretary_court',
+      'dir_greff_bex',
+      'greff_co',
+      'dir_greff_sap',
+      'greff_sap',
+      'dpip',
+      'cpip',
+      'secretary_spip',
+      'educator',
+      'psychologist',
+      'overseer'
+    ]
+  end
 end
