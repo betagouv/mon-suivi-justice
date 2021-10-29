9b63cf45afc08f69a50ea9b7609c1696a13b4a24
jeremira <jmiraille@gmail.com>
2021-10-29 15:10:37 +0200

    typo in notificaitons template

--- app/jobs/seed_reschedule_notification_type_job.rb
+++ app/jobs/seed_reschedule_notification_type_job.rb
@@ -21,7 +21,7 @@ class SeedRescheduleNotificationTypeJob < ApplicationJob
       'Votre débat contradictoire du {rdv.date} à {rdv.heure} devant le juge d’application des peines a été déplacé. '\
       'Vous êtes désormais convoqué.e le {rdv.date} à {rdv.heure} au {lieu.nom} au {lieu.adresse}. '\
       "Pensez à apporter votre pièce d'identité. En cas de besoin, vous pouvez contacter le {lieu.téléphone}."
-    when '1er rdv SPIP', 'Rdv de Suivi SPIP', ' Rdv Bex SPIP', 'Convocation 741)1'
+    when '1er rdv SPIP', 'Rdv de Suivi SPIP', ' Rdv Bex SPIP', 'Convocation 741-1'
       'Votre rendez-vous au {lieu.nom} le {rdv.date} à {rdv.heure} a été modifié. '\
       'Vous êtes désormais convoqué au {lieu.nom} le {rdv.date} à {rdv.heure}. '\
       "Merci de venir avec une pièce d'identité au {lieu.adresse}. "\
