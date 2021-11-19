0795855611ed12282002412e287678be9b16f536
Rémy Maucourt <remy.maucourt@yahoo.fr>
2021-11-19 15:24:32 +0100

    fix bug habilitations : local_admin can edit slot_types

--- app/policies/slot_type_policy.rb
+++ app/policies/slot_type_policy.rb
@@ -1,25 +1,29 @@
 class SlotTypePolicy < ApplicationPolicy
   def index?
-    user.admin?
+    user.admin? || user.local_admin?
+  end
+
+  def show?
+    user.admin? || user.local_admin?
   end
 
   def new?
-    user.admin?
+    user.admin? || user.local_admin?
   end
 
   def create?
-    user.admin?
+    user.admin? || user.local_admin?
   end
 
   def edit?
-    user.admin?
+    user.admin? || user.local_admin?
   end
 
   def update?
-    user.admin?
+    user.admin? || user.local_admin?
   end
 
   def destroy?
-    user.admin?
+    user.admin? || user.local_admin?
   end
 end
