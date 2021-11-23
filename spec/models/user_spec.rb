9d85d28b8611cc31817d9e9d165ef42161b3defc
Rémy Maucourt <remy.maucourt@yahoo.fr>
2021-11-23 17:49:24 +0100

    add scope in_department to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,4 +31,26 @@ RSpec.describe User, type: :model do
       }
     )
   }
+
+  describe '.in_department' do
+    it 'returns places scoped by department' do
+      department1 = create :department, number: '01', name: 'Ain'
+
+      organization1 = create :organization
+      create :areas_organizations_mapping, organization: organization1, area: department1
+      user1 = create :user, organization: organization1
+
+      organization2 = create :organization
+      create :areas_organizations_mapping, organization: organization2, area: department1
+      user2 = create :user, organization: organization2
+
+      department2 = create :department, number: '02', name: 'Aisne'
+
+      organization3 = create :organization
+      create :areas_organizations_mapping, organization: organization3, area: department2
+      create :user, organization: organization3
+
+      expect(User.in_department(department1)).to eq [user1, user2]
+    end
+  end
 end
