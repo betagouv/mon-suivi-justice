b7b59123d77b46390c0e57e2a3716dfd5f7972e9
Rémy Maucourt <remy.maucourt@yahoo.fr>
2021-11-24 17:34:21 +0100

    add policy rule for dpip and slots

--- app/policies/slot_policy.rb
+++ app/policies/slot_policy.rb
@@ -3,30 +3,30 @@ class SlotPolicy < ApplicationPolicy
     def resolve
       if user.admin?
         scope.all
-      elsif user.local_admin? || user.bex? || user.sap?
+      elsif user.local_admin? || user.bex? || user.sap? || user.dpip?
         scope.in_department(user.organization.departments.first)
       end
     end
   end
 
   def index?
-    user.admin? || user.local_admin?
+    user.admin? || user.local_admin? || user.dpip?
   end
 
   def update?
-    user.admin? || user.local_admin?
+    user.admin? || user.local_admin? || user.dpip?
   end
 
   def show?
-    user.admin? || user.local_admin?
+    user.admin? || user.local_admin? || user.dpip?
   end
 
   def create?
-    user.admin? || user.local_admin?
+    user.admin? || user.local_admin? || user.dpip?
   end
 
   def destroy?
-    user.admin? || user.local_admin?
+    user.admin? || user.local_admin? || user.dpip?
   end
 
   def select?
