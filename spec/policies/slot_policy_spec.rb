66d71cca18ef21d29331bc44a6b823cfd954fcbe
Rémy Maucourt <remy.maucourt@yahoo.fr>
2021-11-24 16:18:44 +0100

    change dpip specs

--- spec/policies/slot_policy_spec.rb
+++ spec/policies/slot_policy_spec.rb
@@ -188,13 +188,13 @@ describe SlotPolicy do
   context 'for a dpip user' do
     let(:user) { build(:user, role: 'dpip') }
 
-    it { is_expected.to forbid_action(:show) }
-    it { is_expected.to forbid_action(:index) }
-    it { is_expected.to forbid_action(:new) }
-    it { is_expected.to forbid_action(:create) }
-    it { is_expected.to forbid_action(:edit) }
-    it { is_expected.to forbid_action(:update) }
-    it { is_expected.to forbid_action(:destroy) }
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.to permit_action(:index) }
+    it { is_expected.to permit_action(:new) }
+    it { is_expected.to permit_action(:create) }
+    it { is_expected.to permit_action(:edit) }
+    it { is_expected.to permit_action(:update) }
+    it { is_expected.to permit_action(:destroy) }
   end
 
   context 'for a secretary_spip user' do
