0657a8ada1b0cf269ae95a2cf20dd5e4a0a42245
Rémy Maucourt <remy.maucourt@yahoo.fr>
2021-11-24 17:29:20 +0100

    add scoping rule on slots

--- app/policies/slot_policy.rb
+++ app/policies/slot_policy.rb
@@ -1,4 +1,14 @@
 class SlotPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if user.admin?
+        scope.all
+      elsif user.local_admin? || user.bex? || user.sap?
+        scope.in_department(user.organization.departments.first)
+      end
+    end
+  end
+
   def index?
     user.admin? || user.local_admin?
   end
