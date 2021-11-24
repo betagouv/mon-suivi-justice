971a2b201af84f3676918535ff6f1b6c88307a32
Rémy Maucourt <remy.maucourt@yahoo.fr>
2021-11-24 19:57:13 +0100

    fix bug with available appointment_types in appointment#new

--- app/policies/appointment_policy.rb
+++ app/policies/appointment_policy.rb
@@ -70,9 +70,9 @@ class AppointmentPolicy < ApplicationPolicy
   def appointment_workflow
     apt_type = AppointmentType.find(record.slot&.appointment_type_id)
 
-    if user.work_at_sap? then apt_type.used_at_sap?
-    elsif user.work_at_bex? then apt_type.used_at_bex?
-    elsif user.work_at_spip? then apt_type.used_at_spip?
+    if user.work_at_sap? then apt_type.used_at_sap?.include? apt_type.name
+    elsif user.work_at_bex? then apt_type.used_at_bex?.include? apt_type.name
+    elsif user.work_at_spip? then apt_type.used_at_spip?.include? apt_type.name
     else
       true
     end
