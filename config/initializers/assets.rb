43a6222ff15373ef343da0f90642bb97955e7793
Rémy Maucourt <remy.maucourt@yahoo.fr>
2021-11-26 13:10:41 +0100

    fix deploy bug 2 ?

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,6 @@
+Rails.application.config.assets.configure do |env|
+  env.export_concurrent = false
+end
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
@@ -12,7 +15,3 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w( application_static.js application_static.css )
-
-Rails.application.config.assets.configure do |env|
-  env.export_concurrent = false
-end
