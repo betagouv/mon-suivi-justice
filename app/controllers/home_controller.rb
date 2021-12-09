0f96ed3f07d97372b1d393624853c8a4b9d58baa
Rémy Maucourt <remy.maucourt@yahoo.fr>
2021-12-09 20:19:08 +0100

    add basics stats on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,18 @@ class HomeController < ApplicationController
 
   def home
     @convicts = policy_scope(Convict.all)
+    @users = User.count
+    @notifications = Notification.where(state: 'sent').count
+
+    @recorded = Appointment.count
+    @fulfiled = Appointment.where(state: 'fulfiled').count
+    @no_show = Appointment.where(state: 'no_show').count
+    @booked = Appointment.where(state: 'booked').count
+    @excused = Appointment.where(state: 'excused').count
+    @canceled = Appointment.where(state: 'canceled').count
+
+    @future_booked = Appointment.where(state: 'booked').joins(:slot).where('slots.date > ?', Date.today).count
+    @passed_booked = Appointment.where(state: 'booked').joins(:slot).where('slots.date < ?', Date.today).count
+    @passed_no_canceled = Appointment.joins(:slot).where('slots.date < ?', Date.today).where.not(state: 'canceled').count
   end
 end
