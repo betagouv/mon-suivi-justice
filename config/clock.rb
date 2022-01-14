67b1263c35c5e00889d2c95cb1d327004df2a475
Rémy Maucourt <remy.maucourt@yahoo.fr>
2022-01-14 18:07:53 +0100

    rename job that destroy archived convicts

--- config/clock.rb
+++ config/clock.rb
@@ -7,5 +7,5 @@ module Clockwork
   end
 
   every(1.day, 'slot_creation.job', at: '00:00') { SlotCreationJob.perform_later }
-  every(1.day, 'archived_convict_deletion.job', at: '01:00') { ArchivedConvictsDestroy.perform_later }
+  every(1.day, 'archived_convict_deletion.job', at: '01:00') { ArchivedConvictsDestroyJob.perform_later }
 end
