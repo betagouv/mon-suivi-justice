4b82db77cd0cdabea0aebb28bd5855e46814295c
Rémy Maucourt <remy.maucourt@yahoo.fr>
2022-01-14 12:34:51 +0100

    refactore HistoryItem enum

--- spec/models/history_item_spec.rb
+++ spec/models/history_item_spec.rb
@@ -7,24 +7,25 @@ RSpec.describe HistoryItem, type: :model do
   it { should validate_presence_of(:content) }
   it { should define_enum_for(:category).with_values(%i[appointment notification convict]) }
 
-  it do
+  it {
     should define_enum_for(:event).with_values(
-      %i[
-        book_appointment
-        cancel_appointment
-        fulfil_appointment
-        miss_appointment
-        excuse_appointment
-        reschedule_appointment
-        send_now_summon_notification
-        send_then_reminder_notification
-        cancel_reminder_notification
-        send_now_cancelation_notification
-        send_now_no_show_notification
-        send_now_reschedule_notification
-        archive_convict
-        unarchive_convict
-      ]
+      {
+        book_appointment: 0,
+        cancel_appointment: 1,
+        fulfil_appointment: 2,
+        miss_appointment: 4,
+        excuse_appointment: 5,
+        reschedule_appointment: 6,
+        send_now_summon_notification: 7,
+        send_then_reminder_notification: 8,
+        cancel_reminder_notification: 9,
+        dir_greff_sap: 10,
+        send_now_cancelation_notification: 11,
+        send_now_no_show_notification: 12,
+        send_now_reschedule_notification: 13,
+        archive_convict: 14,
+        unarchive_convict: 15
+      }
     )
-  end
+  }
 end
