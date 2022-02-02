e5bac42d61a7cfcbfcaaf3b26b3b53c9d3021189
Damien Le Thiec <damien.lethiec@gmail.com>
2022-02-02 15:55:44 +0100

    Fix emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
+  config.action_mailer.asset_host = ENV['HOST_URL']
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
