88ac19a032aad2ac9d67b22fa95e561fb5fe837d
Damien Le Thiec <damien.lethiec@gmail.com>
2022-02-11 18:30:26 +0100

    Validate format template

--- app/models/notification_type.rb
+++ app/models/notification_type.rb
@@ -3,7 +3,19 @@ class NotificationType < ApplicationRecord
 
   belongs_to :appointment_type
   validates :template, presence: true
+  validate :template_format
 
   enum role: %i[summon reminder cancelation no_show reschedule]
   enum reminder_period: %i[one_day two_days]
+
+  VALID_NOTIFICATION_KEYS = %w[{rdv.heure} {rdv.date} {lieu.nom} {lieu.adresse}
+                               {lieu.téléphone} {lieu.contact} {lieu.lien_info}].freeze
+
+  private
+
+  def template_format
+    return unless (template.scan(/{.*?}/) - VALID_NOTIFICATION_KEYS).present?
+
+    errors.add(:template, I18n.t('activerecord.errors.models.notification_type.attributes.template.invalid_format'))
+  end
 end
