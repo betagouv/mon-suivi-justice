81f0fd7091da0d25967e4fe5d42e426ca04ba15e
Damien Le Thiec <damien.lethiec@gmail.com>
2022-02-15 14:58:17 +0100

    Write tests user phone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,6 +45,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:invite, keys: %i[first_name last_name role email organization_id])
+    devise_parameter_sanitizer.permit(:invite, keys: %i[first_name last_name role email organization_id phone])
   end
 end
