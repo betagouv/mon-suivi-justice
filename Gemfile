556eb5577bc836fc79030305cb38780c0f677555
Damien Le Thiec <damien.lethiec@gmail.com>
2022-02-17 18:46:32 +0100

    First draft basic auth

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.5'
-gem 'rails', '~> 6.1.3', '>= 6.1.3.2'
+gem 'rails', '~> 6.1.4', '>= 6.1.4.6'
 
 gem 'pg'
 gem 'puma', '~> 5.6'
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,5 @@
 class ApiController < ActionController::API
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTH_USER"],
+    password: ENV["HTTP_BASIC_AUTH_PSWD"]
 end
