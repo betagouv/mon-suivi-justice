d148a2242bb148cedfe0fb812f791fa598dc8665
Damien Le Thiec <damien.lethiec@gmail.com>
2022-02-22 19:25:55 +0100

    Convert user api to convict api

--- app/controllers/api/v1/users_controller.rb
+++
@@ -1,9 +0,0 @@
-module Api
-  module V1
-    class UsersController < ApiController
-      def show
-        @user = User.find(params[:id])
-      end
-    end
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -74,7 +74,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: "json"} do
     namespace :v1 do
-      resources :users, only: :show
+      resources :convicts, only: :show
     end
   end
 
