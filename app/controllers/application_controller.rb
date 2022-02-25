f1ab64198ed98afb3f3dfbc4c9a5867c96a75c06
Rémy Maucourt <remy.maucourt@yahoo.fr>
2022-02-25 12:20:19 +0100

    fix bug with redirect_back on appointment controller 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,13 +26,8 @@ class ApplicationController < ActionController::Base
     ENV['PUBLIC_SITE_ROOT']
   end
 
-  # allows to add anchor to redirect_back. Source : https://www.filippoliverani.com/pass-params-rails-redirect-back
-  def redirect_to_back(options = {})
-    uri = URI(request.referer)
-    uri.query = options.delete(:params)&.to_query
-    uri.fragment = options.delete(:anchor)
-
-    redirect_to(uri.to_s, options)
+  def back_with_anchor(anchor: '')
+    "#{request.referrer}##{anchor}"
   end
 
   def current_organization
