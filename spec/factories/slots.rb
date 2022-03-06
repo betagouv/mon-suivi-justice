dd3912f7402db2ad238e3bad970f2d76f9fd4bf2
Damien Le Thiec <damien.lethiec@gmail.com>
2022-03-06 17:50:55 +0100

    Finally fix build by skipping some validations (because cannot create slots on weekends)

--- spec/factories/slots.rb
+++ spec/factories/slots.rb
@@ -9,5 +9,9 @@ FactoryBot.define do
     slot_type
     appointment_type
     agenda
+
+    trait :without_validations do
+      to_create { |instance| instance.save(validate: false) }
+    end
   end
 end
