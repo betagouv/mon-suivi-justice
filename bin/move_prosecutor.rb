b6fe5631234350663e05e9630e7ad0a35559bf90
Rémy Maucourt <remy.maucourt@yahoo.fr>
2022-03-29 16:33:03 +0200

    add script to move data

---
+++ bin/move_prosecutor.rb
@@ -0,0 +1,16 @@
+# rails r bin/move_prosecutor.rb
+
+require 'ruby-progressbar'
+
+progress = ProgressBar.create(total: Convict.count)
+
+Convict.find_each do |convict|
+  number = convict.prosecutor_number
+  convict.appointments.each do |appointment|
+    appointment.prosecutor_number = number
+    appointment.save!
+  end
+  progress.increment
+end
+
+p "Data moved"
