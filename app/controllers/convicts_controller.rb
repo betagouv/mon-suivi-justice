9a1a49027eb6c96cd518b4f7521864cfc86d6835
Damien Le Thiec <damien.lethiec@gmail.com>
2022-04-12 21:34:02 +0200

    Fix rubocop

--- app/controllers/convicts_controller.rb
+++ app/controllers/convicts_controller.rb
@@ -87,6 +87,7 @@ class ConvictsController < ApplicationController
 
   private
 
+  # rubocop:disable Metrics/AbcSize
   def save_and_redirect(convict)
     @duplicate_presence = duplicate_convict_presence
     force_duplication = ActiveRecord::Type::Boolean.new.deserialize(params.dig(:convict, :force_duplication))
@@ -101,6 +102,7 @@ class ConvictsController < ApplicationController
       render :new
     end
   end
+  # rubocop:enable Metrics/AbcSize
 
   def duplicate_convict_presence
     Convict.exists?(
