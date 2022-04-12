8114a4a2dadf412d5354c6958615b422138337cf
Damien Le Thiec <damien.lethiec@gmail.com>
2022-04-13 00:30:01 +0200

    Fix rubocop

--- spec/factories/convict.rb
+++ spec/factories/convict.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :convict do
     first_name { 'Jane' }
     last_name { 'Doe' }
-    sequence(:phone, 2) { |n| "060606060#{n.to_s}" }
+    sequence(:phone, 2) { |n| "060606060#{n}" }
   end
 end
