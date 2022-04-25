398f7b3264d16dac9613e005763b8c69575d3673
Damien Le Thiec <damien.lethiec@gmail.com>
2022-04-25 16:10:09 +0200

    Rubocop

--- spec/policies/convict_invitation_policy_spec.rb
+++ spec/policies/convict_invitation_policy_spec.rb
@@ -7,14 +7,12 @@ describe ConvictPolicy do
     let(:user) { build(:user, role: 'admin') }
     let(:convict) { build(:convict) }
 
-
     it { is_expected.to permit_action(:create) }
   end
 
   context 'for a local_admin' do
     let(:user) { build(:user, role: 'local_admin') }
     let(:convict) { build(:convict) }
-
 
     it { is_expected.not_to permit_action(:create) }
   end
@@ -23,14 +21,12 @@ describe ConvictPolicy do
     let(:user) { build(:user, role: 'prosecutor') }
     let(:convict) { build(:convict) }
 
-
     it { is_expected.not_to permit_action(:create) }
   end
 
   context 'for a jap user' do
     let(:user) { build(:user, role: 'jap') }
     let(:convict) { build(:convict) }
-
 
     it { is_expected.not_to permit_action(:create) }
   end
@@ -39,14 +35,12 @@ describe ConvictPolicy do
     let(:user) { build(:user, role: 'secretary_court') }
     let(:convict) { build(:convict) }
 
-
     it { is_expected.not_to permit_action(:create) }
   end
 
   context 'for a dir_greff_bex user' do
     let(:user) { build(:user, role: 'dir_greff_bex') }
     let(:convict) { build(:convict) }
-
 
     it { is_expected.not_to permit_action(:create) }
   end
@@ -55,14 +49,12 @@ describe ConvictPolicy do
     let(:user) { build(:user, role: 'bex') }
     let(:convict) { build(:convict) }
 
-
     it { is_expected.not_to permit_action(:create) }
   end
 
   context 'for a greff_co user' do
     let(:user) { build(:user, role: 'greff_co') }
     let(:convict) { build(:convict) }
-
 
     it { is_expected.not_to permit_action(:create) }
   end
@@ -71,14 +63,12 @@ describe ConvictPolicy do
     let(:user) { build(:user, role: 'dir_greff_sap') }
     let(:convict) { build(:convict) }
 
-
     it { is_expected.not_to permit_action(:create) }
   end
 
   context 'for a greff_sap user' do
     let(:user) { build(:user, role: 'greff_sap') }
     let(:convict) { build(:convict) }
-
 
     it { is_expected.not_to permit_action(:create) }
   end
@@ -87,14 +77,12 @@ describe ConvictPolicy do
     let(:user) { build(:user, role: 'cpip') }
     let(:convict) { build(:convict) }
 
-
     it { is_expected.not_to permit_action(:create) }
   end
 
   context 'for a cpip user linked to correct convict' do
     let(:user) { build(:user, role: 'cpip') }
     let(:convict) { build(:convict, user: user) }
-
 
     it { is_expected.to permit_action(:create) }
   end
@@ -103,14 +91,12 @@ describe ConvictPolicy do
     let(:user) { build(:user, role: 'educator') }
     let(:convict) { build(:convict) }
 
-
     it { is_expected.not_to permit_action(:create) }
   end
 
   context 'for a psychologist user' do
     let(:user) { build(:user, role: 'psychologist') }
     let(:convict) { build(:convict) }
-
 
     it { is_expected.not_to permit_action(:create) }
   end
@@ -119,7 +105,6 @@ describe ConvictPolicy do
     let(:user) { build(:user, role: 'overseer') }
     let(:convict) { build(:convict) }
 
-
     it { is_expected.not_to permit_action(:create) }
   end
 
@@ -127,14 +112,12 @@ describe ConvictPolicy do
     let(:user) { build(:user, role: 'dpip') }
     let(:convict) { build(:convict) }
 
-
     it { is_expected.not_to permit_action(:create) }
   end
 
   context 'for a secretary_spip user' do
     let(:user) { build(:user, role: 'secretary_spip') }
     let(:convict) { build(:convict) }
-
 
     it { is_expected.not_to permit_action(:create) }
   end
