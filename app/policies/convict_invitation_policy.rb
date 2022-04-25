54ba0a6fdfd2c0eef0ad1cb93c3279f79a90ed4d
Damien Le Thiec <damien.lethiec@gmail.com>
2022-04-25 16:03:30 +0200

    Don't show invitation if convict not linked to CPIP

--- app/policies/convict_invitation_policy.rb
+++ app/policies/convict_invitation_policy.rb
@@ -1,5 +1,5 @@
 class ConvictInvitationPolicy < ApplicationPolicy
   def create?
-    (user.cpip? || user.admin?) && record.invitable_to_convict_interface?
+    (user.cpip? && record.cpip == user || user.admin?) && record.invitable_to_convict_interface?
   end
 end
