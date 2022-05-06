5e27919585bacaac589731332e832f61693aea36
Charles Marcoin <charles.marcoin@gmail.com>
2022-05-06 16:21:26 +0200

    removed user_roles array of symbols in tests and now using index policy to display the button

--- spec/policies/users_appointment_policy_spec.rb
+++ spec/policies/users_appointment_policy_spec.rb
@@ -7,29 +7,7 @@ describe Users::AppointmentPolicy do
   let(:slot) { create :slot, :without_validations, appointment_type: appointment_type }
   let!(:appointment) { create(:appointment, slot: slot) }
 
-  users_roles = %w[
-    admin
-    local_admin
-    jap
-    bex
-    prosecutor
-    secretary_court
-    dir_greff_bex
-    greff_co
-    greff_tpe
-    greff_crpc
-    greff_ca
-    dir_greff_sap
-    greff_sap
-    dpip
-    cpip
-    secretary_spip
-    educator
-    psychologist
-    overseer
-  ]
-
-  users_roles.delete_if { |r| r == 'cpip' }.each do |role|
+  User.roles.keys.delete_if { |r| r == 'cpip' }.each do |role|
     context "a #{role} user" do
       let(:user) { build(:user, role: role) }
       it { is_expected.to forbid_action(:index) }
