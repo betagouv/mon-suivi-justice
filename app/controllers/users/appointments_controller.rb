8f276c3fc2e954b2511d992a9135d4ece61f0bdf
Charles Marcoin <charles.marcoin@gmail.com>
2022-05-06 15:42:59 +0200

    fixed styling in appointments_controller.rb

--- app/controllers/users/appointments_controller.rb
+++ app/controllers/users/appointments_controller.rb
@@ -1,6 +1,5 @@
 module Users
   class AppointmentsController < ApplicationController
-
     def index
       @appointments = policy_scope([:users, Appointment])
                       .order('slots.date DESC, slots.starting_time DESC')
