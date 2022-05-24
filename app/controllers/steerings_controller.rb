f9e9f70a86980bfb7695203dd016bfaa5eabba9a
Rémy Maucourt <remy.maucourt@yahoo.fr>
2022-05-24 14:56:46 +0200

    add steering page for convict app stats

--- app/controllers/steerings_controller.rb
+++ app/controllers/steerings_controller.rb
@@ -1,8 +1,8 @@
 class SteeringsController < ApplicationController
   before_action :authenticate_user!
 
-  def show
-    authorize :steering, :show?
+  def user_app_stats
+    authorize :steering, :user_app_stats?
 
     @global = DataCollector.new.perform
     @local = []
@@ -10,5 +10,13 @@ class SteeringsController < ApplicationController
     Organization.all.each do |orga|
       @local << DataCollector.new(organization_id: orga.id).perform
     end
+  end
+
+  def convict_app_stats
+    authorize :steering, :convict_app_stats?
+
+    @invites = Convict.where(invitation_to_convict_interface_count: ..1).count
+    @reminder = Convict.where(invitation_to_convict_interface_count: ..2).count
+    @account_created = Convict.where.not(timestamp_convict_interface_creation: nil).count
   end
 end
--- app/policies/steering_policy.rb
+++ app/policies/steering_policy.rb
@@ -1,5 +1,9 @@
 class SteeringPolicy < ApplicationPolicy
-  def show?
+  def user_app_stats?
+    user.admin?
+  end
+
+  def convict_app_stats?
     user.admin?
   end
 end
