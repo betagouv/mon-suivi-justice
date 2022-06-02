dd7dc93969c1bd24a276ddda8974763d6ad2da42
Charles Marcoin <charles.marcoin@gmail.com>
2022-06-02 09:42:10 +0200

    fix policy and added template

--- app/controllers/users/appointments_controller.rb
+++ app/controllers/users/appointments_controller.rb
@@ -1,9 +1,24 @@
 module Users
   class AppointmentsController < ApplicationController
     def index
-      @appointments = policy_scope([:users, Appointment])
-                      .order('slots.date DESC, slots.starting_time DESC')
-                      .page(params[:page]).per(25)
+      @q = policy_scope([:users, Appointment]).active.ransack(params[:q])
+
+      @all_appointments = @q.result(distinct: true)
+      .joins(:convict, slot: [:appointment_type, { agenda: [:place] }])
+      .includes(:convict, slot: [:appointment_type, { agenda: [:place] }])
+      .order('slots.date ASC, slots.starting_time ASC')
+
+      @appointments = @all_appointments.page(params[:page]).per(25)
+
+      respond_to do |format|
+        format.html do
+          render 'appointments/index'
+        end
+        format.pdf do
+          render template: 'appointments/index_pdf.html.erb',
+          pdf: 'Liste des rdv', footer: { right: '[page]/[topage]' }
+        end
+      end
       authorize @appointments
     end
   end
--- app/policies/users/appointment_policy.rb
+++ app/policies/users/appointment_policy.rb
@@ -3,7 +3,7 @@ module Users
     class Scope < Scope
       def resolve
         if user.cpip? || user.psychologist? || user.overseer?
-          scope.includes(:convict, slot: [:appointment_type, { agenda: [:place] }]).where(convict: { user: user })
+          scope.includes(:convict, slot: [:appointment_type, { agenda: [:place] }]).where(user: user)
         else
           raise Pundit::NotAuthorizedError
         end
