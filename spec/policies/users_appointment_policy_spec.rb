1a68fd98d08a5920e9a6c4909f09d2429bcf0c66
Charles Marcoin <charles.marcoin@gmail.com>
2022-06-09 10:20:12 +0200

    started adding a test for the users appointements scope

--- spec/policies/users_appointment_policy_spec.rb
+++ spec/policies/users_appointment_policy_spec.rb
@@ -7,15 +7,38 @@ describe Users::AppointmentPolicy do
   let(:slot) { create :slot, :without_validations, appointment_type: appointment_type }
   let!(:appointment) { create(:appointment, slot: slot) }
 
-  User.roles.keys.delete_if { |r| r == 'cpip' }.each do |role|
-    context "a #{role} user" do
+  User.roles.keys.delete_if { |r| %[cpip psychologist overseer ].include? r }.each do |role|
+    context "for a #{role} user" do
       let(:user) { build(:user, role: role) }
+      let(:scope) { Pundit.policy_scope!(user, Users::AppointmentPolicy) } 
+
+      it 'should raise a '
+
       it { is_expected.to forbid_action(:index) }
     end
   end
 
+  # Replace this test with https://stackoverflow.com/questions/54330559/how-to-test-pundit-scopes-in-rspec
   context 'a cpip user' do
     let(:user) { build(:user, role: 'cpip') }
     it { is_expected.to permit_action(:index) }
   end
+
+
+  describe "Scope" do
+    context 'user is not cpip / psychologist / overseer' do
+      it 'should raise an error' do
+        expect(scope.to_a).to match_array([account2_report])
+      end 
+    end
+    context 'admin user' do
+      let(:user) { User.new(role: 'admin') }
+      it 'allows access to all the reports' do
+        expect(scope.to_a).to match_array([account1_report, account2_report])
+      end
+    end
+  end
+
+
+
 end
