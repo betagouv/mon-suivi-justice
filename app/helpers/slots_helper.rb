6f6f22ba9f05b3b9ee55a153ea39e6dc37cfc27c
Charles Marcoin <charles.marcoin@gmail.com>
2022-06-15 12:49:30 +0200

    fixed typo

--- app/helpers/slots_helper.rb
+++ app/helpers/slots_helper.rb
@@ -4,7 +4,7 @@ module SlotsHelper
            elsif user.work_at_bex? then "Sortie d'audience SPIP"
            elsif user.work_at_spip? || user.admin? then ["Sortie d'audience SAP",
                                                          "Sortie d'audience SPIP"]
-           # assuming local_admin does not work at bex or spip (TODO need to clarify roles) :
+           # assuming local_admin does not work at bex or spip (TODO: need to clarify roles) :
            elsif user.local_admin?
              user.organization.organization_type == 'spip' ? "Sortie d'audience SPIP" : "Sortie d'audience SAP"
            end
