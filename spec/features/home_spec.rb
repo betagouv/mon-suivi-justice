d6209cedd5be37380d6be2577e95cfb77ac84202
Charles Marcoin <charles.marcoin@gmail.com>
2022-06-30 10:31:03 +0200

    added test

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -16,32 +16,43 @@ RSpec.feature 'Home', type: :feature do
 
   describe 'Uninformed appointments alert'
   before do
-    @user = create_admin_user_and_login
+    user = create_admin_user_and_login
 
+    appointment_type = create :appointment_type
+    create :notification_type, role: :no_show, appointment_type: appointment_type
+    create :notification_type, role: :reminder, appointment_type: appointment_type
+                               
     place = create :place, organization: @user.organization
-    @agenda = create :agenda, place: place
+    agenda = create :agenda, place: place
 
-    slot1 = create(:slot, :without_validations, agenda: @agenda,
-                                                date: Date.civil(2022, 6, 27),
+    slot1 = create(:slot, :without_validations, agenda: agenda, appointment_type: appointment_type,
+                                                date: Date.civil(2022, 5, 26),
                                                 starting_time: new_time_for(13, 0))
-    slot2 = create(:slot, agenda: @agenda,
+    slot2 = create(:slot, agenda: agenda, appointment_type: appointment_type,
                           date: Date.civil(2022, 6, 27),
                           starting_time: new_time_for(15, 30))
 
-    convict = create(:convict)
-    create :areas_convicts_mapping, convict: convict, area: @user.organization.departments.first
+    convict = create :convict
+    create :areas_convicts_mapping, convict: convict, area: user.organization.departments.first
 
-    @appointment1 = build(:appointment, convict: convict, slot: slot1, state: 'fulfiled')
-    @appointment2 = build(:appointment, convict: convict, slot: slot2, state: 'booked')
-    @appointment1.save(validate: false)
-    @appointment2.save(validate: false)
+    appointment1 = build :appointment, :with_notifications, convict: convict, slot: slot1
+    appointment2 = build :appointment, :with_notifications, convict: convict, slot: slot2
+    appointment1.save validate: false
+    appointment2.save validate: false
+
+    appointment2.book
   end
 
-  it 'should display a flash alert if uninformed appointments percentage is above 20' do
+  it 'should display a flash alert and link to a page which lists only uninformed appointments' do
     visit home_path
 
     expect(page).to have_content("Attention, il y a 50% de rendez-vous dont le statut n'est pas complété")
-  end
 
-  it ''
+    within first('p.alert', text: 'Attention') do
+      click_on('Cliquez-ici')
+    end
+
+    expect(page).to have_content(Date.civil(2022, 6, 27))
+    expect(page).to have_content('15:30')
+  end
 end
