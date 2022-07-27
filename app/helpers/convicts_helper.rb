d269543cc51302bb0ee27ba052d05f4c5e7e647b
Rémy Maucourt <remy.maucourt@yahoo.fr>
2022-07-27 18:27:16 +0200

    fix bug convict#edit

--- app/helpers/convicts_helper.rb
+++ app/helpers/convicts_helper.rb
@@ -8,6 +8,6 @@ module ConvictsHelper
   end
 
   def can_be_linked_to_user?(convict, current_user)
-    current_user.cpip? && convict.present? && convict.cpip.nil?
+    (current_user.cpip? || current_user.dpip?) && convict.present? && convict.cpip.nil?
   end
 end
