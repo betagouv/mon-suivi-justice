20beca7aec7006695a0ca2e973ff18163683f928
Rémy Maucourt <remy.maucourt@yahoo.fr>
2022-09-02 15:47:29 +0200

    setup dropdown for time zone selection

--- app/helpers/organization_helper.rb
+++ app/helpers/organization_helper.rb
@@ -1,30 +1,41 @@
 module OrganizationHelper
+  FRENCH_TIMEZONES = [
+    { id: 'Europe/Paris', label: 'France Métropolitaine' },
+    { id: 'Indian/Mayotte', label: 'Mayotte' },
+    { id: 'Europe/Samara', label: 'La Réunion' },
+    { id: 'Pacific/Noumea', label: 'Nouvelle-Calédonie' },
+    { id: 'Pacific/Tarawa', label: 'Wallis et Futuna' },
+    { id: 'Pacific/Honolulu', label: 'Polynésie française - îles Australes / Société / Tuamotu' },
+    { id: 'Pacific/Marquesas', label: 'Polynésie française - îles Marquises' },
+    { id: 'Pacific/Gambier', label: 'Polynésie française - îles Gambier' },
+    { id: 'America/Puerto_Rico', label: 'Guadeloupe / Martinique / St Martin / St Barthélemy' },
+    { id: 'America/Argentina/Cordoba', label: 'Guyane' },
+    { id: 'America/Miquelon', label: 'St Pierre et Miquelon' }
+  ].freeze
+
+  def time_zones_for_select
+    FRENCH_TIMEZONES.map do |tz|
+      data = TZInfo::Timezone.get(tz[:id])
+      ["#{formatted_offset(data)} - #{tz[:label]}", data.name]
+    end
+  end
+
+  def offset_in_hours(time_zone)
+    time_zone.current_period.offset.utc_total_offset.to_f / 3600.0
+  end
+
+  def formatted_offset(data)
+    offset = offset_in_hours(data)
+    sign = offset.positive? ? '+' : '-'
+    hour = offset.abs.to_i.to_s.rjust(2, '0')
+
+    minutes = (offset.modulo(1) * 60).to_i.to_s.rjust(2, '0')
+    "UTC #{sign} #{hour}:#{minutes}"
+  end
+
   def form_organization_type
     Organization.organization_types.map do |k, _v|
       [I18n.t("activerecord.attributes.organization.organization_types.#{k}"), k]
     end.sort_by(&:last)
-  end
-
-  def time_zones_for_select
-    # Toutes les zones, hors territoires autraux et ile clipperton
-    tz_list = [
-      TZInfo::Timezone.get('Europe/Paris'), # ETC +2
-      TZInfo::Timezone.get('Pacific/Honolulu'), # ETC -10
-      TZInfo::Timezone.get('Pacific/Marquesas'), # ETC -9:30
-      TZInfo::Timezone.get('Pacific/Gambier'), # ETC -9
-      TZInfo::Timezone.get('America/Puerto_Rico'), # ETC -4
-      TZInfo::Timezone.get('America/Argentina/Cordoba'), # ETC -3
-      TZInfo::Timezone.get('America/Miquelon'), # ETC -3 + DST
-      TZInfo::Timezone.get('Indian/Mayotte'), # ETC +3
-      TZInfo::Timezone.get('Europe/Samara'), # ETC +4
-      TZInfo::Timezone.get('Pacific/Noumea'), # ETC +11
-      TZInfo::Timezone.get('Pacific/Tarawa') # ETC +12
-    ]
-
-    tz_list.map { |tz| [ "#{offset_in_hours(tz)} - #{tz.name}", tz.name] }
-  end
-
-  def offset_in_hours(tz)
-    (tz.current_period.offset.utc_total_offset).to_f / 3600.0
   end
 end
