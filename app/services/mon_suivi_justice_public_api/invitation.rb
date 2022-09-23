4fd10f9d026b29543b8541cdf08eb9f575d8aba8
Charles Marcoin <charles.marcoin@gmail.com>
2022-09-23 15:30:25 +0200

    added invite to node app in invitation.rb

--- app/services/mon_suivi_justice_public_api/invitation.rb
+++ app/services/mon_suivi_justice_public_api/invitation.rb
@@ -2,7 +2,11 @@ module MonSuiviJusticePublicApi
   class Invitation < Base
     class << self
       def create(params = {})
-        connection.post("#{BASE_URL}/sms_invitations", params.to_json, 'Content-Type': 'application/json')
+        if ENV['APP'] == 'mon-suivi-justice-staging'
+          connection.post("#{BASE_URL}/api/users/invite", params.to_json, 'Content-Type': 'application/json')
+        else
+          connection.post("#{BASE_URL}/sms_invitations", params.to_json, 'Content-Type': 'application/json')
+        end
       end
     end
   end
