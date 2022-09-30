90a1e46c17c7feac3f9ea83ae77c132e3a503266
Charles Marcoin <charles.marcoin@gmail.com>
2022-09-30 16:26:26 +0200

    api fixes and linting issues fixes

--- config/routes.rb
+++ config/routes.rb
@@ -102,7 +102,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: "json"} do
     namespace :v1 do
       resources :convicts, only: :show do
-        get 'cpip' => 'convicts#get_cpip'
+        get 'cpip'
         resource :invitation, only: :update, controller: 'convict_invitations'
       end
     end
