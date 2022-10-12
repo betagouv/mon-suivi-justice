e645a619272635292dbc5b95f2e85411663863d1
Charles Marcoin <charles.marcoin@gmail.com>
2022-10-12 20:26:41 +0200

    added jurisdiction collection and link to admin interface on production. revamped seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,24 @@ FRENCH_JURISDICTIONS.each do |name|
 end
 
 org_spip_92 = Organization.create!(name: 'SPIP 92', organization_type: 'spip')
-
 org_tj_nanterre = Organization.create!(name: 'TJ Nanterre', organization_type: 'tj')
-
 org_spip_75 = Organization.create!(name: 'SPIP 75', organization_type: 'spip')
+org_spip_77 = Organization.create!(name: 'SPIP 77', organization_type: 'spip')
+org_tj_melun = Organization.create!(name: 'TJ Melun', organization_type: 'tj')
+org_tj_fontainebleau = Organization.create!(name: 'TJ de Fointainebleau', organization_type: 'tj')
 
 # Un service peut être rattaché à des départements et/ou des juridictions
 # Cela impacte le rattachement des ppsmj à tel  département et/ou juridiction à leur création
 AreasOrganizationsMapping.create organization: org_spip_92, area: Department.find_by(number: '92')
 AreasOrganizationsMapping.create organization: org_spip_92, area: Jurisdiction.find_by(name: 'TJ NANTERRE')
 AreasOrganizationsMapping.create organization: org_tj_nanterre, area: Department.find_by(number: '92')
+
 AreasOrganizationsMapping.create organization: org_spip_75, area: Department.find_by(number: '75')
+
+AreasOrganizationsMapping.create organization: org_spip_77, area: Department.find_by(number: '77')
+AreasOrganizationsMapping.create organization: org_spip_77, area: Jurisdiction.find_by(name: 'TJ MELUN')
+AreasOrganizationsMapping.create organization: org_tj_melun, area: Department.find_by(number: '77')
+AreasOrganizationsMapping.create organization: org_tj_fontainebleau, area: Department.find_by(number: '77')
 
 convict_1 = Convict.create!(first_name: "Michel", last_name: "Blabla", phone: "0677777777", appi_uuid: "12345")
 convict_2 = Convict.create!(first_name: "Dark", last_name: "Vador", phone: "0600000000", appi_uuid: "12346")
@@ -28,84 +35,113 @@ AreasConvictsMapping.create convict: convict_2, area: Department.find_by(number:
 AreasConvictsMapping.create convict: convict_2, area: Jurisdiction.find_by(name: 'TJ NANTERRE')
 AreasConvictsMapping.create convict: convict_3, area: Department.find_by(number: '75')
 
-admin = User.create!(
+User.create!(
         organization: org_spip_92, email: 'admin@example.com', password: '1mot2passeSecurise!',
         password_confirmation: '1mot2passeSecurise!', role: :admin, first_name: 'Kevin', last_name: 'McCallister'
       )
 
-cpip = User.create!(
+User.create!(
   organization: org_spip_92, email: 'cpip@example.com', password: '1mot2passeSecurise!',
   password_confirmation: '1mot2passeSecurise!', role: :cpip, first_name: 'Bob', last_name: 'Dupneu'
 )
 
-cpip2 = User.create!(
+User.create!(
   organization: org_spip_75, email: 'cpip75@example.com', password: '1mot2passeSecurise!',
   password_confirmation: '1mot2passeSecurise!', role: :cpip, first_name: 'Luke', last_name: 'Skywalker'
 )
 
-local_admin_spip_75 = User.create!(
+User.create!(
   organization: org_spip_75, email: 'localadminSpip75@example.com', password: '1mot2passeSecurise!',
-  password_confirmation: '1mot2passeSecurise!', role: :local_admin, first_name: 'Shia', last_name: 'Leboeuf'
+  password_confirmation: '1mot2passeSecurise!', role: :local_admin, first_name: 'Matthieu', last_name: 'Paris'
 )
 
-local_admin_tj_nanterre = User.create!(
+User.create!(
+  organization: org_spip_92, email: 'localadminSpip92@example.com', password: '1mot2passeSecurise!',
+  password_confirmation: '1mot2passeSecurise!', role: :local_admin, first_name: 'Henri', last_name: 'HautsDeSeine'
+)
+
+User.create!(
   organization: org_tj_nanterre, email: 'localadmintjnanterre@example.com', password: '1mot2passeSecurise!',
   password_confirmation: '1mot2passeSecurise!', role: :local_admin, first_name: 'André', last_name: 'Dussolier'
 )
 
-place_1 = Place.create!(
+User.create!(
+  organization: org_tj_melun, email: 'localadmintjmelun@example.com', password: '1mot2passeSecurise!',
+  password_confirmation: '1mot2passeSecurise!', role: :local_admin, first_name: 'Michel', last_name: 'Melun'
+)
+
+User.create!(
+  organization: org_tj_fontainebleau, email: 'localadmintjfontainebleau@example.com', password: '1mot2passeSecurise!',
+  password_confirmation: '1mot2passeSecurise!', role: :local_admin, first_name: 'Robert', last_name: 'Fontaine'
+)
+
+place_tj_nanterre = Place.create!(
   organization: org_tj_nanterre, name: "Tribunal judiciaire de Nanterre", adress: "179-191 av. Joliot Curie, 92020 NANTERRE", phone: '0606060606'
 )
 
-agenda_1 = Agenda.create!(place: place_1, name: "Agenda 1 tribunal Nanterre")
-agenda_2 = Agenda.create!(place: place_1, name: "Agenda 2 tribunal Nanterre")
-appointment_type_1 = AppointmentType.create!(name: "Sortie d'audience SAP")
-appointment_type_2 = AppointmentType.create!(name: 'RDV de suivi JAP')
-PlaceAppointmentType.create!(place: place_1, appointment_type: appointment_type_1)
-PlaceAppointmentType.create!(place: place_1, appointment_type: appointment_type_2)
+place_tj_melun = Place.create!(
+  organization: org_tj_melun, name: "Tribunal judiciaire de Melun", adress: "2 Av. du Général Leclerc, 77010 Melun", phone: '0606060606'
+)
 
-NotificationType.create!(appointment_type: appointment_type_1, role: :summon, template: "Vous êtes convoqué, merci de venir.", is_default: true)
-NotificationType.create!(appointment_type: appointment_type_1, role: :reminder, template: "RAPPEL Vous êtes convoqué, vraiment il faut venir.", reminder_period: :two_days, is_default: true)
-NotificationType.create!(appointment_type: appointment_type_1, role: :cancelation, template: "Finalement non, c'est pas la peine.", is_default: true)
-NotificationType.create!(appointment_type: appointment_type_1, role: :no_show, template: "Vous n'êtes pas venu :(", is_default: true)
-NotificationType.create!(appointment_type: appointment_type_1, role: :reschedule, template: "Changement du rdv de date X a date Y.", is_default: true)
-NotificationType.create!(appointment_type: appointment_type_2, role: :summon, template: "Vous êtes convoqué, merci de venir.", is_default: true)
-NotificationType.create!(appointment_type: appointment_type_2, role: :reminder, template: "RAPPEL Vous êtes convoqué, vraiment il faut venir.", reminder_period: :two_days, is_default: true)
-NotificationType.create!(appointment_type: appointment_type_2, role: :cancelation, template: "Finalement non, c'est pas la peine.", is_default: true)
-NotificationType.create!(appointment_type: appointment_type_2, role: :no_show, template: "Vous n'êtes pas venu :(", is_default: true)
-NotificationType.create!(appointment_type: appointment_type_2, role: :reschedule, template: "Changement du rdv de date X a date Y.", is_default: true)
+place_tj_fontainebleau = Place.create!(
+  organization: org_tj_fontainebleau, name: "Tribunal judiciaire de Fontainebleau", adress: "159 Rue Grande, 77300 Fontainebleau", phone: '0606060606'
+)
 
-SlotType.create(appointment_type: appointment_type_1, agenda: agenda_1, week_day: :wednesday, starting_time: Time.new(2021, 6, 21, 10, 00, 0), duration: 60, capacity: 3)
-SlotType.create(appointment_type: appointment_type_1, agenda: agenda_1, week_day: :wednesday, starting_time: Time.new(2021, 6, 21, 11, 00, 0), duration: 60, capacity: 3)
-
-place_2 = Place.create!(organization: org_spip_92, name: "SPIP 92", adress: "94 Boulevard du Général Leclerc, 92000 Nanterre", phone: '0606060606')
-agenda_3 = Agenda.create!(place: place_2, name: "Agenda SPIP 92")
-
-appointment_type_3 = AppointmentType.create!(name: "RDV de suivi SPIP")
-PlaceAppointmentType.create!(place: place_2, appointment_type: appointment_type_3)
-
-NotificationType.create!(appointment_type: appointment_type_3, role: :summon, template: "Vous êtes convoqué, merci de venir.", is_default: true)
-NotificationType.create!(appointment_type: appointment_type_3, role: :reminder, template: "RAPPEL Vous êtes convoqué, vraiment il faut venir.", reminder_period: :two_days, is_default: true)
-NotificationType.create!(appointment_type: appointment_type_3, role: :cancelation, template: "Finalement non, c'est pas la peine.", is_default: true)
-NotificationType.create!(appointment_type: appointment_type_3, role: :no_show, template: "Vous n'êtes pas venu :(", is_default: true)
-NotificationType.create!(appointment_type: appointment_type_3, role: :reschedule, template: "Changement du rdv de date X a date Y.", is_default: true)
-
-appointment_type_4 = AppointmentType.create!(name: "Sortie d'audience SPIP")
-PlaceAppointmentType.create!(place: place_2, appointment_type: appointment_type_4)
-NotificationType.create!(appointment_type: appointment_type_4, role: :summon, template: "Vous êtes convoqué à votre sortie d'audience SPIP, merci de venir.", is_default: true)
-NotificationType.create!(appointment_type: appointment_type_4, role: :reminder, template: "RAPPEL Vous êtes convoqué à votre sortie d'audience SPIP, vraiment il faut venir.", reminder_period: :two_days, is_default: true)
-NotificationType.create!(appointment_type: appointment_type_4, role: :cancelation, template: "Finalement non, c'est pas la peine.", is_default: true)
-NotificationType.create!(appointment_type: appointment_type_4, role: :no_show, template: "Vous n'êtes pas venu :(", is_default: true)
-NotificationType.create!(appointment_type: appointment_type_4, role: :reschedule, template: "Changement du rdv de date X a date Y.", is_default: true)
-
-SlotType.create(appointment_type: appointment_type_4, agenda: agenda_3, week_day: :monday, starting_time: Time.new(2021, 6, 21, 10, 00, 0), duration: 60, capacity: 3)
-SlotType.create(appointment_type: appointment_type_4, agenda: agenda_3, week_day: :monday, starting_time: Time.new(2021, 6, 21, 11, 00, 0), duration: 60, capacity: 3)
-
-org_spip_75_place_1 = Place.create!(
+place_spip_75 = Place.create!(
   organization: org_spip_75, name: "SPIP 75", adress: "12-14 rue Charles Fourier, 75 013 PARIS", phone: '0606060606'
 )
 
-agenda_org_spip_75_place_1 = Agenda.create!(place: org_spip_75_place_1, name: "Agenda SPIP 75")
+place_spip_92 = Place.create!(organization: org_spip_92, name: "SPIP 92", adress: "94 Boulevard du Général Leclerc, 92000 Nanterre", phone: '0606060606')
+
+agenda_tj_nanterre = Agenda.create!(place: place_tj_nanterre, name: "Agenda 1 tribunal Nanterre")
+agenda_tj_nanterre_2 = Agenda.create!(place: place_tj_nanterre, name: "Agenda 2 tribunal Nanterre")
+
+agenda_tj_melun = Agenda.create!(place: place_tj_melun, name: "Agenda 1 tribunal Melun")
+agenda_tj_fontainebleau = Agenda.create!(place: place_tj_fontainebleau, name: "Agenda 1 tribunal Fontainebleau")
+
+agenda_spip_75 = Agenda.create!(place: place_spip_75, name: "Agenda SPIP 75")
+agenda_spip_92 = Agenda.create!(place: place_spip_92, name: "Agenda SPIP 92")
+
+apt_type_sortie_audience_sap = AppointmentType.create!(name: "Sortie d'audience SAP")
+apt_type_rdv_suivi_jap = AppointmentType.create!(name: 'RDV de suivi JAP')
+apt_type_rdv_suivi_spip = AppointmentType.create!(name: "RDV de suivi SPIP")
+apt_type_sortie_audience_spip = AppointmentType.create!(name: "Sortie d'audience SPIP")
+
+PlaceAppointmentType.create!(place: place_tj_nanterre, appointment_type: apt_type_sortie_audience_sap)
+PlaceAppointmentType.create!(place: place_tj_nanterre, appointment_type: apt_type_rdv_suivi_jap)
+PlaceAppointmentType.create!(place: place_spip_92, appointment_type: apt_type_rdv_suivi_spip)
+PlaceAppointmentType.create!(place: place_spip_92, appointment_type: apt_type_sortie_audience_spip)
+
+NotificationType.create!(appointment_type: apt_type_sortie_audience_sap, role: :summon, template: "Vous êtes convoqué, merci de venir.", is_default: true)
+NotificationType.create!(appointment_type: apt_type_sortie_audience_sap, role: :reminder, template: "RAPPEL Vous êtes convoqué, vraiment il faut venir.", reminder_period: :two_days, is_default: true)
+NotificationType.create!(appointment_type: apt_type_sortie_audience_sap, role: :cancelation, template: "Finalement non, c'est pas la peine.", is_default: true)
+NotificationType.create!(appointment_type: apt_type_sortie_audience_sap, role: :no_show, template: "Vous n'êtes pas venu :(", is_default: true)
+NotificationType.create!(appointment_type: apt_type_sortie_audience_sap, role: :reschedule, template: "Changement du rdv de date X a date Y.", is_default: true)
+
+NotificationType.create!(appointment_type: apt_type_rdv_suivi_jap, role: :summon, template: "Vous êtes convoqué, merci de venir.", is_default: true)
+NotificationType.create!(appointment_type: apt_type_rdv_suivi_jap, role: :reminder, template: "RAPPEL Vous êtes convoqué, vraiment il faut venir.", reminder_period: :two_days, is_default: true)
+NotificationType.create!(appointment_type: apt_type_rdv_suivi_jap, role: :cancelation, template: "Finalement non, c'est pas la peine.", is_default: true)
+NotificationType.create!(appointment_type: apt_type_rdv_suivi_jap, role: :no_show, template: "Vous n'êtes pas venu :(", is_default: true)
+NotificationType.create!(appointment_type: apt_type_rdv_suivi_jap, role: :reschedule, template: "Changement du rdv de date X a date Y.", is_default: true)
+
+NotificationType.create!(appointment_type: apt_type_rdv_suivi_spip, role: :summon, template: "Vous êtes convoqué, merci de venir.", is_default: true)
+NotificationType.create!(appointment_type: apt_type_rdv_suivi_spip, role: :reminder, template: "RAPPEL Vous êtes convoqué, vraiment il faut venir.", reminder_period: :two_days, is_default: true)
+NotificationType.create!(appointment_type: apt_type_rdv_suivi_spip, role: :cancelation, template: "Finalement non, c'est pas la peine.", is_default: true)
+NotificationType.create!(appointment_type: apt_type_rdv_suivi_spip, role: :no_show, template: "Vous n'êtes pas venu :(", is_default: true)
+NotificationType.create!(appointment_type: apt_type_rdv_suivi_spip, role: :reschedule, template: "Changement du rdv de date X a date Y.", is_default: true)
+
+NotificationType.create!(appointment_type: apt_type_sortie_audience_spip, role: :summon, template: "Vous êtes convoqué à votre sortie d'audience SPIP, merci de venir.", is_default: true)
+NotificationType.create!(appointment_type: apt_type_sortie_audience_spip, role: :reminder, template: "RAPPEL Vous êtes convoqué à votre sortie d'audience SPIP, vraiment il faut venir.", reminder_period: :two_days, is_default: true)
+NotificationType.create!(appointment_type: apt_type_sortie_audience_spip, role: :cancelation, template: "Finalement non, c'est pas la peine.", is_default: true)
+NotificationType.create!(appointment_type: apt_type_sortie_audience_spip, role: :no_show, template: "Vous n'êtes pas venu :(", is_default: true)
+NotificationType.create!(appointment_type: apt_type_sortie_audience_spip, role: :reschedule, template: "Changement du rdv de date X a date Y.", is_default: true)
+
+SlotType.create(appointment_type: apt_type_sortie_audience_spip, agenda: agenda_spip_92, week_day: :monday, starting_time: Time.new(2021, 6, 21, 10, 00, 0), duration: 60, capacity: 3)
+SlotType.create(appointment_type: apt_type_sortie_audience_spip, agenda: agenda_spip_92, week_day: :monday, starting_time: Time.new(2021, 6, 21, 11, 00, 0), duration: 60, capacity: 3)
+SlotType.create(appointment_type: apt_type_sortie_audience_sap, agenda: agenda_tj_nanterre, week_day: :wednesday, starting_time: Time.new(2021, 6, 21, 10, 00, 0), duration: 60, capacity: 3)
+SlotType.create(appointment_type: apt_type_sortie_audience_sap, agenda: agenda_tj_nanterre, week_day: :wednesday, starting_time: Time.new(2021, 6, 21, 11, 00, 0), duration: 60, capacity: 3)
+SlotType.create(appointment_type: apt_type_sortie_audience_sap, agenda: agenda_tj_melun, week_day: :monday, starting_time: Time.new(2021, 6, 21, 10, 00, 0), duration: 60, capacity: 3)
+SlotType.create(appointment_type: apt_type_sortie_audience_sap, agenda: agenda_tj_fontainebleau, week_day: :tuesday, starting_time: Time.new(2021, 6, 21, 11, 00, 0), duration: 60, capacity: 3)
 
 SlotFactory.perform
 
