cd8d86d1611ebccd2beaa2fda22f60cca8d43879
Matthieu Faugere <matthieu.faugere@gmail.com>
2022-11-10 14:26:10 +0100

    Allow to display password (#168)
    * Allow to display password
    
    * Update node version
    
    * remove dsfr icon
    
    * remove dsfr
    
    * fix css issue
    
    * dsfr
    
    * try stuff
    
    * remove dsfr

---
+++ app/javascript/components/password_toggle.js
@@ -0,0 +1,27 @@
+function togglePassword() {
+  $('[data-action="toggle-password"]').on("click", function(e) {
+    const eyeOn = 'fa-eye';
+    const eyeOff = 'fa-eye-slash';
+    const icon = $(e.target);
+    const target = icon.attr("data-target");
+
+    if (!target) { return; }
+
+    const passwordInput = $(`#${target}`);
+
+    if (passwordInput.length === 0) {
+      return;
+    }
+    if (passwordInput.attr('type') === 'password') {
+      icon.removeClass(eyeOn);
+      icon.addClass(eyeOff);
+      passwordInput.attr('type', 'text');
+    } else {
+      icon.addClass(eyeOn);
+      icon.removeClass(eyeOff);
+      passwordInput.attr('type', 'password');
+    }
+
+  })
+}
+document.addEventListener('turbolinks:load', togglePassword)
