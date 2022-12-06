6a4ab87fddfd97d56505a0ab002fd56dcdeb8028
Matthieu Faugere <matthieu.faugere@gmail.com>
2022-12-06 14:52:09 +0100

    display notification

--- app/javascript/channels/convict_invitation_channel.js
+++ app/javascript/channels/convict_invitation_channel.js
@@ -14,5 +14,15 @@ consumer.subscriptions.create("Noticed::ConvictInvitationChannel", {
   received(data) {
     // Called when there's incoming data on the websocket for this channel
     console.log(data)
+    const container = $("#user-notifications-container");
+    const notif = `
+    <div class="fr-alert fr-alert--${data.type} fr-alert--sm">
+      <p>
+        l'invitation à ${data.invitation_params.first_name} ${data.invitation_params.last_name} ${data.status === 'pending' ? "est en cours d'envoi" : "a été envoyée"}
+      </p>
+    </div>
+    `
+    console.log(container)
+    container.append(notif)
   }
 });
