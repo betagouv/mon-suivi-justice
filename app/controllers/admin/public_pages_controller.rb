92e44171ac95a691610593f669262e401470e5e6
Charles Marcoin <charles.marcoin@gmail.com>
2022-12-08 10:58:03 +0100

    now detecting if image already exists

--- app/controllers/admin/public_pages_controller.rb
+++ app/controllers/admin/public_pages_controller.rb
@@ -39,38 +39,32 @@ module Admin
         file64 =  Base64.strict_encode64(uploaded_image.read)
         mime_type = "image/png"
         uploaded_image_name = params[:picture].original_filename
+        image_path_in_repo = "app/frontend/images/#{uploaded_image_name}"
 
         sha = gh_api_client.get("/repos/betagouv/mon-suivi-justice-public/git/ref/heads/add-spipXX").object.sha
 
         begin
-          existing_image = gh_api_client.contents("betagouv/mon-suivi-justice-public", {path: "app/frontend/images/#{uploaded_image_name}g", ref: sha})
+          existing_image = gh_api_client.contents("betagouv/mon-suivi-justice-public", {path: image_path_in_repo, ref: sha})
+          
         rescue Octokit::NotFound
           gh_api_client.create_contents("betagouv/mon-suivi-justice-public",
-            "app/frontend/images/#{uploaded_image_name}",
+            image_path_in_repo,
             "Adding content",
             file64,
-            :branch => "add-spipXX")
-
-
+            {branch: "add-spipXX"})
         else
-
-
+          debugger
+          gh_api_client.update_contents("betagouv/mon-suivi-justice-public",
+            image_path_in_repo,
+            "Updating image",
+            existing_image[:sha],
+            file64,
+            {branch: "add-spipXX"})
         end
-        
-
-
-
-
-
-
-
-
-
-
-
-
+      
         reponse = gh_api_client.last_response
 
+        
         
 
       end
