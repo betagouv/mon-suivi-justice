c55f7b1abb2ff187a0bfeac8a833e0412e31f3b1
Charles Marcoin <charles.marcoin@gmail.com>
2022-12-09 16:22:09 +0100

    dynamic branch creating, various fixes for image and page upload to github

--- app/controllers/admin/public_pages_controller.rb
+++ app/controllers/admin/public_pages_controller.rb
@@ -1,19 +1,9 @@
 module Admin
     class PublicPagesController < Admin::ApplicationController
       include Devise::Controllers::Helpers
-
       require 'octokit'
-      require 'base64'
   
       def index
-
-        gh_api_client = Octokit::Client.new(:access_token => ENV['GITHUB_API_TOKEN'])
-
-
-        @sha_object = gh_api_client.get("/repos/betagouv/mon-suivi-justice-public/git/ref/heads/add-spipXX").object
-
-
-
         render locals: {
           resources: [],
           search_term: '',
@@ -23,22 +13,20 @@ module Admin
       end
 
       def create
+        @org_name = params[:organization_name]
+        @gh_api_client = Octokit::Client.new(:access_token => ENV['GITHUB_API_TOKEN'])
 
-        # FIND_DEVELOP
         # Get develop branch hash (https://docs.github.com/fr/rest/git/refs?apiVersion=2022-11-28#get-a-reference)
-        # develop_sha = gh_api_client.get("/repos/betagouv/mon-suivi-justice-public/git/ref/heads/develop").object.sha
+        develop_sha = @gh_api_client.get("/repos/betagouv/mon-suivi-justice-public/git/ref/heads/develop").object.sha
 
         # Create new branch from develop
-        # gh_api_client.create_ref("betagouv/mon-suivi-justice-public", "heads/add-spipXX", develop_sha)
+        @gh_api_client.create_ref("betagouv/mon-suivi-justice-public", "heads/add-#{@org_name}-page", develop_sha)
 
-        @gh_api_client = Octokit::Client.new(:access_token => ENV['GITHUB_API_TOKEN'])
-        @org_name = params[:spip_name]
-        @sha = @gh_api_client.get("/repos/betagouv/mon-suivi-justice-public/git/ref/heads/add-spipXX").object.sha
+        @new_page_branch = @gh_api_client.get("/repos/betagouv/mon-suivi-justice-public/git/ref/heads/add-#{@org_name}-page").object.sha
 
-        #handle_image
+        handle_image
         handle_template
-
-
+        #handle_routes
 
         flash[:notice] = "La création de la page de RDV est en cours. Elle sera disponible dans le CMS d'ici quelques minutes"
 
@@ -48,75 +36,66 @@ module Admin
       private
 
       def handle_image
-        image_name = params[:picture].original_filename
-        image_path_in_repo = "app/frontend/images/#{image_name}"
-        
-        temp_picture = params[:picture].tempfile
+        image_extension = File.extname(params[:picture].original_filename)
+        @image_filename = @org_name + image_extension
+        image_repo_path = "app/frontend/images/#{@image_filename}"
+
+        temp_image = params[:picture].tempfile
         # read ensures files is closed before returning
-        stringified_picture = temp_picture.read
+        org_image = temp_image.read
 
         begin
-          existing_image = @gh_api_client.contents("betagouv/mon-suivi-justice-public", {path: image_path_in_repo, ref: @sha})
+          existing_image = @gh_api_client.contents("betagouv/mon-suivi-justice-public", {path: image_repo_path, ref: @new_page_branch})
           
         rescue Octokit::NotFound
           @gh_api_client.create_contents("betagouv/mon-suivi-justice-public",
-            image_path_in_repo,
-            "Adding content",
-            stringified_picture,
-            {branch: "add-spipXX"})
+            image_repo_path,
+            "Adding image for #{@org_name}",
+            org_image,
+            {branch: "add-#{@org_name}-page"})
         else
           @gh_api_client.update_contents("betagouv/mon-suivi-justice-public",
-            image_path_in_repo,
-            "Updating image",
+            image_repo_path,
+            "Updating image for #{@org_name}",
             existing_image[:sha],
-            stringified_picture,
-            {branch: "add-spipXX"})
+            org_image,
+            {branch: "add-#{@org_name}-page"})
         end
       
         reponse = @gh_api_client.last_response
-        temp_picture.unlink
+
+        # ensures file is removed from disk
+        temp_image.unlink
       end
 
       def handle_template
-
-        # spina_view_erb = Tempfile.new('spip-test.html.erb')
-
         template_content = File.open("./app/views/admin/public_pages/templates/preparer_page_template.html.erb").read
 
         Tempfile.create('spina_view') do |f|
-
-          new_content = template_content.gsub(/\bplaceholder.jpg\b/, "pouet-pouet-mon-image.jpg")
-
+          new_content = template_content.gsub(/\bplaceholder.jpg\b/, @image_filename)
           f.write(new_content)
           f.rewind
-
-          @toto = f.read
-
 
           view_path_in_repo = "app/views/pages/preparer_test.html.erb"
 
           begin
-            existing_view = @gh_api_client.contents("betagouv/mon-suivi-justice-public", {path: view_path_in_repo, ref: @sha})
+            existing_view = @gh_api_client.contents("betagouv/mon-suivi-justice-public", {path: view_path_in_repo, ref: @new_page_branch})
             
           rescue Octokit::NotFound
             @gh_api_client.create_contents("betagouv/mon-suivi-justice-public",
               view_path_in_repo,
-              "Adding spina view page for organization",
-              @toto,
-              {branch: "add-spipXX"})
+              "Adding spina view page for for #{@org_name}",
+              f.read,
+              {branch: "add-#{@org_name}-page"})
           else
+            # TODO: maybe do nothing here and instead tell the user that the organization already exists ?
             @gh_api_client.update_contents("betagouv/mon-suivi-justice-public",
               view_path_in_repo,
-              "Updating spina view page for organization",
+              "Updating spina view page for for #{@org_name}",
               existing_view[:sha],
-              @toto,
-              {branch: "add-spipXX"})
+              f.read,
+              {branch: "add-#{@org_name}-page"})
           end
-
-
-
-
-          #debugger
           f.close
        end
 
