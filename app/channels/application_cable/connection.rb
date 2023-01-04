3e603a8b4ae30e79a4cda9bdfe39b6b12502bb45
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-01-04 16:01:34 +0100

    linting

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -10,15 +10,13 @@ module ApplicationCable
     protected
 
     def find_verified_user
-      begin
-        if (current_user = env['warden'].user)
-          current_user
-        else
-          reject_unauthorized_connection
-        end
-      rescue StandardError => e
+      if (current_user = env['warden'].user)
+        current_user
+      else
         reject_unauthorized_connection
       end
+    rescue StandardError
+      reject_unauthorized_connection
     end
   end
 end
