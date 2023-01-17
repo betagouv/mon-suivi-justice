54526c4180c5efa2b445f62c46eed801c0996af3
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-01-17 15:34:42 +0100

    revert migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_13_104038) do
+ActiveRecord::Schema.define(version: 2023_01_13_103520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,7 +124,6 @@ ActiveRecord::Schema.define(version: 2023_01_13_104038) do
     t.boolean "case_prepared", default: false, null: false
     t.bigint "inviter_user_id"
     t.bigint "creating_organization_id"
-    t.json "organization_specific_data"
     t.index ["convict_id"], name: "index_appointments_on_convict_id"
     t.index ["creating_organization_id"], name: "index_appointments_on_creating_organization_id"
     t.index ["slot_id"], name: "index_appointments_on_slot_id"
@@ -250,7 +249,6 @@ ActiveRecord::Schema.define(version: 2023_01_13_104038) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "organization_type", default: 0
     t.string "time_zone", default: "Europe/Paris", null: false
-    t.json "appointment_added_fields", default: "{}", null: false
     t.index ["name"], name: "index_organizations_on_name", unique: true
   end
 
