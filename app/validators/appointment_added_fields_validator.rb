c7a8dbca06e6889c67bed994cc1a69c271eb58db
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-01-20 14:32:19 +0100

    change input type depending on the selected option

--- app/validators/appointment_added_fields_validator.rb
+++
@@ -1,7 +0,0 @@
-class AppointmentAddedFieldsValidator < ActiveModel::Validator
-  def validate(record)
-    if record.appointment_added_fields.keys().size > 3
-      record.errors.add :appointment_added_fields, "can't have more than 3 extra fields"
-    end
-  end
-end
