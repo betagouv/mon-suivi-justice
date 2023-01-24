dc6b4144488931eadff9de95bda10c516133b999
Charles Marcoin <charles.marcoin@gmail.com>
2023-01-24 15:02:54 +0100

    added dashboards to administrate cities, tjs and spips

--- app/dashboards/city_dashboard.rb
+++ app/dashboards/city_dashboard.rb
@@ -1,6 +1,6 @@
-require 'administrate/base_dashboard'
+require "administrate/base_dashboard"
 
-class SlotDashboard < Administrate::BaseDashboard
+class CityDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
   # a hash that describes the type of each of the model's fields.
   #
@@ -9,20 +9,14 @@ class SlotDashboard < Administrate::BaseDashboard
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     id: Field::Number,
-    agenda: Field::BelongsTo,
-    appointment_type: Field::BelongsTo,
-    appointments: Field::HasMany,
-    available: Field::Boolean,
-    capacity: Field::Number,
-    date: Field::Date,
-    duration: Field::Number,
-    full: Field::Boolean,
-    slot_type: Field::BelongsTo,
-    starting_time: Field::Time,
-    used_capacity: Field::Number,
-    versions: Field::HasMany,
+    city_id: Field::String,
+    code_insee: Field::String,
+    name: Field::String,
+    spip: Field::BelongsTo,
+    tj: Field::BelongsTo,
+    zipcode: Field::String,
     created_at: Field::DateTime,
-    updated_at: Field::DateTime
+    updated_at: Field::DateTime,
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -32,27 +26,22 @@ class SlotDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
     id
-    agenda
-    appointment_type
-    appointments
+    zipcode
+    name
+    tj
+    spip
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
     id
-    agenda
-    appointment_type
-    appointments
-    available
-    capacity
-    date
-    duration
-    full
-    slot_type
-    starting_time
-    used_capacity
-    versions
+    city_id
+    code_insee
+    name
+    spip
+    tj
+    zipcode
     created_at
     updated_at
   ].freeze
@@ -61,15 +50,8 @@ class SlotDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    agenda
-    appointment_type
-    available
-    capacity
-    date
-    duration
-    full
-    starting_time
-    used_capacity
+    spip
+    tj
   ].freeze
 
   # COLLECTION_FILTERS
@@ -84,10 +66,10 @@ class SlotDashboard < Administrate::BaseDashboard
   #   }.freeze
   COLLECTION_FILTERS = {}.freeze
 
-  # Overwrite this method to customize how slots are displayed
+  # Overwrite this method to customize how cities are displayed
   # across all pages of the admin dashboard.
   #
-  def display_resource(slot)
-    "#{slot.date} #{slot.localized_time.to_s(:time)}"
+  def display_resource(city)
+    "#{city.name} - #{city.zipcode}"
   end
 end
