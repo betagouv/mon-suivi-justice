b6868f819f8b853bd773fbcaeb2fb4d096efdae3
Charles Marcoin <charles.marcoin@gmail.com>
2023-02-01 15:46:53 +0100

    added a cities controller

---
+++ app/controllers/cities_controller.rb
@@ -0,0 +1,8 @@
+class CitiesController < ApplicationController
+    before_action :authenticate_user!
+
+    def services
+        debugger
+    end
+
+end
--- app/javascript/components/city_selector.js
+++ app/javascript/components/city_selector.js
@@ -21,7 +21,22 @@ $(document).on('select2:open', () => {
 // https://select2.org/programmatic-control/events#limiting-the-scope-of-the-change-event
 $(document).on("select2:select", function (e) {
 
-  // Make ajax call the tj and spips here ?
+  // Make ajax call the tj and spips here ?var menuId = $( "ul.nav" ).first().attr( "id" );
+  var request = $.ajax({
+    url: "script.php",
+    method: "POST",
+    data: { city_id : e.target.value },
+    dataType: "html"
+  });
+  
+  request.done(function( msg ) {
+    console.log("retour rails", msg)
+    $( "#log" ).html( msg );
+  });
+  
+  request.fail(function( jqXHR, textStatus ) {
+    alert( "Request failed: " + textStatus );
+  });
 
 
   $("#city-organizations").append( `<strong>Le SPIP et le TJ ${e.target.value}</strong>` );
