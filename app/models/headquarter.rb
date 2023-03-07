5b1f1c6d303b0aecd0dceedfe1ae3545798a000a
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-03-07 14:27:19 +0100

    nullify headquarter_id for user and organization when headquarter is deleted

--- app/models/headquarter.rb
+++ app/models/headquarter.rb
@@ -1,5 +1,5 @@
 class Headquarter < ApplicationRecord
-  has_many :organizations
-  has_many :users
+  has_many :organizations, dependent: :nullify
+  has_many :users, dependent: :nullify
   validates :name, presence: true
 end
