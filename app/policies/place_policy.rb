7c1cac5f759470d03d884a02258b53e79ff4e97b
Charles Marcoin <charles.marcoin@gmail.com>
2023-03-09 15:47:47 +0100

    fix place policy

--- app/policies/place_policy.rb
+++ app/policies/place_policy.rb
@@ -3,7 +3,7 @@ class PlacePolicy < ApplicationPolicy
 
   class Scope < Scope
     def resolve
-      if user.admin? || user.local_admin? || user.work_at_bex? || user.role = 'greff_sap'
+      if user.admin? || user.local_admin? || user.work_at_bex? || user.role == 'greff_sap'
         scope.in_dep_spips(user.organization.departments).or(scope.in_organization(user.organization)).distinct
       else
         scope.in_organization(user.organization)
