b695d0c5b879897ce07a39497b71628ef93552aa
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-03-16 15:37:59 +0100

    desactivate actioncable for now (#249)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// import "channels"
 
 require("jquery");
 require("@nathanvda/cocoon");
