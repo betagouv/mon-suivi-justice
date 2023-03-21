cd10a7e2c3de246868d6939ca64f99e2c7c8fa9a
Charles Marcoin <charles.marcoin@gmail.com>
2023-03-21 16:22:14 +0100

    Feature/rename tjs and spips to srj tjs and spips (#254)

--- app/models/spip.rb
+++
@@ -1,4 +0,0 @@
-class Spip < ApplicationRecord
-  belongs_to :organization, optional: true
-  has_one :city
-end
--- app/models/tj.rb
+++
@@ -1,8 +0,0 @@
-class Tj < ApplicationRecord
-  belongs_to :organization, optional: true
-  has_one :city
-
-  def short_name
-    name.gsub(%r{Tribunal judiciaire / Tribunal de Grande Instance}, 'TJ')
-  end
-end
