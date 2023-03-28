b4f2e783d576e658e1a01a22f903e51afe66170c
Charles Marcoin <charles.marcoin@gmail.com>
2023-03-28 09:15:23 +0200

    Feature/update tests suite 3 (#257)
    - Fixed all appointments tests
    - Added ajax search to administrate
    - Fixed "clean and reseed" feature in admin
    - New trait for user factory (adds the user to an organization of the
    chosen type)
    - New user creation and login methods for tests
    - Flash messages fixes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -124,8 +124,8 @@ def create_cpip_user_and_login
   @cpip
 end
 
-def create_user_and_login(role)
-  @user = create(:user, role: role)
+def create_user_and_login(role, org_type)
+  @user = create(:user, :in_organization, type: org_type, role: role)
   login_as(@user)
   @user
 end
