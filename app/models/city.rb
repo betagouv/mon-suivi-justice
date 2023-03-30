3ea1ebcd78b26caf1dce35ced0e3830e08534fc8
Charles Marcoin <charles.marcoin@gmail.com>
2023-03-30 11:48:20 +0200

    Feature/inter ressort demo fixes (#261)

--- app/models/city.rb
+++ app/models/city.rb
@@ -5,4 +5,8 @@ class City < ApplicationRecord
   scope :with_at_least_one_tj, -> { joins(:srj_tj).where('srj_tjs.organization_id IS NOT NULL') }
   scope :with_at_least_one_spip, -> { joins(:srj_spip).where('srj_spips.organization_id IS NOT NULL') }
   scope :with_at_least_one_service, -> { (with_at_least_one_tj + with_at_least_one_spip).uniq }
+
+  def organizations
+    [srj_tj&.organization, srj_spip&.organization]
+  end
 end
