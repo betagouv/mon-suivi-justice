783b7faeab6bd13b94e498234e2b89caac8b6dd6
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-05-16 10:33:24 +0200

    hide user info for appointments for non spip users (#305)
    related to
    https://www.notion.so/monsuivijustice/Mise-jour-des-tests-rspec-21ee1943d0b74c35949dc0d18d0d1767?pvs=4
    <img width="1324" alt="Image collée à 2023-5-16 10-22"
    src="https://github.com/betagouv/mon-suivi-justice/assets/4519374/bcca1855-6034-441d-ba88-57791d118323">

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,7 +4,7 @@ class AppointmentsController < ApplicationController
   def index
     @q = policy_scope(Appointment).active.ransack(params[:q])
     @all_appointments = @q.result(distinct: true)
-                          .joins(:convict, :user, slot: [:appointment_type, { agenda: [:place] }])
+                          .joins(:convict, slot: [:appointment_type, { agenda: [:place] }])
                           .includes(:convict, :user, slot: [:appointment_type, { agenda: [:place] }])
                           .order('slots.date ASC, slots.starting_time ASC')
 
