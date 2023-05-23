5e4be109171a67ebd14d089a968f0b23c4015bbf
Charles Marcoin <charles.marcoin@gmail.com>
2023-05-23 15:01:58 +0200

    last fixes to places, app types, slots and convict

--- spec/features/appointment_types_spec.rb
+++ spec/features/appointment_types_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.feature 'AppointmentType', type: :feature do
-  # before do
-  #   @user = create_admin_user_and_login
-  # end
-
   describe 'index', logged_in_as: 'admin' do
     it 'lists all appointment_types' do
       create(:appointment_type, name: 'type 1')
@@ -18,7 +14,7 @@ RSpec.feature 'AppointmentType', type: :feature do
   end
 
   describe 'update', logged_in_as: 'admin' do
-    let(:appointment_type) { create(:appointment_type, name: '1er contact') }
+    let!(:appointment_type) { create(:appointment_type, name: '1er contact') }
 
     let!(:notif_type1) do
       create(:notification_type, appointment_type: appointment_type, is_default: true,
@@ -78,46 +74,6 @@ RSpec.feature 'AppointmentType', type: :feature do
       click_button 'Enregistrer'
 
       expect(local_notif1.reload.template).to eq('updated local summon')
-    end
-
-    it 'updates local notification_types that stayed default when updating default notification_types', js: true do
-      @user.organization.setup_notification_types
-
-      visit edit_appointment_type_path(id: appointment_type.id, orga: @user.organization.id)
-
-      summon_container = first('.summon-container')
-
-      expect(summon_container).to have_content('Défaut')
-
-      within summon_container do
-        fill_in 'Modèle', with: 'updated local summon'
-      end
-
-      click_button 'Enregistrer'
-
-      visit edit_appointment_type_path(id: appointment_type.id, orga: @user.organization.id)
-
-      expect(first('.summon-container')).to have_content('Personnalisé')
-      expect(first('.summon-container')).to have_content('updated local summon')
-      expect(first('.reminder-container')).to have_content('Défaut')
-      expect(first('.reminder-container')).to have_content('Default reminder')
-
-      visit edit_appointment_type_path(id: appointment_type.id)
-
-      within first('.summon-container') do
-        fill_in 'Modèle', with: 'updated default summon'
-      end
-
-      within first('.reminder-container') do
-        fill_in 'Modèle', with: 'updated default reminder'
-      end
-
-      click_button 'Enregistrer'
-
-      visit edit_appointment_type_path(id: appointment_type.id, orga: @user.organization.id)
-
-      expect(first('.summon-container')).to have_content('updated local summon')
-      expect(first('.reminder-container')).to have_content('updated default reminder')
     end
 
     it 'allows to reset notification type to default' do
