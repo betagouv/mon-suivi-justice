cffb97de565ef028575653a37365767332b96882
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-06-01 15:08:48 +0200

    setup job to dup agendas and move slots

--- app/controllers/admin/place_transferts_controller.rb
+++ app/controllers/admin/place_transferts_controller.rb
@@ -13,6 +13,7 @@ module Admin
       authorize_resource(resource)
 
       if resource.save
+        PreparePlaceTransfertJob.perform_later(resource)
         redirect_to(
           after_resource_created_path(resource),
           notice: translate_with_resource('create.success')
--- app/jobs/transfert_places_job.rb
+++ app/jobs/transfert_places_job.rb
@@ -5,10 +5,7 @@ class TransfertPlacesJob < ApplicationJob
     @new_place = @transfert_place.new_place
 
     p old_place: @old_place, new_place: @new_place
-    # move appointments from old place to new place after transfert date
-    # Les créneaux dans l’ancien lieu après la date de déménagement sont supprimés et la création de nouveaux créneaux après cette date est bloquée
-    # La création de RDVs (notamment suivi SPIP) dans l’ancien lieu après la date de déménagement est bloquée
     # Le contenu des notifications SMS déjà prévues pour les RDVs qui auront lieu après le déménagement est modifié pour indiquer l’adresse du nouveau lieu
-    # Après le déménagement, les anciens lieux du SPIP 78 sont archivés
+    # les anciens lieux du SPIP 78 sont archivés
   end
 end
