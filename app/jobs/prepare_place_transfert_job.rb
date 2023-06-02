afe6dcf83ccba81ae3a3b1c22c19d7e90b221c92
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-06-02 14:43:45 +0200

    add notification

--- app/jobs/prepare_place_transfert_job.rb
+++ app/jobs/prepare_place_transfert_job.rb
@@ -30,15 +30,33 @@ class PreparePlaceTransfertJob < ApplicationJob
     end
   end
 
-  def transfert_slots(_old_agenda, _new_agenda, date)
-    agenda.slots.where('date >= ?', date).update_all(agenda_id: new_place_agenda.id)
+  def transfert_slots(old_place_agenda, new_place_agenda, date)
+    old_slots = old_place_agenda.slots.where('date >= ?', date)
+    old_slots.update_all(agenda_id: new_place_agenda.id)
 
-    agenda.slot_types.each do |slot_type|
+    old_place_agenda.slot_types.each do |slot_type|
       new_slot_type = slot_type.dup
       new_slot_type.agenda = new_place_agenda
       new_slot_type.save
     end
 
+    update_notifications_text(old_place_agenda, new_place_agenda, old_slots)
+
     @transfert_successes.push("Les créneaux de l'agenda #{agenda.name} ont été transférés avec succès")
+  end
+
+  def update_notifications_text(old_place_agenda, new_place_agenda, old_slots)
+    old_place = old_place_agenda.place
+    new_place = new_place_agenda.place
+
+    old_slots.each do |slot|
+      slot.appointments.each do |appointment|
+        appointment.notifications.where(state: %w[programmed created]).update_all(
+          content: ActiveRecord::Base.connection.quote_string(
+            notification.content.gsub(old_place.name, new_place.name).gsub(old_place.address, new_place.address)
+          )
+        )
+      end
+    end
   end
 end
