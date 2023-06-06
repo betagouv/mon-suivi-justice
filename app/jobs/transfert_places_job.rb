df232b8ef4d49e07791a8bed972e11523f39c109
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-06-06 11:48:04 +0200

    fix job

--- app/jobs/transfert_places_job.rb
+++ app/jobs/transfert_places_job.rb
@@ -4,7 +4,12 @@ class TransfertPlacesJob < ApplicationJob
 
     place_transferts.each do |transfert_place|
       p "TransfertPlacesJob: archive place: #{transfert_place.old_place.name}"
-      archive_place(transfert_place.old_place)
+
+      transfert_place.old_place.discard
+      transfert_place.old_place.agendas.discard_all
+      transfert_place.old_place.agendas.each do |agenda|
+        agenda.slot_types.discard_all
+      end
     end
   end
 end
