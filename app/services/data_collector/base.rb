1e295df59c5019a55f7167747b55fc5ffc89001e
Charles Marcoin <charles.marcoin@gmail.com>
2023-06-07 15:54:56 +0200

    Feature/fix perf issue (#325)
    relates to
    https://www.notion.so/monsuivijustice/Retravailler-la-performance-de-la-page-d-accueil-b1fbed1a09b245e7bf0489854bc0e2dd?pvs=4
    
    <img width="820" alt="Capture d’écran 2023-06-07 à 14 49 47"
    src="https://github.com/betagouv/mon-suivi-justice/assets/25039335/e494d4ca-e2b4-4e44-8e16-3bee7ef46450">

--- app/services/data_collector/base.rb
+++ app/services/data_collector/base.rb
@@ -1,7 +1,8 @@
 module DataCollector
   class Base
-    def initialize(organization_id: nil)
+    def initialize(organization_id: nil, display_notifications: true)
       @organization = Organization.find(organization_id) if organization_id.present?
+      @display_notifications = display_notifications
     end
   end
 end
