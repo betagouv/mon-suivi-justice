3d2baee5c9fe6557db68e51c896f03dfa1217ca9
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-06-14 11:26:42 +0200

    prevent the place_transfert date to be in the past

--- app/models/place_transfert.rb
+++ app/models/place_transfert.rb
@@ -6,4 +6,13 @@ class PlaceTransfert < ActiveRecord::Base
 
   validates :date, presence: true
   validates :date, uniqueness: { scope: %i[new_place_id old_place_id] }
+  validate :in_the_future
+
+  private
+
+  def in_the_future
+    return if date >= Time.zone.tomorrow
+
+    errors.add(:date, I18n.t('activerecord.errors.models.place_transfert.attributes.date.past'))
+  end
 end
