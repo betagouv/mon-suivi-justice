d7675681887750a1480b46dd49de9964b9218486
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-06-14 16:06:55 +0200

    Improve TransfertPlaceJob

--- app/jobs/transfert_places_job.rb
+++ app/jobs/transfert_places_job.rb
@@ -1,15 +1,20 @@
 class TransfertPlacesJob < ApplicationJob
   def perform
-    place_transferts = PlaceTransfert.where('date = ?', Date.today)
+    place_transferts = PlaceTransfert.where('date <= ? AND status = ?', Date.today, 'transfert_pending')
 
     place_transferts.each do |transfert_place|
-      p "TransfertPlacesJob: archive place: #{transfert_place.old_place.name}"
+      archive_place(transfert_place.old_place)
+      transfert_place.status = 'transfert_done'
+    end
+  end
 
-      transfert_place.old_place.discard
-      transfert_place.old_place.agendas.discard_all
-      transfert_place.old_place.agendas.each do |agenda|
-        agenda.slot_types.discard_all
-      end
+  def archive_place(place)
+    p "TransfertPlacesJob: archive place: #{place.name}"
+
+    place.discard
+    place.agendas.discard_all
+    place.agendas.each do |agenda|
+      agenda.slot_types.discard_all
     end
   end
 end
