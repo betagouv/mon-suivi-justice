587e1bdb11b2ac6cf2c6798701c204a518ea5d8e
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-06-16 10:15:47 +0200

    fix transfert

--- app/jobs/transfert_places_job.rb
+++ app/jobs/transfert_places_job.rb
@@ -1,10 +1,11 @@
 class TransfertPlacesJob < ApplicationJob
   def perform
-    place_transferts = PlaceTransfert.where('date <= ? AND status = ?', Date.today, 'transfert_pending')
+    place_transferts = PlaceTransfert.where('date <= ? AND status = ?', Date.today, 0)
 
     place_transferts.each do |transfert_place|
       archive_place(transfert_place.old_place)
       transfert_place.status = 'transfert_done'
+      transfert_place.save(validate: false)
     end
   end
 
