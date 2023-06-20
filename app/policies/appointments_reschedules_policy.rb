526e5a00cc59a5b4063ae7bee6c81b28ffd7cd8d
Charles Marcoin <charles.marcoin@gmail.com>
2023-06-20 21:07:05 +0200

    users working at the spip should also be able to reschedule. removing all role related conditions

--- app/policies/appointments_reschedules_policy.rb
+++ app/policies/appointments_reschedules_policy.rb
@@ -1,15 +1,9 @@
 class AppointmentsReschedulesPolicy < ApplicationPolicy
   def new?
-    user_role_allowed? && user.organization.in?([record.organization, record.creating_organization])
+    user.organization.in?([record.organization, record.creating_organization])
   end
 
   def create?
     new?
-  end
-
-  private
-
-  def user_role_allowed?
-    user.local_admin? || user.work_at_bex? || user.work_at_sap?
   end
 end
