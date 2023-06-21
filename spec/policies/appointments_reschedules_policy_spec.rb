d485e7523bcd408f4692964b075b607b9f193e30
Charles Marcoin <charles.marcoin@gmail.com>
2023-06-21 10:35:10 +0200

    added test

--- spec/policies/appointments_reschedules_policy_spec.rb
+++ spec/policies/appointments_reschedules_policy_spec.rb
@@ -1,45 +1,23 @@
 require 'rails_helper'
 
 describe AppointmentsReschedulesPolicy do
-  User.roles.keys.delete_if { |r| %(cpip psychologist overseer local_admin).include? r }.each do |role|
-    context "for a #{role} user" do
-      org_type = %w[educator dpip secretary_spip].include?(role) ? 'spip' : 'tj'
+  let(:appointment_type) { create(:appointment_type) }
+  let(:slot) { create :slot, :without_validations, appointment_type: appointment_type }
+  let(:appointment) { create(:appointment, slot: slot) }
 
-      let(:user) { build(:user, :in_organization, type: org_type, role: role) }
-      let(:scope) { Users::AppointmentPolicy::Scope.new(user, Appointment).resolve }
+  User.roles.each_key do |role|
+    context "for a #{role} user which belongs to the same organization as the appointment's" do
+      let(:user) { build(:user, organization: slot.agenda.place.organization, role: role) }
+      subject { AppointmentsReschedulesPolicy.new(user, appointment) }
 
-      it 'Policy scope should raise an error' do
-        expect { scope }.to raise_error(Pundit::NotAuthorizedError)
-      end
+      it { is_expected.to permit_action(:new) }
     end
 
-    context 'for a tj local_admin user' do
-      let(:user) { build(:user, :in_organization, type: 'tj', role: 'local_admin') }
-      let(:scope) { Users::AppointmentPolicy::Scope.new(user, Appointment).resolve }
+    context "for a #{role} user which does not belong to the same organization as the appointment's" do
+      let(:user) { build(:user, :in_organization, role: role) }
+      subject { AppointmentsReschedulesPolicy.new(user, appointment) }
 
-      it 'Policy scope should raise an error' do
-        expect { scope }.to raise_error(Pundit::NotAuthorizedError)
-      end
-    end
-
-    context 'for a spip local_admin user' do
-      let(:user) { build(:user, :in_organization, type: 'spip', role: 'local_admin') }
-      let(:scope) { Users::AppointmentPolicy::Scope.new(user, Appointment).resolve }
-
-      it 'Policy scope should raise an error' do
-        expect { scope }.to raise_error(Pundit::NotAuthorizedError)
-      end
-    end
-  end
-
-  %w[cpip psychologist overseer].each do |role|
-    context "for a #{role} user" do
-      let(:user) { create(:user_with_appointments, :in_organization, role: role) }
-      let(:scope) { Users::AppointmentPolicy::Scope.new(user, Appointment).resolve }
-
-      it 'Policy scope should return proper appointments' do
-        expect(scope.to_a).to match_array(user.appointments)
-      end
+      it { is_expected.not_to permit_action(:new) }
     end
   end
 end
