0cedec3514ca2673e937b104256c08ae4636277f
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-07-07 17:58:55 +0200

    setup bordeaux and cleanup seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,193 +3,26 @@ Dir[Rails.root.join('db/seeds/*.rb')].sort.each do |file|
   puts "Processing #{file.split('/').last}"
   require file
 end
-org_spip_92 = Organization.create!(name: 'SPIP 92', organization_type: 'spip')
-org_tj_nanterre = Organization.create!(name: 'TJ Nanterre', organization_type: 'tj', jap_modal_content: '<b>Pouet</b>')
-org_spip_75 = Organization.create!(name: 'SPIP 75', organization_type: 'spip')
-org_spip_77 = Organization.create!(name: 'SPIP 77', organization_type: 'spip')
-org_tj_melun = Organization.create!(name: 'TJ Melun', organization_type: 'tj')
-org_tj_fontainebleau = Organization.create!(name: 'TJ de Fontainebleau', organization_type: 'tj')
-org_tj_paris = Organization.create!(name: 'TJ Paris', organization_type: 'tj')
+
+org_tj_paris = Organization.find_or_create_by!(name: 'TJ Paris', organization_type: 'tj')
+
+User.find_or_create_by!(
+  organization: org_tj_paris, email: 'bextjparis@example.com', role: :bex, first_name: 'Stanislas', last_name: 'Wawrinka'
+) do |user|
+  user.password = '1mot2passeSecurise!'
+  user.password_confirmation = '1mot2passeSecurise!'
+end
+
+User.find_or_create_by!(
+  organization: org_tj_paris, email: 'admin@example.com', role: :admin, first_name: 'Kevin', last_name: 'McCallister'
+) do |user|
+  user.password = '1mot2passeSecurise!'
+  user.password_confirmation = '1mot2passeSecurise!'
+end
 
 
-convict_1 = Convict.create!(first_name: "Michel", last_name: "Blabla", phone: "0677777777", appi_uuid: "12345", date_of_birth: "02/01/1986", organizations: [org_spip_92])
-convict_2 = Convict.create!(first_name: "Dark", last_name: "Vador", phone: "0600000000", appi_uuid: "12346", date_of_birth: "02/01/1986", organizations: [org_spip_92, org_tj_nanterre])
-convict_3 = Convict.create!(first_name: "Bobba", last_name: "Smet", phone: "0611111111", appi_uuid: "12347", date_of_birth: "02/01/1986", organizations: [org_spip_75])
-convict_4 = Convict.create!(first_name: "Conor", last_name: "McGregor", phone: "0611111112", appi_uuid: "12348", date_of_birth: "02/01/1986", organizations: [org_spip_77, org_tj_melun])
-convict_5 = Convict.create!(first_name: "Georges", last_name: "Saint-Pierre", phone: "0611111113", appi_uuid: "12349", date_of_birth: "02/01/1986", organizations: [org_spip_77, org_tj_fontainebleau])
-convict_6 = Convict.create!(first_name: "John", last_name: "Jones", no_phone: true, appi_uuid: "123410", organizations: [org_spip_92, org_tj_nanterre], date_of_birth: "02/01/1986")
-convict_7 = Convict.create!(first_name: "Dark", last_name: "Vador", no_phone: true, appi_uuid: "123411", organizations: [org_spip_92, org_tj_nanterre], date_of_birth: "02/01/1986")
 
-User.create!(
-  organization: org_spip_92, email: 'admin@example.com', password: '1mot2passeSecurise!',
-  password_confirmation: '1mot2passeSecurise!', role: :admin, first_name: 'Kevin', last_name: 'McCallister'
-)
 
-User.create!(
-  organization: org_spip_92, email: 'cpip@example.com', password: '1mot2passeSecurise!',
-  password_confirmation: '1mot2passeSecurise!', role: :cpip, first_name: 'Bob', last_name: 'Dupneu'
-)
 
-User.create!(
-  organization: org_spip_75, email: 'cpip75@example.com', password: '1mot2passeSecurise!',
-  password_confirmation: '1mot2passeSecurise!', role: :cpip, first_name: 'Luke', last_name: 'Skywalker'
-)
-
-User.create!(
-  organization: org_spip_75, email: 'localadminSpip75@example.com', password: '1mot2passeSecurise!',
-  password_confirmation: '1mot2passeSecurise!', role: :local_admin, first_name: 'Matthieu', last_name: 'Paris'
-)
-
-User.create!(
-  organization: org_spip_92, email: 'localadminSpip92@example.com', password: '1mot2passeSecurise!',
-  password_confirmation: '1mot2passeSecurise!', role: :local_admin, first_name: 'Henri', last_name: 'HautsDeSeine'
-)
-
-User.create!(
-  organization: org_tj_nanterre, email: 'localadmintjnanterre@example.com', password: '1mot2passeSecurise!',
-  password_confirmation: '1mot2passeSecurise!', role: :local_admin, first_name: 'André', last_name: 'Dussolier'
-)
-
-User.create!(
-  organization: org_tj_nanterre, email: 'bexnanterre@example.com', password: '1mot2passeSecurise!',
-  password_confirmation: '1mot2passeSecurise!', role: :bex, first_name: 'Max', last_name: 'Verstappen'
-)
-
-User.create!(
-  organization: org_tj_melun, email: 'localadmintjmelun@example.com', password: '1mot2passeSecurise!',
-  password_confirmation: '1mot2passeSecurise!', role: :local_admin, first_name: 'Michel', last_name: 'Melun'
-)
-
-User.create!(
-  organization: org_tj_fontainebleau, email: 'localadmintjfontainebleau@example.com', password: '1mot2passeSecurise!',
-  password_confirmation: '1mot2passeSecurise!', role: :local_admin, first_name: 'Robert', last_name: 'Fontaine'
-)
-
-User.create!(
-  organization: org_tj_fontainebleau, email: 'bextjfontainebleau@example.com', password: '1mot2passeSecurise!',
-  password_confirmation: '1mot2passeSecurise!', role: :bex, first_name: 'Cyril', last_name: 'Gane'
-)
-
-User.create!(
-  organization: org_spip_77, email: 'localadminSpip77@example.com', password: '1mot2passeSecurise!',
-  password_confirmation: '1mot2passeSecurise!', role: :local_admin, first_name: 'Flash', last_name: 'McQueen'
-)
-
-User.create!(
-  organization: org_spip_77, email: 'cpip77@example.com', password: '1mot2passeSecurise!',
-  password_confirmation: '1mot2passeSecurise!', role: :cpip, first_name: 'Henri', last_name: 'Vasnier'
-)
-
-User.create!(
-  organization: org_tj_melun, email: 'bextjmelun@example.com', password: '1mot2passeSecurise!',
-  password_confirmation: '1mot2passeSecurise!', role: :bex, first_name: 'Roger', last_name: 'Federer'
-)
-
-User.create!(
-  organization: org_tj_paris, email: 'bextjparis@example.com', password: '1mot2passeSecurise!',
-  password_confirmation: '1mot2passeSecurise!', role: :bex, first_name: 'Stanislas', last_name: 'Wawrinka'
-)
-
-place_tj_nanterre = Place.create!(
-  organization: org_tj_nanterre, name: "Tribunal judiciaire de Nanterre", adress: "179-191 av. Joliot Curie, 92020 NANTERRE", phone: '0606060606'
-)
-
-place_tj_melun = Place.create!(
-  organization: org_tj_melun, name: "Tribunal judiciaire de Melun", adress: "2 Av. du Général Leclerc, 77010 Melun", phone: '0606060606'
-)
-
-place_tj_fontainebleau = Place.create!(
-  organization: org_tj_fontainebleau, name: "Tribunal judiciaire de Fontainebleau", adress: "159 Rue Grande, 77300 Fontainebleau", phone: '0606060606'
-)
-
-place_spip_75 = Place.create!(
-  organization: org_spip_75, name: "SPIP 75", adress: "12-14 rue Charles Fourier, 75013 PARIS", phone: '0606060606'
-)
-
-place_spip_77 = Place.create!(
-  organization: org_spip_77, name: "SPIP 77", adress: "5 rue de la montagne du Mée, 77000 MELUN", phone: '0606060606'
-)
-
-place_spip_77_permanence_tj = Place.create!(
-  organization: org_spip_77, name: "Permanence TJ Fontainebleau", adress: "5 rue de la montagne du Mée, 77000 MELUN", phone: '0606060606'
-)
-
-place_spip_92 = Place.create!(organization: org_spip_92, name: "SPIP 92", adress: "94 Boulevard du Général Leclerc, 92000 Nanterre", phone: '0606060606')
-
-agenda_tj_nanterre = Agenda.create!(place: place_tj_nanterre, name: "Agenda 1 tribunal Nanterre")
-agenda_tj_nanterre_2 = Agenda.create!(place: place_tj_nanterre, name: "Agenda 2 tribunal Nanterre")
-
-agenda_tj_melun = Agenda.create!(place: place_tj_melun, name: "Agenda 1 tribunal Melun")
-agenda_tj_fontainebleau = Agenda.create!(place: place_tj_fontainebleau, name: "Agenda 1 tribunal Fontainebleau")
-
-agenda_spip_75 = Agenda.create!(place: place_spip_75, name: "Agenda SPIP 75")
-agenda_spip_92 = Agenda.create!(place: place_spip_92, name: "Agenda SPIP 92")
-agenda_spip_77 = Agenda.create!(place: place_spip_77, name: "Agenda SPIP 77")
-agenda_spip_77_permanence_tj = Agenda.create!(place: place_spip_77_permanence_tj, name: "Permanence TJ Fontainebleau")
-agenda_spip_77_permanence_tj_2 = Agenda.create!(place: place_spip_77_permanence_tj, name: "Permanence TJ Fontainebleau 2")
-
-apt_type_sortie_audience_sap = AppointmentType.find_or_create_by!(name: "Sortie d'audience SAP")
-apt_type_rdv_suivi_jap = AppointmentType.find_or_create_by!(name: 'Convocation de suivi JAP')
-apt_type_rdv_suivi_spip = AppointmentType.find_or_create_by!(name: "Convocation de suivi SPIP")
-apt_type_sortie_audience_spip = AppointmentType.find_or_create_by!(name: "Sortie d'audience SPIP")
-apt_type_sap_ddse = AppointmentType.find_or_create_by!(name: "SAP DDSE")
-
-PlaceAppointmentType.create!(place: place_tj_nanterre, appointment_type: apt_type_sortie_audience_sap)
-PlaceAppointmentType.create!(place: place_tj_nanterre, appointment_type: apt_type_convocation_suivi_jap)
-PlaceAppointmentType.create!(place: place_spip_92, appointment_type: apt_type_convocation_suivi_spip)
-PlaceAppointmentType.create!(place: place_spip_61_alencon, appointment_type: apt_type_convocation_suivi_spip)
-PlaceAppointmentType.create!(place: place_spip_61_argentan, appointment_type: apt_type_convocation_suivi_spip)
-PlaceAppointmentType.create!(place: place_spip_92, appointment_type: apt_type_sortie_audience_spip)
-PlaceAppointmentType.create!(place: place_spip_92, appointment_type: apt_type_sap_ddse)
-
-PlaceAppointmentType.create!(place: place_tj_melun, appointment_type: apt_type_convocation_suivi_jap)
-PlaceAppointmentType.create!(place: place_tj_melun, appointment_type: apt_type_sortie_audience_sap)
-PlaceAppointmentType.create!(place: place_tj_fontainebleau, appointment_type: apt_type_convocation_suivi_jap)
-PlaceAppointmentType.create!(place: place_tj_fontainebleau, appointment_type: apt_type_sortie_audience_sap)
-PlaceAppointmentType.create!(place: place_spip_77, appointment_type: apt_type_sortie_audience_spip)
-PlaceAppointmentType.create!(place: place_spip_77_permanence_tj, appointment_type: apt_type_sortie_audience_spip)
-
-NotificationType.create!(appointment_type: apt_type_sortie_audience_sap, role: :summon, template: "Vous êtes convoqué, merci de venir.", is_default: true)
-NotificationType.create!(appointment_type: apt_type_sortie_audience_sap, role: :reminder, template: "RAPPEL Vous êtes convoqué, vraiment il faut venir.", reminder_period: :two_days, is_default: true)
-NotificationType.create!(appointment_type: apt_type_sortie_audience_sap, role: :cancelation, template: "Finalement non, c'est pas la peine.", is_default: true)
-NotificationType.create!(appointment_type: apt_type_sortie_audience_sap, role: :no_show, template: "Vous n'êtes pas venu :(", is_default: true)
-NotificationType.create!(appointment_type: apt_type_sortie_audience_sap, role: :reschedule, template: "Changement de la convocation de date X a date Y.", is_default: true)
-
-NotificationType.create!(appointment_type: apt_type_convocation_suivi_jap, role: :summon, template: "Vous êtes convoqué, merci de venir.", is_default: true)
-NotificationType.create!(appointment_type: apt_type_convocation_suivi_jap, role: :reminder, template: "RAPPEL Vous êtes convoqué, vraiment il faut venir.", reminder_period: :two_days, is_default: true)
-NotificationType.create!(appointment_type: apt_type_convocation_suivi_jap, role: :cancelation, template: "Finalement non, c'est pas la peine.", is_default: true)
-NotificationType.create!(appointment_type: apt_type_convocation_suivi_jap, role: :no_show, template: "Vous n'êtes pas venu :(", is_default: true)
-NotificationType.create!(appointment_type: apt_type_convocation_suivi_jap, role: :reschedule, template: "Changement de la convocation de date X a date Y.", is_default: true)
-
-NotificationType.create!(appointment_type: apt_type_convocation_suivi_spip, role: :summon, template: "Vous êtes convoqué, merci de venir.", is_default: true)
-NotificationType.create!(appointment_type: apt_type_convocation_suivi_spip, role: :reminder, template: "RAPPEL Vous êtes convoqué, vraiment il faut venir.", reminder_period: :two_days, is_default: true)
-NotificationType.create!(appointment_type: apt_type_convocation_suivi_spip, role: :cancelation, template: "Finalement non, c'est pas la peine.", is_default: true)
-NotificationType.create!(appointment_type: apt_type_convocation_suivi_spip, role: :no_show, template: "Vous n'êtes pas venu :(", is_default: true)
-NotificationType.create!(appointment_type: apt_type_convocation_suivi_spip, role: :reschedule, template: "Changement de la convocation de date X a date Y.", is_default: true)
-
-NotificationType.create!(appointment_type: apt_type_sortie_audience_spip, role: :summon, template: "Vous êtes convoqué à votre sortie d'audience SPIP, merci de venir.", is_default: true)
-NotificationType.create!(appointment_type: apt_type_sortie_audience_spip, role: :reminder, template: "RAPPEL Vous êtes convoqué à votre sortie d'audience SPIP, vraiment il faut venir.", reminder_period: :two_days, is_default: true)
-NotificationType.create!(appointment_type: apt_type_sortie_audience_spip, role: :cancelation, template: "Finalement non, c'est pas la peine.", is_default: true)
-NotificationType.create!(appointment_type: apt_type_sortie_audience_spip, role: :no_show, template: "Vous n'êtes pas venu :(", is_default: true)
-NotificationType.create!(appointment_type: apt_type_sortie_audience_spip, role: :reschedule, template: "Changement de la convocation de date X a date Y.", is_default: true)
-
-NotificationType.create!(appointment_type: apt_type_sap_ddse, role: :summon, template: "Vous êtes convoqué à votre rdv SAP DDSE, merci de venir.", is_default: true)
-NotificationType.create!(appointment_type: apt_type_sap_ddse, role: :reminder, template: "RAPPEL Vous êtes convoqué à votre rdv SAP DDSE, vraiment il faut venir.", reminder_period: :two_days, is_default: true)
-NotificationType.create!(appointment_type: apt_type_sap_ddse, role: :cancelation, template: "Finalement non, c'est pas la peine.", is_default: true)
-NotificationType.create!(appointment_type: apt_type_sap_ddse, role: :no_show, template: "Vous n'êtes pas venu :(", is_default: true)
-NotificationType.create!(appointment_type: apt_type_sap_ddse, role: :reschedule, template: "Changement de la convocation de date X a date Y.", is_default: true)
-
-SlotType.create(appointment_type: apt_type_sortie_audience_spip, agenda: agenda_spip_92, week_day: :monday, starting_time: Time.new(2021, 6, 21, 10, 00, 0), duration: 60, capacity: 3)
-SlotType.create(appointment_type: apt_type_sortie_audience_spip, agenda: agenda_spip_92, week_day: :monday, starting_time: Time.new(2021, 6, 21, 11, 00, 0), duration: 60, capacity: 3)
-SlotType.create(appointment_type: apt_type_sap_ddse, agenda: agenda_spip_92, week_day: :thursday, starting_time: Time.new(2021, 6, 21, 15, 00, 0), duration: 60, capacity: 5)
-SlotType.create(appointment_type: apt_type_sortie_audience_sap, agenda: agenda_tj_nanterre, week_day: :wednesday, starting_time: Time.new(2021, 6, 21, 10, 00, 0), duration: 60, capacity: 3)
-SlotType.create(appointment_type: apt_type_sortie_audience_sap, agenda: agenda_tj_nanterre_2, week_day: :wednesday, starting_time: Time.new(2021, 6, 21, 11, 00, 0), duration: 60, capacity: 3)
-SlotType.create(appointment_type: apt_type_sortie_audience_sap, agenda: agenda_tj_melun, week_day: :monday, starting_time: Time.new(2021, 6, 21, 10, 00, 0), duration: 60, capacity: 3)
-SlotType.create(appointment_type: apt_type_sortie_audience_sap, agenda: agenda_tj_fontainebleau, week_day: :tuesday, starting_time: Time.new(2021, 6, 21, 11, 00, 0), duration: 60, capacity: 3)
-SlotType.create(appointment_type: apt_type_sortie_audience_spip, agenda: agenda_spip_77, week_day: :monday, starting_time: Time.new(2021, 6, 21, 11, 00, 0), duration: 60, capacity: 3)
-SlotType.create(appointment_type: apt_type_sortie_audience_spip, agenda: agenda_spip_77_permanence_tj, week_day: :monday, starting_time: Time.new(2021, 6, 21, 11, 00, 0), duration: 60, capacity: 3)
-SlotType.create(appointment_type: apt_type_sortie_audience_spip, agenda: agenda_spip_77_permanence_tj_2, week_day: :monday, starting_time: Time.new(2021, 6, 21, 11, 00, 0), duration: 60, capacity: 3)
-
-SlotFactory.perform
 
 p 'Database seeded'
---
+++ db/seeds/regular_sortie_audience.rb
@@ -0,0 +1,66 @@
+require 'faker'
+
+org_tj_bordeaux = Organization.find_or_create_by!(name: 'TJ de Bordeaux', organization_type: 'tj')
+org_spip_33_bordeaux = Organization.find_or_create_by!(name: 'SPIP 33 - Bordeaux', organization_type: 'spip') do |org|
+  org.tjs = [org_tj_bordeaux]
+end
+
+place_spip_33_bordeaux = Place.find_or_create_by!(organization: org_spip_33_bordeaux, name: "SPIP de Bordeaux", adress: "37 Rue Général de Larminat, 33000 Bordeaux", phone: '+33606060606')
+place_tj_bordeaux = Place.find_or_create_by!(organization: org_tj_bordeaux, name: "TJ de Bordeaux", adress: "30 Rue des Frères Bonie 33000 Bordeaux", phone: '+33606060606')
+
+apt_type_sortie_audience_sap = AppointmentType.find_or_create_by!(name: "Sortie d'audience SAP")
+apt_type_sortie_audience_spip = AppointmentType.find_or_create_by!(name: "Sortie d'audience SPIP")
+apt_type_sap_ddse = AppointmentType.find_or_create_by!(name: "SAP DDSE")
+apt_type_rdv_suivi_jap = AppointmentType.find_or_create_by!(name: 'RDV de suivi JAP')
+apt_type_rdv_suivi_spip = AppointmentType.find_or_create_by!(name: "RDV de suivi SPIP")
+
+PlaceAppointmentType.find_or_create_by!(place: place_spip_33_bordeaux, appointment_type: apt_type_sortie_audience_spip)
+PlaceAppointmentType.find_or_create_by!(place: place_tj_bordeaux, appointment_type: apt_type_sortie_audience_sap)
+
+agenda_spip_bordeaux = Agenda.find_or_create_by!(place: place_spip_33_bordeaux, name: "Agenda SPIP St Brieuc")
+agenda_tj_bordeaux = Agenda.find_or_create_by!(place: place_tj_bordeaux, name: "Agenda TJ St Brieuc")
+
+Slot.create!(agenda: agenda_tj_bordeaux, starting_time: Time.zone.now, date: Date.tomorrow.next_occurring(:monday), duration: 15, capacity: 1, appointment_type: apt_type_sortie_audience_sap)
+Slot.create!(agenda: agenda_spip_bordeaux, starting_time: Time.zone.now, date: Date.tomorrow.next_occurring(:tuesday), duration: 15, capacity: 1, appointment_type: apt_type_sortie_audience_spip)
+
+User.find_or_create_by!(
+  organization: org_spip_33_bordeaux, email: 'cpip33bdx@example.com', role: :cpip
+) do |user|
+  user.password = '1mot2passeSecurise!'
+  user.password_confirmation = '1mot2passeSecurise!'
+  user.first_name = Faker::Name.first_name
+  user.last_name = Faker::Name.last_name
+end
+
+User.find_or_create_by!(
+  organization: org_spip_33_bordeaux, email: 'localadmin33bdx@example.com', role: :local_admin
+) do |user|
+  user.password = '1mot2passeSecurise!'
+  user.password_confirmation = '1mot2passeSecurise!'
+  user.first_name = Faker::Name.first_name
+  user.last_name = Faker::Name.last_name
+end
+
+User.find_or_create_by!(
+  organization: org_tj_bordeaux, email: 'bextjbdx@example.com', role: :bex
+) do |user|
+  user.password = '1mot2passeSecurise!'
+  user.password_confirmation = '1mot2passeSecurise!'
+  user.first_name = Faker::Name.first_name
+  user.last_name = Faker::Name.last_name
+end
+
+User.find_or_create_by!(
+  organization: org_tj_bordeaux, email: 'localadmintjbdx@example.com', role: :local_admin
+) do |user|
+  user.password = '1mot2passeSecurise!'
+  user.password_confirmation = '1mot2passeSecurise!'
+  user.first_name = Faker::Name.first_name
+  user.last_name = Faker::Name.last_name
+end
+
+SlotType.create(appointment_type: apt_type_sortie_audience_spip, agenda: agenda_spip_bordeaux, week_day: :monday, starting_time: Time.new(2021, 6, 21, 11, 00, 0), duration: 60, capacity: 3)
+SlotType.create(appointment_type: apt_type_sap_ddse, agenda: agenda_spip_bordeaux, week_day: :thursday, starting_time: Time.new(2021, 6, 21, 15, 00, 0), duration: 60, capacity: 5)
+SlotType.create(appointment_type: apt_type_sortie_audience_sap, agenda: agenda_tj_bordeaux, week_day: :wednesday, starting_time: Time.new(2021, 6, 21, 10, 00, 0), duration: 60, capacity: 3)
+
+SlotFactory.perform
