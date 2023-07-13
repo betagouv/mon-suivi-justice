729e46e407eac894ade937127415873f2dac02de
Charles Marcoin <charles.marcoin@gmail.com>
2023-07-13 10:59:50 +0200

    fixed test suite

--- spec/features/notifications_spec.rb
+++ spec/features/notifications_spec.rb
@@ -13,17 +13,14 @@ RSpec.feature 'Notifications', type: :feature do
     it "is programmed even if the convict don't have a phone", js: true do
       allow(LinkMobilityAdapter).to receive(:new).and_return adapter_dbl
 
-      create(:convict, first_name: 'Bobby', last_name: 'Lapointe', phone: '', no_phone: true,
+      @convict = create(:convict, first_name: 'Bobby', last_name: 'Lapointe', phone: '', no_phone: true,
                        organizations: [@user.organization])
       appointment_type = create :appointment_type, :with_notification_types, name: 'Convocation de suivi SPIP'
       place = create :place, name: 'SPIP de Thorigné', appointment_types: [appointment_type],
                              organization: @user.organization
       create :agenda, place: place, name: 'Agenda du SPIP de Thorigné'
 
-      visit new_appointment_path
-
-      first('.select2-container', minimum: 1).click
-      find('li.select2-results__option', text: 'LAPOINTE Bobby').click
+      visit new_appointment_path({ convict_id: @convict.id })
 
       select 'Convocation de suivi SPIP', from: :appointment_appointment_type_id
       select 'SPIP de Thorigné', from: 'Lieu'
