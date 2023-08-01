b44dd636ca8a2c6f6a1d3e9cff64ace1d30e88a7
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-08-01 14:18:26 +0200

    fix mail test

--- spec/mailers/devise_invitable_spec.rb
+++ spec/mailers/devise_invitable_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe DeviseInvitable::Mailer, type: :mailer do
   describe 'deliver' do
     let(:organization) { create(:organization, name: 'Organisation de test') }
-    let(:user) { create(:user, email: 'agent@example.com', first_name: 'Super Agent', organization: organization) }
+    let(:user) { create(:user, email: 'agent@example.com', first_name: 'Super Agent', organization:) }
     let(:mail) { user.invite! }
 
     it 'renders the headers' do
@@ -12,11 +12,10 @@ RSpec.describe DeviseInvitable::Mailer, type: :mailer do
       expect(mail.from).to eq(['support@mon-suivi-justice.beta.gouv.fr'])
     end
 
-    # rubocop:disable Layout/LineLength
     it 'renders the body' do
       expect(mail.body.encoded).to match('Bonjour Super Agent')
-      expect(mail.body.encoded).to match("Pour activer votre compte et commencer =C3=A0 utiliser Mon Suivi Justice,=\n veuillez copier-coller le lien suivant dans le navigateur Edge :")
+      expect(mail.body.encoded).to match('Pour activer votre compte et commencer =C3=A0 utiliser Mon Suivi Justice,')
+      expect(mail.body.encoded).to match('veuillez copier-coller le lien suivant dans le navigateur Edge :')
     end
-    # rubocop:enable Layout/LineLength
   end
 end
