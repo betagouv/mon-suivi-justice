1d695827173cfeafcfea51607daf74f00ab65ea2
Charles Marcoin <charles.marcoin@gmail.com>
2023-08-21 16:08:43 +0200

    overriding devise's create action

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,6 +1,20 @@
 class InvitationsController < Devise::InvitationsController
   layout 'agent_interface', only: [:new, :create]
 
+  # Overriding devise's create action so that admins can move users to other organizations
+  def create
+    email = invite_params[:email]
+    existing_user = User.find_by(email:)
+
+    if existing_user && (existing_user.organization != current_organization)
+      redirect_to new_user_invitation_path,
+                  alert: "Cet agent fait déjà partie d'un autre service : “#{existing_user.organization.name}”. Si cet agent a été muté dans votre service, vous pouvez effectuer sa mutation en cliquant ici : “Muter l’agent dans mon service”. Un email sera envoyé au service actuel pour l’informer"
+      return
+    end
+
+    super
+  end
+
   protected
 
   def after_invite_path_for(_, _)
