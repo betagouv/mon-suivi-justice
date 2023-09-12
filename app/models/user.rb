087980781c023582683ce5333def7a5daa179744
Charles Marcoin <charles.marcoin@gmail.com>
2023-09-12 14:30:55 +0200

    noticed configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User < ApplicationRecord
   has_many :invited_appointments, class_name: 'Appointment', foreign_key: :inviter_user_id, dependent: :nullify
   has_many :visits, class_name: 'Ahoy::Visit'
   has_many :user_notifications, as: :recipient, dependent: :destroy
+  has_many :user_alerts, as: :recipient, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :trackable and :omniauthable
