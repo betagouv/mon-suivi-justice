41c40a077cac6c366c7672949fee223648a4c0fa
Charles Marcoin <charles.marcoin@gmail.com>
2023-09-13 17:12:12 +0200

    now creating user alerts from admin

--- app/jobs/user_alert_delivery_job.rb
+++ app/jobs/user_alert_delivery_job.rb
@@ -1,18 +1,24 @@
 class UserAlertDeliveryJob < ApplicationJob
   queue_as :default
+  sidekiq_options retry: false
 
-  def perform(comment, organization_id, role)
+  def perform(comment, organization_id, role, user)
     @errors = []
 
-    @organization = Organization.find(organization_id)
+    @organization = Organization.find_by id: organization_id
 
-    recipients = User.where(organization_id:, role:)
+    query_conditions = {}
+    query_conditions[:organization] = @organization if @organization.present?
+    query_conditions[:role] = role if role.present?
+
+    recipients = User.where(query_conditions)
     notification = UserAlertNotification.with(comment:)
 
     notification.deliver(recipients)
   rescue StandardError => e
     @errors.push("Erreur : #{e.message}")
   ensure
-    AdminMailer.with(comment:, organization:, import_errors:).user_alert_delivery_report.deliver_later
+    AdminMailer.with(user:, comment:, organization: @organization,
+                     errors: @errors).user_alert_delivery_report.deliver_later
   end
 end
