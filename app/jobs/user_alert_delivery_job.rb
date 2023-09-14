2caab4f2112950cd4f8881ad583142db2760fe67
Charles Marcoin <charles.marcoin@gmail.com>
2023-09-14 10:47:55 +0200

    added the recap email

--- app/jobs/user_alert_delivery_job.rb
+++ app/jobs/user_alert_delivery_job.rb
@@ -6,19 +6,24 @@ class UserAlertDeliveryJob < ApplicationJob
     @errors = []
 
     @organization = Organization.find_by id: organization_id
-
-    query_conditions = {}
-    query_conditions[:organization] = @organization if @organization.present?
-    query_conditions[:role] = role if role.present?
-
-    recipients = User.where(query_conditions)
+    recipients = find_recipients(role)
     notification = UserAlertNotification.with(comment:)
 
     notification.deliver(recipients)
   rescue StandardError => e
     @errors.push("Erreur : #{e.message}")
   ensure
-    AdminMailer.with(user:, comment:, organization: @organization,
+    AdminMailer.with(user:, comment:, organization: @organization, role:, number_of_recipients: recipients.count,
                      errors: @errors).user_alert_delivery_report.deliver_later
+  end
+
+  private
+
+  def find_recipients(role)
+    query_conditions = {}
+    query_conditions[:organization] = @organization if @organization.present?
+    query_conditions[:role] = role if role.present?
+
+    User.where(query_conditions)
   end
 end
