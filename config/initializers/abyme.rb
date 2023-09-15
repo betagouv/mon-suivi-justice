906bd0e2ad6384f15083453dc7c60b1152f51a55
Charles Marcoin <charles.marcoin@gmail.com>
2023-09-15 14:32:09 +0200

    skip attributes building by abyme if env is pentest app

--- config/initializers/abyme.rb
+++ config/initializers/abyme.rb
@@ -1,7 +1,8 @@
 module Abyme
   module Model
     def self.permit_attributes(class_name, association, attributes, permit, association_class_name = nil)
-      return if ENV['APP']&.match?(/^mon-suivi-justice-staging-pr\d+/)
+      return if ENV['APP']&.match?(/^mon-suivi-justice-staging-pr\d+/) || ENV['APP']&.match?(/^msj-agents-pentest/)
+      
       @permitted_attributes[class_name]["#{association}_attributes".to_sym] = AttributesBuilder.new(class_name, association, attributes, permit, association_class_name)
       .build_attributes
     end
