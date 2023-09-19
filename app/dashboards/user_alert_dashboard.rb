306fafc57688a382749bdde3223dff60fc1feb79
Charles Marcoin <charles.marcoin@gmail.com>
2023-09-19 11:07:42 +0200

    user alert form the administrate way

--- app/dashboards/user_alert_dashboard.rb
+++ app/dashboards/user_alert_dashboard.rb
@@ -1,9 +1,70 @@
-require 'administrate/custom_dashboard'
+require "administrate/base_dashboard"
 
-class UserAlertDashboard < Administrate::CustomDashboard
-  resource 'Alerte' # used by administrate in the views
+class UserAlertDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    params: Field::String.with_options(searchable: false),
+    read_at: Field::DateTime,
+    recipient: Field::Polymorphic,
+    content: RichTextAreaField,
+    type: Field::String,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+  }.freeze
 
-  def show_search_bar
-    false
-  end
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = %i[
+    id
+    params
+    read_at
+    recipient
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = %i[
+    id
+    read_at
+    recipient
+    content
+    created_at
+    updated_at
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = %i[
+    read_at
+    content
+  ].freeze
+
+  # COLLECTION_FILTERS
+  # a hash that defines filters that can be used while searching via the search
+  # field of the dashboard.
+  #
+  # For example to add an option to search for open resources by typing "open:"
+  # in the search field:
+  #
+  #   COLLECTION_FILTERS = {
+  #     open: ->(resources) { resources.where(open: true) }
+  #   }.freeze
+  COLLECTION_FILTERS = {}.freeze
+
+  # Overwrite this method to customize how user alerts are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(user_alert)
+  #   "UserAlert ##{user_alert.id}"
+  # end
 end
