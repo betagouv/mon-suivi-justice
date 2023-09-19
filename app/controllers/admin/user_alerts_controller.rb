21e89c645b4aec69ba6299dd68a8ac0c74506184
Charles Marcoin <charles.marcoin@gmail.com>
2023-09-19 11:07:42 +0200

    user alert form the administrate way

--- app/controllers/admin/user_alerts_controller.rb
+++ app/controllers/admin/user_alerts_controller.rb
@@ -1,24 +1,46 @@
 module Admin
   class UserAlertsController < Admin::ApplicationController
-    def index
-      render locals: {
-        resources: [],
-        search_term: '',
-        page: '',
-        show_search_bar: show_search_bar?
-      }
-    end
+    # Overwrite any of the RESTful controller actions to implement custom behavior
+    # For example, you may want to send an email after a foo is updated.
+    #
+    # def update
+    #   super
+    #   send_foo_updated_email(requested_resource)
+    # end
 
-    def create
-      UserAlertDeliveryJob.perform_later(params[:comment], params[:organization_id], params[:role], current_user)
+    # Override this method to specify custom lookup behavior.
+    # This will be used to set the resource for the `show`, `edit`, and `update`
+    # actions.
+    #
+    # def find_resource(param)
+    #   Foo.find_by!(slug: param)
+    # end
 
-      redirect_to admin_user_alerts_path, notice: I18n.t('admin.user_alerts.create.notice')
-    end
+    # The result of this lookup will be available as `requested_resource`
 
-    private
+    # Override this if you have certain roles that require a subset
+    # this will be used to set the records shown on the `index` action.
+    #
+    # def scoped_resource
+    #   if current_user.super_admin?
+    #     resource_class
+    #   else
+    #     resource_class.with_less_stuff
+    #   end
+    # end
 
-    def show_search_bar?
-      false
-    end
+    # Override `resource_params` if you want to transform the submitted
+    # data before it's persisted. For example, the following would turn all
+    # empty values into nil values. It uses other APIs such as `resource_class`
+    # and `dashboard`:
+    #
+    # def resource_params
+    #   params.require(resource_class.model_name.param_key).
+    #     permit(dashboard.permitted_attributes(action_name)).
+    #     transform_values { |value| value == "" ? nil : value }
+    # end
+
+    # See https://administrate-demo.herokuapp.com/customizing_controller_actions
+    # for more information
   end
 end
--- app/models/user_alert.rb
+++ app/models/user_alert.rb
@@ -1,4 +1,6 @@
 class UserAlert < ApplicationRecord
   include Noticed::Model
   belongs_to :recipient, polymorphic: true
+
+  has_rich_text :content
 end
