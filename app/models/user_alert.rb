a63646a013e0ccdaafefe84df925594096211adc
Charles Marcoin <charles.marcoin@gmail.com>
2023-09-26 10:23:39 +0200

    added automatic target blank

--- app/models/user_alert.rb
+++ app/models/user_alert.rb
@@ -1,6 +1,13 @@
 class UserAlert < ApplicationRecord
   include Noticed::Model
   belongs_to :recipient, polymorphic: true
+  before_save :set_target_blank
 
   has_rich_text :content
+
+  private
+
+  def set_target_blank
+    content.body = content.body.to_s.gsub('<a href', '<a target="_blank" href')
+  end
 end
