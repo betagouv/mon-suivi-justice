e6b9a06d042efb3ef2d02703b2a023fbb085edc7
Charles Marcoin <charles.marcoin@gmail.com>
2023-09-28 10:57:59 +0200

    websockets and noticed activation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "@hotwired/turbo-rails"
-// import "channels"
+import "channels"
 
 require("jquery");
 require("@nathanvda/cocoon");
