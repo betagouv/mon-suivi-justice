2afc5a472e755bc0c501d6e6d1f2a22dfdca341b
Charles Marcoin <charles.marcoin@gmail.com>
2023-10-05 09:43:07 +0200

    updated administrate to work with new user_alerts

--- db/migrate/20230912121239_create_user_alerts.rb
+++ db/migrate/20230912121239_create_user_alerts.rb
@@ -1,8 +1,8 @@
 class CreateUserAlerts < ActiveRecord::Migration[7.0]
   def change
     create_table :user_alerts do |t|
-      t.string :type, null: false
-      t.datetime :read_at
+      t.string :alert_type, null: false
+      t.datetime :read_at, null: true
 
       t.timestamps
     end
