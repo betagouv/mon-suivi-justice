1bd9e713caa3a48c0c34a46035927246943e7af7
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-10-12 15:45:30 +0200

    lint

--- spec/policies/appointments_reschedules_policy_spec.rb
+++ spec/policies/appointments_reschedules_policy_spec.rb
@@ -134,37 +134,36 @@ describe AppointmentsReschedulesPolicy do
     let(:appointment_type) { create(:appointment_type, name: 'Sortie d\'audience SAP') }
     let(:slot) { create :slot, :without_validations, appointment_type: }
 
-
     context 'for a booked appointment' do
       let(:appointment) { create(:appointment, slot:, state: :booked) }
       User.tj_roles.each do |role|
         context "for a #{role} user which belongs to the same organization as the appointment's" do
           let(:user) { build(:user, organization: slot.agenda.place.organization, role:) }
           subject { AppointmentsReschedulesPolicy.new(user, appointment) }
-  
+
           it { is_expected.to permit_action(:new) }
         end
-  
+
         context "for a #{role} user which does not belong to the same organization as the appointment's" do
           let(:user) { build(:user, :in_organization, role:) }
           subject { AppointmentsReschedulesPolicy.new(user, appointment) }
-  
+
           it { is_expected.not_to permit_action(:new) }
         end
       end
-  
+
       User.spip_roles.each do |role|
         context "for a #{role} user which belongs to the same organization as the appointment's" do
           let(:user) { build(:user, organization: slot.agenda.place.organization, role:) }
           subject { AppointmentsReschedulesPolicy.new(user, appointment) }
-  
+
           it { is_expected.to forbid_action(:new) }
         end
-  
+
         context "for a #{role} user which does not belong to the same organization as the appointment's" do
           let(:user) { build(:user, :in_organization, role:) }
           subject { AppointmentsReschedulesPolicy.new(user, appointment) }
-  
+
           it { is_expected.not_to permit_action(:new) }
         end
       end
@@ -176,34 +175,33 @@ describe AppointmentsReschedulesPolicy do
         context "for a #{role} user which belongs to the same organization as the appointment's" do
           let(:user) { build(:user, organization: slot.agenda.place.organization, role:) }
           subject { AppointmentsReschedulesPolicy.new(user, appointment) }
-  
+
           it { is_expected.to permit_action(:create) }
         end
-  
+
         context "for a #{role} user which does not belong to the same organization as the appointment's" do
           let(:user) { build(:user, :in_organization, role:) }
           subject { AppointmentsReschedulesPolicy.new(user, appointment) }
-  
+
           it { is_expected.not_to permit_action(:create) }
         end
       end
-  
+
       User.spip_roles.each do |role|
         context "for a #{role} user which belongs to the same organization as the appointment's" do
           let(:user) { build(:user, organization: slot.agenda.place.organization, role:) }
           subject { AppointmentsReschedulesPolicy.new(user, appointment) }
-  
+
           it { is_expected.to forbid_action(:create) }
         end
-  
+
         context "for a #{role} user which does not belong to the same organization as the appointment's" do
           let(:user) { build(:user, :in_organization, role:) }
           subject { AppointmentsReschedulesPolicy.new(user, appointment) }
-  
+
           it { is_expected.not_to permit_action(:create) }
         end
       end
     end
-    
   end
 end
