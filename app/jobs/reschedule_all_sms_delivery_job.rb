667ce711a366688f933cce3003cb3cd825826b84
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-10-20 16:19:57 +0200

    lint

--- app/jobs/reschedule_all_sms_delivery_job.rb
+++ app/jobs/reschedule_all_sms_delivery_job.rb
@@ -2,32 +2,41 @@ class RescheduleSmsDeliveryJob < ApplicationJob
   queue_as :default
 
   def perform(start_date = Date.today)
-    # Fetch all scheduled notification IDs from Sidekiq's scheduled set
-    scheduled_notification_ids = Sidekiq::ScheduledSet.new.map do |job|
+    scheduled_notification_ids = fetch_scheduled_notification_ids
+    notifs = fetch_relevant_notifications(start_date)
+    rescheduled_jobs_count = process_notifications(notifs, scheduled_notification_ids)
+
+    puts rescheduled_jobs_count
+    puts rescheduled_jobs_count.values.sum
+  end
+
+  private
+
+  def fetch_scheduled_notification_ids
+    Sidekiq::ScheduledSet.new.map do |job|
       job.item.dig('args', 0, 'arguments', 0) if job.item['wrapped'] == 'SmsDeliveryJob'
     end.compact.to_set
+  end
 
-    # Fetch relevant notifications
-    notifs = Notification.joins(appointment: %i[slot convict])
-                         .where('slots.date > ?', start_date)
-                         .where(appointments: { state: 'booked' })
-                         .where(convicts: { no_phone: false })
-                         .where(state: 'programmed')
+  def fetch_relevant_notifications(start_date)
+    Notification.joins(appointment: %i[slot convict])
+                .where('slots.date > ?', start_date)
+                .where(appointments: { state: 'booked' })
+                .where(convicts: { no_phone: false })
+                .where(state: 'programmed')
+  end
 
-    # Hash to track rescheduled jobs per day
+  def process_notifications(notifs, scheduled_notification_ids)
     rescheduled_jobs_count = Hash.new(0)
 
     notifs.each do |notif|
       next if scheduled_notification_ids.include?(notif.id)
 
-      # Increment the counter for the slot's date
       rescheduled_jobs_count[notif.appointment.slot.date] += 1
-
       SmsDeliveryJob.set(wait_until: notif.delivery_time)
                     .perform_later(notif.id)
     end
 
-    puts rescheduled_jobs_count
-    puts rescheduled_jobs_count.values.sum
+    rescheduled_jobs_count
   end
 end
