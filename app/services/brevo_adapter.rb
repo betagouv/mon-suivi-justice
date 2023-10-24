4afd0a5021cbbf02d2d3cc0c1dbf7a2f2864c229
Charles Marcoin <charles.marcoin@gmail.com>
2023-10-24 11:46:00 +0200

    handling cases where user does not exist in brevo

--- app/services/brevo_adapter.rb
+++ app/services/brevo_adapter.rb
@@ -18,6 +18,8 @@ class BrevoAdapter
   end
 
   # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Layout/LineLength
   def create_contact_for_user(user)
     create_contact_data = {
       email: user.email,
@@ -32,7 +34,14 @@ class BrevoAdapter
     create_contact_data[:listIds] = [ENV.fetch('BREVO_TEST_LIST_ID', 9).to_i] unless Rails.env.production?
     create_contact = SibApiV3Sdk::CreateContact.new(create_contact_data)
 
-    @client.create_contact(create_contact)
+    begin
+      @client.create_contact(create_contact)
+    rescue SibApiV3Sdk::ApiError => e
+      error_response = JSON.parse(e.response_body)
+      raise unless error_response['code'] == 'duplicate_parameter' && error_response['message'] == 'Contact already exist'
+
+      update_user_contact(user)
+    end
   end
 
   def update_user_contact(user)
@@ -48,11 +57,29 @@ class BrevoAdapter
     }
 
     update_contact = SibApiV3Sdk::UpdateContact.new(update_contact_data)
-    @client.update_contact(identifier, update_contact)
+    begin
+      @client.update_contact(identifier, update_contact)
+    rescue SibApiV3Sdk::ApiError => e
+      error_response = JSON.parse(e.response_body)
+      raise unless contact_not_found_error?(error_response)
+
+      create_contact_for_user(user)
+    end
   end
   # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/AbcSize
 
   def delete_user_contact(user_email)
     @client.delete_contact(user_email)
+  rescue SibApiV3Sdk::ApiError => e
+    error_response = JSON.parse(e.response_body)
+    raise unless contact_not_found_error?(error_response)
+  end
+  # rubocop:enable Layout/LineLength
+
+  private
+
+  def contact_not_found_error?(error_response)
+    error_response['code'] == 'document_not_found' && error_response['message'] == 'Contact does not exist'
   end
 end
