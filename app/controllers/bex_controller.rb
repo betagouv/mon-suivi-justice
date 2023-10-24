7987ad3f6018e5e5b08152e720ae798a6aa0fb3d
Charles Marcoin <charles.marcoin@gmail.com>
2023-10-24 15:24:05 +0200

    removed unncessary to fs

--- app/controllers/bex_controller.rb
+++ app/controllers/bex_controller.rb
@@ -100,7 +100,7 @@ class BexController < ApplicationController
   end
 
   def selected_day(days_with_slots_in_selected_month, params)
-    if params[:date]&.present? && days_with_slots_in_selected_month.include?(params[:date].to_date.to_fs)
+    if params[:date]&.present? && days_with_slots_in_selected_month.include?(params[:date].to_date)
       params[:date].to_date.to_fs
     else
       days_with_slots_in_selected_month.first&.to_fs
