5ce818c95316b2873f05823a54eb26cb8e51e40a
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-11-21 16:42:11 +0100

    Improve slot_type policy

--- app/policies/slot_type_policy.rb
+++ app/policies/slot_type_policy.rb
@@ -2,30 +2,30 @@ class SlotTypePolicy < ApplicationPolicy
   ALLOWED_TO_EDIT = %w[admin local_admin jap dir_greff_bex dir_greff_sap greff_sap dpip].freeze
 
   def index?
-    ALLOWED_TO_EDIT.include? user.role
-  end
-
-  def show?
-    ALLOWED_TO_EDIT.include? user.role
-  end
-
-  def new?
-    ALLOWED_TO_EDIT.include? user.role
-  end
-
-  def create?
-    ALLOWED_TO_EDIT.include? user.role
-  end
-
-  def edit?
-    ALLOWED_TO_EDIT.include? user.role
+    check_ownership? && ALLOWED_TO_EDIT.include?(user.role)
   end
 
   def update?
-    ALLOWED_TO_EDIT.include? user.role
+    check_ownership? && ALLOWED_TO_EDIT.include?(user.role)
+  end
+
+  def show?
+    check_ownership? && ALLOWED_TO_EDIT.include?(user.role)
+  end
+
+  def create?
+    check_ownership? && ALLOWED_TO_EDIT.include?(user.role)
   end
 
   def destroy?
-    ALLOWED_TO_EDIT.include? user.role
+    check_ownership? && ALLOWED_TO_EDIT.include?(user.role)
+  end
+
+  def select?
+    true
+  end
+
+  def check_ownership?
+    record.place.organization == user.organization
   end
 end
