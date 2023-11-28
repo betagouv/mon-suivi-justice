4c4a378c95a7420f4f14b2d2b5950dacfa2ed795
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-11-28 11:17:48 +0100

    improve load_places tests

--- spec/controllers/appointments_bookings_controller_spec.rb
+++ spec/controllers/appointments_bookings_controller_spec.rb
@@ -4,11 +4,13 @@ RSpec.describe AppointmentsBookingsController, type: :controller do
   describe 'GET #load_places' do
     let(:spip1) { create(:organization, name: 'SPIP 1', organization_type: 'spip') }
     let(:spip2) { create(:organization, name: 'SPIP 2', organization_type: 'spip') }
-    let(:user) { create(:user, organization: spip1) }
-    let(:convict) { create(:convict, organizations: [spip1, spip2]) }
+    let(:tj) { create(:organization, name: 'TJ', organization_type: 'tj', spips: [spip1]) }
+    let(:user) { create(:user, organization: spip1, role: 'cpip') }
+    let(:convict) { create(:convict, organizations: [spip1, spip2, tj]) }
     let(:appointment_type) { create(:appointment_type) }
-    let!(:place1) { create(:place, organization: spip1, appointment_types: [appointment_type]) }
-    let!(:place2) { create(:place, organization: spip2, appointment_types: [appointment_type]) }
+    let!(:place1) { create(:place, organization: spip1, appointment_types: [appointment_type], name: "place for spip1") }
+    let!(:place2) { create(:place, organization: tj, appointment_types: [appointment_type], name: "place for tj") }
+    let!(:place3) { create(:place, organization: spip2, appointment_types: [appointment_type], name: "place for spip2") }
 
     before do
       sign_in user
@@ -19,11 +21,29 @@ RSpec.describe AppointmentsBookingsController, type: :controller do
       it 'loads the places related to user organization' do
         get :load_places, xhr: true, params: { convict_id: convict.id, apt_type_id: appointment_type.id }
 
-        expect(assigns(:places)).to eq([place1])
-        # Further expectations regarding the contents of @places
+        expect(assigns(:places)).to match_array([place1])
+      end
+
+      context 'bex user not in an inter ressort organization' do
+        let(:user) { create(:user, organization: tj, role: 'bex') }
+
+        it 'loads all places in jurisdiction' do
+          get :load_places, xhr: true, params: { convict_id: convict.id, apt_type_id: appointment_type.id }
+
+          expect(assigns(:places)).to match_array([place1, place2])
+        end
+      end
+
+      context 'bex user is in an inter ressort organization' do
+        let(:tj) { create(:organization, name: 'TJ', organization_type: 'tj', spips: [spip1], use_inter_ressort: true) }
+        let(:user) { create(:user, organization: tj, role: 'bex') }
+
+        it 'loads all places related to the convict organizations' do
+          get :load_places, xhr: true, params: { convict_id: convict.id, apt_type_id: appointment_type.id }
+
+          expect(assigns(:places)).to match_array([place1, place2, place3])
+        end
       end
     end
-
-    # Add more contexts as necessary for different test scenarios
   end
 end
