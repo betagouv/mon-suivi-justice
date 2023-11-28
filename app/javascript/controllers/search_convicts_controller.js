a9fc9ade1c5c4b20fea64c3750e80393915c9f08
Charles Marcoin <charles.marcoin@gmail.com>
2023-11-28 19:32:32 +0100

    handling my convicts vs all convicts filter

--- app/javascript/controllers/search_convicts_controller.js
+++ app/javascript/controllers/search_convicts_controller.js
@@ -1,20 +1,36 @@
 import { ApplicationController, useDebounce } from 'stimulus-use'
 
 export default class extends ApplicationController {
-    static targets = ["results", "query", "table"]
+    static values = { allConvicts: String, myConvicts: String }
+    static targets = ["results", "query", "table", "mineButton"]
     static debounces = ['search']
 
     connect() {
-        useDebounce(this, { wait: 500 })
+        useDebounce(this, { wait: 500 });
+        const url = new URL(window.location.href);
+        const queryParams = new URLSearchParams(url.search);
+        this.onlyMine = queryParams.get('only_mine') === 'true';
+        this.mineButtonTarget.textContent = this.onlyMine ? this.allConvictsValue : this.myConvictsValue;
     }
 
     get query() {
         return this.queryTarget.value
     }
 
+    toggleMine(event) {
+        if (this.hasMineButtonTarget) {
+            event.preventDefault();
+            this.onlyMine = !this.onlyMine;
+            this.mineButtonTarget.textContent = this.onlyMine ? "Tous les probationnaires" : "Uniquement mes probationnaires";
+            this.search();
+        }
+    }
+
     search() {
+
+        console.log("Searching for " + this.query)
+
         if (this.query.length === 0) {
-            this.tableTarget.style.display = 'block';
             this.reset();
             return;
         }
@@ -34,10 +50,12 @@ export default class extends ApplicationController {
             return;
         }
 
+        let searchUrl = `/convicts/search?q=${encodeURIComponent(this.query)}&only_mine=${this.onlyMine}`;
+
         this.abortPreviousFetchRequest()
 
         this.abortController = new AbortController()
-        fetch('/convicts/search?q=' + encodeURIComponent(this.query), { signal: this.abortController.signal })
+        fetch(searchUrl, { signal: this.abortController.signal })
             .then(response => response.text())
             .then(html => {
                 this.handleResults(html)
@@ -48,6 +66,14 @@ export default class extends ApplicationController {
     }
     
     reset() {
+
+        if (this.hasTableTarget) {
+            console.log("On affiche le tableau")
+            // this.tableTarget.style.display = 'block';
+             let redirectionUrl = this.onlyMine ? "/convicts?only_mine=true" : "/convicts?only_mine=false";
+             window.location.href = redirectionUrl;
+        }
+
         this.resultsTarget.innerHTML = ""
         this.queryTarget.value = ""
         this.previousQuery = null
@@ -60,7 +86,10 @@ export default class extends ApplicationController {
     }
 
     handleResults(data) {
-        this.tableTarget.style.display = 'none';
+        if (this.hasTableTarget) {
+            this.tableTarget.style.display = 'none';
+        }
+
         this.resultsTarget.innerHTML = data
     }
 }
