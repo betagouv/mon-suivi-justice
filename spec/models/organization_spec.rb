93efc5c42fac213624f13b84d92d7b8dce5bbbc2
Damien Le Thiec <damien@dlet.fr>
2023-12-07 15:20:48 +0100

    Add test

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -11,4 +11,28 @@ RSpec.describe Organization, type: :model do
 
   it { should define_enum_for(:organization_type).with_values({ spip: 0, tj: 1 }) }
   it { should validate_presence_of(:organization_type) }
+
+  describe 'destruction with dependent associations' do
+    let(:organization) { create(:organization, tjs: [create(:organization, organization_type: 'tj')]) }
+    let(:convict) { create(:convict, organizations: [organization]) }
+
+    before do
+      create(:user, organization: organization)
+      create(:place, organization: organization)
+      create(:notification_type, organization: organization)
+      create(:appointment, creating_organization: organization)
+      create(:extra_field, name: 'Extra field A', organization: organization, data_type: 'text', scope: 'appointment_create', appointment_types: [create(:appointment_type)])
+    end
+
+    it 'can be destroyed along with its dependencies' do
+      expect { organization.destroy }.to change(Organization, :count).by(-1)
+      expect(User.where(organization_id: organization.id)).to be_empty
+      expect(Place.where(organization_id: organization.id)).to be_empty
+      expect(NotificationType.where(organization_id: organization.id)).to be_empty
+      expect(Appointment.where(creating_organization: organization.id)).to be_empty
+      expect(ExtraField.where(organization_id: organization.id)).to be_empty
+      convict.reload
+      expect(convict.organizations).to be_empty
+    end
+  end
 end
