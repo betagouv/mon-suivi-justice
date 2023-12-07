7f4e26fd77e87e3b0533d5b097408fd218d2f20f
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-12-07 07:53:08 +0100

    use scope instead of code

--- app/models/place.rb
+++ app/models/place.rb
@@ -27,10 +27,15 @@ class Place < ApplicationRecord
   accepts_nested_attributes_for :agendas, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :place_appointment_types
 
-  scope :in_organization, ->(organization) { where(organization:) }
+  scope :in_organization, ->(organization) { joins(:appointment_types, organization: :convicts).where(organization:) }
 
   scope :in_jurisdiction, lambda { |user_organization|
     where(organization: [user_organization, *user_organization.linked_organizations])
+  }
+
+  scope :linked_with_ddse, lambda { |user_organization|
+    joins(:appointment_types, organization: :convicts)
+      .where(organization: user_organization.linked_organizations, appointment_types: { name: "SAP DDSE" })
   }
 
   scope :in_dep_spips, lambda { |departments|
--- app/policies/place_policy.rb
+++ app/policies/place_policy.rb
@@ -9,6 +9,8 @@ class PlacePolicy < ApplicationPolicy
 
       if user.admin? || user.work_at_bex? || user.local_admin_tj?
         scope.in_jurisdiction(user.organization)
+      elsif user.work_at_sap?
+        scope.in_organization(user.organization).or(scope.linked_with_ddse(user.organization))
       else
         scope.in_organization(user.organization)
       end
