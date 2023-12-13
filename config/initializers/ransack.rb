2d68e33dad1b1775b535af1b521dce20b95f7b6c
Charles Marcoin <charles.marcoin@gmail.com>
2023-12-13 16:33:23 +0100

    added expand up and down arrow

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -1,3 +1,9 @@
 Ransack.configure do |config|
-  config.custom_arrows = { default_arrow: '-' }
+
+  config.custom_arrows = {
+    up_arrow: "<span class='fr-icon-arrow-up-s-line fr-icon-sm' aria-hidden='true'></span>",
+    down_arrow: "<span class='fr-icon-arrow-down-s-line fr-icon-sm' aria-hidden='true'></span>",
+    default_arrow: "<img src='assets/expand-up-down-line.svg' style='width: 1.2rem; height: 1.2rem;' alt='expand'>"
+  }
+
 end
