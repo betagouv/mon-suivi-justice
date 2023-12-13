6eed414dcfe55d632a4b0112fb2a7b7c356251e9
Matthieu Faugere <matthieu.faugere@gmail.com>
2023-12-13 14:26:12 +0100

    localize and disable saturday and sunday

--- app/javascript/controllers/pick_multiple_dates_controller.js
+++ app/javascript/controllers/pick_multiple_dates_controller.js
@@ -1,9 +1,16 @@
 import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr"
+import { French } from "flatpickr/dist/l10n/fr.js"
 
 export default class extends Controller {
   connect() {
     console.log(this.element)
-    flatpickr(this.element, { mode: 'multiple', minDate: 'today'})
+    flatpickr(this.element, { mode: 'multiple', minDate: 'today', locale: French, disable: [
+      function(date) {
+          // return true to disable
+          return (date.getDay() === 0 || date.getDay() === 6);
+
+      }
+    ]})
   }
 }
