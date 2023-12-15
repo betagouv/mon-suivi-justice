9ec56912d640fc627a462acbdfdbc24200829c4e
Charles Marcoin <charles.marcoin@gmail.com>
2023-12-15 14:26:19 +0100

    [MSJ-878] replaced fa icons by css (#526)
    <img width="1186" alt="Capture d’écran 2023-12-14 à 16 35 46"
    src="https://github.com/betagouv/mon-suivi-justice/assets/25039335/2572d4c6-3bd2-4e59-98bb-a3c165c268bb">
    
    <img width="195" alt="Capture d’écran 2023-12-12 à 15 06 21"
    src="https://github.com/betagouv/mon-suivi-justice/assets/25039335/1a1308ba-b297-46a6-b66b-8ae2d2afe9da">
    
    ---------
    
    Co-authored-by: Matthieu Faugere <matthieu.faugere@gmail.com>

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -1,3 +1,7 @@
 Ransack.configure do |config|
-  config.custom_arrows = { default_arrow: '<i class="fa fa-sort"></i>' }
+  config.custom_arrows = {
+    up_arrow: "<span class='fr-icon-arrow-up-s-line fr-icon-sm' aria-hidden='true'></span>",
+    down_arrow: "<span class='fr-icon-arrow-down-s-line fr-icon-sm' aria-hidden='true'></span>",
+    default_arrow: "<img src='assets/expand-up-down-line.svg' class='table-header__arrows--expand' alt='expand'>"
+  }
 end
