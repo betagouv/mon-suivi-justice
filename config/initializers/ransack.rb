a53ff5d7dedd8a61636d9533504fbe24824b631a
Charles Marcoin <charles.marcoin@gmail.com>
2023-12-19 17:00:35 +0100

    attempt with asset path helper

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -1,11 +1,7 @@
 Ransack.configure do |config|
-  asset_filename = Rails.application.assets_manifest.assets['expand-up-down-line.svg'] || 'expand-up-down-line.svg'
-  base_path = Rails.env.production? ? "" : "/assets"
-
-
   config.custom_arrows = {
     up_arrow: "<span class='fr-icon-arrow-up-s-line fr-icon-sm' aria-hidden='true'></span>",
     down_arrow: "<span class='fr-icon-arrow-down-s-line fr-icon-sm' aria-hidden='true'></span>",
-    default_arrow: "<img src='#{base_path}/#{asset_filename}' class='table-header__arrows--expand' alt='expand'>"
+    default_arrow: "<img src='#{ActionController::Base.helpers.asset_path("expand-up-down-line.svg")}' class='table-header__arrows--expand' alt='expand'>"
   }
 end
