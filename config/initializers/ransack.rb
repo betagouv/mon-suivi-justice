d1b1dae8faecbaf074a36b181ff94317e6714f94
Charles Marcoin <charles.marcoin@gmail.com>
2023-12-19 16:28:07 +0100

    change assets base path based on env

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -1,7 +1,9 @@
 Ransack.configure do |config|
+  assets_base_path = Rails.env.production? ? "/assets" : "assets"
+
   config.custom_arrows = {
     up_arrow: "<span class='fr-icon-arrow-up-s-line fr-icon-sm' aria-hidden='true'></span>",
     down_arrow: "<span class='fr-icon-arrow-down-s-line fr-icon-sm' aria-hidden='true'></span>",
-    default_arrow: "<img src='assets/expand-up-down-line.svg' class='table-header__arrows--expand' alt='expand'>"
+    default_arrow: "<img src='#{assets_base_path}/expand-up-down-line.svg' class='table-header__arrows--expand' alt='expand'>"
   }
 end
