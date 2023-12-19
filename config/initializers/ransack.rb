861d8d403ddfefd3e7334c13201c5eb765a3e6e1
Charles Marcoin <charles.marcoin@gmail.com>
2023-12-19 16:42:54 +0100

    now trying with assets manifest

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -1,9 +1,11 @@
 Ransack.configure do |config|
-  assets_base_path = Rails.env.production? ? "/assets" : "assets"
+  asset_filename = Rails.application.assets_manifest.assets['expand-up-down-line.svg'] || 'expand-up-down-line.svg'
+  base_path = Rails.env.production? ? "" : "/assets"
+
 
   config.custom_arrows = {
     up_arrow: "<span class='fr-icon-arrow-up-s-line fr-icon-sm' aria-hidden='true'></span>",
     down_arrow: "<span class='fr-icon-arrow-down-s-line fr-icon-sm' aria-hidden='true'></span>",
-    default_arrow: "<img src='#{assets_base_path}/expand-up-down-line.svg' class='table-header__arrows--expand' alt='expand'>"
+    default_arrow: "<img src='#{base_path}/#{asset_filename}' class='table-header__arrows--expand' alt='expand'>"
   }
 end
