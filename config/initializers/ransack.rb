b225909363cd265c8b29f4b79103580212c9b6c4
Charles Marcoin <charles.marcoin@gmail.com>
2023-12-20 14:46:24 +0100

    removed default arrow

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -2,6 +2,5 @@ Ransack.configure do |config|
   config.custom_arrows = {
     up_arrow: "<span class='fr-icon-arrow-up-s-line fr-icon-sm' aria-hidden='true'></span>",
     down_arrow: "<span class='fr-icon-arrow-down-s-line fr-icon-sm' aria-hidden='true'></span>",
-    default_arrow: "<img src='#{ActionController::Base.helpers.asset_path("expand-up-down-line.svg")}' class='table-header__arrows--expand' alt='expand'>"
   }
 end
