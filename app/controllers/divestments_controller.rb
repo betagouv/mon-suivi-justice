f7eb4cdd1077e63a96897e9e5db7eb567726f551
Charles Marcoin <charles.marcoin@gmail.com>
2023-12-30 17:21:54 +0100

    added checks to display the divestment button

--- app/controllers/divestments_controller.rb
+++ app/controllers/divestments_controller.rb
@@ -1,18 +1,34 @@
 # app/controllers/divestments_controller.rb
 class DivestmentsController < ApplicationController
   def create
-    @divestment = Divestment.new(divestment_params)
-    @divestment.organization_from_id = current_user.organization.id
-    if @divestment.save
-      redirect_to some_path, notice: 'La déssaisissement a bien été créé.'
+    convict = Convict.find_by(id: params[:convict_id])
+    if convict
+      create_divestments_for_convict(convict)
+      redirect_to some_path, notice: 'Divestments were successfully created.'
     else
-      @organizations = Organization.all
-      @convicts = Convict.all
-      render :new
+      redirect_to some_path, alert: 'Convict not found.'
     end
   end
 
   private
+
+  def create_divestments_for_convict(convict)
+    user_organization = current_user.organization
+
+    ActiveRecord::Base.transaction do
+      convict.organizations.each do |org|
+        Divestment.create!(
+          convict_id: convict.id,
+          organization_from_id: org.id,
+          organization_to_id: user_organization.id,
+          state: 'pending'
+        )
+      end
+    end
+  rescue ActiveRecord::RecordInvalid => e
+    # TODO : Handle error (back to convicts/new ?)
+    Rails.logger.error("Error creating divestments: #{e.message}")
+  end
 
   def divestment_params
     params.require(:divestment).permit(:user_id, :organization_to_id, :decision_date, :comment)
