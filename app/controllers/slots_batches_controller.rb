1903d3e4de56098628855f03e13ffc8a312ada5e
Matthieu Faugere <matthieu.faugere@gmail.com>
2024-01-09 15:20:46 +0100

    simplify flatten stuff

--- app/controllers/slots_batches_controller.rb
+++ app/controllers/slots_batches_controller.rb
@@ -17,7 +17,7 @@ class SlotsBatchesController < ApplicationController
   def handle_create(params, slot_params)
     result = batch_create(params, slot_params)
 
-    if result.flatten(1).all?(&:persisted?)
+    if result.all?(&:persisted?)
       flash.discard
       redirect_to slots_path
     else
@@ -67,39 +67,41 @@ class SlotsBatchesController < ApplicationController
 
   # rubocop:disable Metrics/AbcSize
   # rubocop:disable Metrics/CyclomaticComplexity
-  # rubocop:disable Metrics/PerceivedComplexity
   def batch_create(params, slot_params)
     raise StandardError, 'You must provide at least one starting time or a valid interval' unless valid_time?(params)
 
     starting_times = params.require(:starting_times).each_slice(2).to_a if params[:starting_times].present?
     interval_times = generate_times_from_intervals(params) if valid_interval?(params)
-    times = [*starting_times, *interval_times&.flatten(1)].compact.uniq
+    times = [*starting_times, *interval_times].compact.uniq
 
     dates = slot_params.require(:date).split(', ').map(&:to_date)
     slots_data = dates.map { |date| times.map { |time| build_slot(slot_params, date, time) } }
-    Slot.create(slots_data) unless slots_data.empty?
+    # need to flatten to gave all dates time as one array
+    Slot.create(slots_data.flatten(1)) unless slots_data.empty?
   end
-  # rubocop:enable Metrics/PerceivedComplexity
-  # rubocop:enable Metrics/CyclomaticComplexity
 
   def generate_times_from_intervals(params)
     start_times = params.require(:start_times).each_slice(2).to_a
     end_times = params.require(:end_times).each_slice(2).to_a
-    intervals = params.require(:intervals).each_slice(2).to_a
+    intervals = params.require(:intervals).to_a
 
-    start_times.map.with_index do |start_time, idx|
+    result = start_times.map.with_index do |start_time, idx|
       end_time = end_times[idx] if end_times[idx].present?
       interval = intervals[idx] if intervals[idx].present?
       generate_times(start_time, end_time, interval) if start_time.present? && interval.present? && end_time.present?
     end
+
+    # need to flatten to have all starting_times intervals as 1 array
+    result&.flatten(1)
   end
+  # rubocop:enable Metrics/CyclomaticComplexity
 
   def generate_times(start_time, end_time, interval)
     start_time = Time.zone.parse("#{start_time[0]}:#{start_time[1]}")
     end_time = Time.zone.parse("#{end_time[0]}:#{end_time[1]}")
     raise ArgumentError, 'Start time must be before end time' if start_time > end_time
 
-    interval_minutes = interval.first.to_i.minutes
+    interval_minutes = interval.to_i.minutes
     result = []
 
     current_time = start_time
