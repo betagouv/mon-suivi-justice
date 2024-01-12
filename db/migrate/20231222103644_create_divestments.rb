388addff1384cbc981ecfb6fdccd8d7e51e5c76c
Charles Marcoin <charles.marcoin@gmail.com>
2024-01-12 11:06:44 +0100

    updated schema

--- db/migrate/20231222103644_create_divestments.rb
+++ db/migrate/20231222103644_create_divestments.rb
@@ -1,6 +1,7 @@
 class CreateDivestments < ActiveRecord::Migration[7.0]
   def change
     create_table :divestments do |t|
+      t.references :organization, null: false, foreign_key: true
       t.references :convict, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
       t.string :state
