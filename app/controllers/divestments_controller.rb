5becdb5e0f77e3c00d569e71fca821ad1a54a2f8
Charles Marcoin <charles.marcoin@gmail.com>
2024-01-12 11:33:55 +0100

    revamped creation method for divestments

--- app/controllers/divestments_controller.rb
+++ app/controllers/divestments_controller.rb
@@ -18,18 +18,22 @@ class DivestmentsController < ApplicationController
     user_organization = current_user.organization
 
     ActiveRecord::Base.transaction do
+      divestment = Divestment.create!(
+        convict_id: convict.id,
+        user_id: current_user.id,
+        organization_id: user_organization.id,
+        state: 'pending'
+      )
+
       convict.organizations.each do |org|
-        Divestment.create!(
-          convict_id: convict.id,
-          user_id: current_user.id,
-          organization_from_id: org.id,
-          organization_to_id: user_organization.id,
-          state: 'pending'
+        OrganizationDivestment.create!(
+          divestment_id: divestment.id,
+          organization_id: org.id
         )
       end
     end
   rescue ActiveRecord::RecordInvalid => e
-    # TODO : Handle error (back to convicts/new ?)
+    # TODO: Handle error (back to convicts/new ?)
     Rails.logger.error("Error creating divestments: #{e.message}")
   end
 
