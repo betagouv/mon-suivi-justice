16c9cdb37d1dc753deab1c81a5f3623680756312
Charles Marcoin <charles.marcoin@gmail.com>
2024-01-12 16:51:50 +0100

    added unicity to divestments

--- app/dashboards/organization_divestment_dashboard.rb
+++ app/dashboards/organization_divestment_dashboard.rb
@@ -1,4 +1,4 @@
-require "administrate/base_dashboard"
+require 'administrate/base_dashboard'
 
 class OrganizationDivestmentDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
@@ -15,7 +15,7 @@ class OrganizationDivestmentDashboard < Administrate::BaseDashboard
     organization: Field::BelongsTo,
     state: Field::String,
     created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    updated_at: Field::DateTime
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -25,9 +25,10 @@ class OrganizationDivestmentDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
     id
-    comment
-    decision_date
+    organization
     divestment
+    decision_date
+    state
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
@@ -47,10 +48,7 @@ class OrganizationDivestmentDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    comment
     decision_date
-    divestment
-    organization
     state
   ].freeze
 
