114fcd4279700d636c2b3ffd354b69e46fb4f771
Matthieu Faugere <matthieu.faugere@gmail.com>
2024-01-16 17:00:28 +0100

    [MSJ-920] test multi creneau (#554)
    Co-authored-by: Damien Le Thiec <damien@dlet.fr>

--- app/controllers/slots_batches_controller.rb
+++ app/controllers/slots_batches_controller.rb
@@ -16,6 +16,7 @@ class SlotsBatchesController < ApplicationController
 
   def handle_create(params, slot_params)
     result = batch_create(params, slot_params)
+
     if result.all?(&:persisted?)
       flash.discard
       redirect_to slots_path
@@ -51,21 +52,75 @@ class SlotsBatchesController < ApplicationController
     render turbo_stream: turbo_stream.append('display_time_fields', partial: 'time_fields')
   end
 
+  def display_interval_fields
+    render turbo_stream: turbo_stream.append('display_time_fields', partial: 'interval_fields')
+  end
+
   private
 
   def slot_params
     params.require(:slot_batch).permit(:agenda_id, :appointment_type_id, :date, :available,
-                                       :starting_time, :capacity, :duration, starting_times: [])
+                                       :starting_time, :capacity, :duration, :interval, :start_time, :end_time,
+                                       starting_times: [],
+                                       intervals: [], start_times: [], end_times: [])
   end
 
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/CyclomaticComplexity
   def batch_create(params, slot_params)
-    times = params.require(:starting_times).each_slice(2).to_a
+    raise ArgumentErrorError, t('.argument_error') unless valid_time?(params)
+
+    starting_times = params.require(:starting_times).each_slice(2).to_a if params[:starting_times].present?
+    interval_times = generate_times_from_intervals(params) if valid_interval?(params)
+    times = [*starting_times, *interval_times].compact.uniq
+
     dates = slot_params.require(:date).split(', ').map(&:to_date)
-    slots_data = []
+    slots_data = dates.map { |date| times.map { |time| build_slot(slot_params, date, time) } }
+    # need to flatten to gave all dates time as one array
+    Slot.create(slots_data.flatten(1)) unless slots_data.empty?
+  end
 
-    dates.each { |date| times.each { |time| slots_data << build_slot(slot_params, date, time) } }
+  def generate_times_from_intervals(params)
+    start_times = params.require(:start_times).each_slice(2).to_a
+    end_times = params.require(:end_times).each_slice(2).to_a
+    intervals = params.require(:intervals).to_a
 
-    Slot.create(slots_data)
+    result = start_times.map.with_index do |start_time, idx|
+      end_time = end_times[idx] if end_times[idx].present?
+      interval = intervals[idx] if intervals[idx].present?
+      generate_times(start_time, end_time, interval) if start_time.present? && interval.present? && end_time.present?
+    end
+
+    # need to flatten to have all starting_times intervals as 1 array
+    result&.flatten(1)
+  end
+  # rubocop:enable Metrics/CyclomaticComplexity
+
+  def generate_times(start_time, end_time, interval)
+    start_time = Time.zone.parse("#{start_time[0]}:#{start_time[1]}")
+    end_time = Time.zone.parse("#{end_time[0]}:#{end_time[1]}")
+    raise ArgumentError, t('.start_time_before_end_time') if start_time > end_time
+
+    interval_minutes = interval.to_i.minutes
+    result = []
+
+    current_time = start_time
+    while current_time <= end_time
+      result << [current_time.hour.to_s.rjust(2, '0'), current_time.min.to_s.rjust(2, '0')]
+      current_time += interval_minutes
+    end
+    result
+  end
+  # rubocop:enable Metrics/AbcSize
+
+  def valid_time?(params)
+    params[:starting_times]&.compact_blank.present? || valid_interval?(params)
+  end
+
+  def valid_interval?(params)
+    params[:start_times]&.compact_blank.present? &&
+      params[:end_times]&.compact_blank.present? &&
+      params[:intervals]&.compact_blank.present?
   end
 
   def build_slot(params, date, time)
