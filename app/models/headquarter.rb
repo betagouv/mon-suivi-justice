c14696d28efacdd0aa6315bcb33c278ec629939c
Damien Le Thiec <damien@dlet.fr>
2024-01-18 15:07:50 +0100

    Fix user mailer bug when no local admin

--- app/models/headquarter.rb
+++ app/models/headquarter.rb
@@ -2,4 +2,6 @@ class Headquarter < ApplicationRecord
   has_many :organizations, dependent: :nullify
   has_many :users, dependent: :nullify
   validates :name, presence: true
+
+  delegate :local_admin, to: :users
 end
