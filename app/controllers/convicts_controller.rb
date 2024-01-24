7bba8cbe461deb31fe971d19e61f515cef626f1d
Charles Marcoin <charles.marcoin@gmail.com>
2024-01-24 09:50:23 +0100

    removed duplicate code

--- app/controllers/convicts_controller.rb
+++ app/controllers/convicts_controller.rb
@@ -149,64 +149,6 @@ class ConvictsController < ApplicationController
     end
   end
 
-  def divestment_button_checks
-    @duplicate_alert = ''
-
-    if @duplicate_convict&.organizations&.include?(current_organization)
-      handle_duplicate_is_under_current_org
-    else
-      handle_duplicate_is_under_other_org
-    end
-  end
-
-  def handle_duplicate_is_under_current_org
-    @show_divestment_button = false
-    org_names = org_names_with_custom_label('votre propre service')
-    @duplicate_alert = format_duplicate_alert_string(org_names)
-  end
-
-  def handle_duplicate_is_under_other_org
-    pending_divestment_and_future_appointments
-    @show_divestment_button = @pending_divestment.nil? && @future_appointments.none?
-
-    @duplicate_alert = if @pending_divestment.present?
-                         @show_pending_divestment_notice =
-                           "Le probationnaire #{@duplicate_convict.full_name} fait \
-                           déjà l'objet d'une demande de dessaisissement \
-                           de la part de #{@pending_divestment.organization.name} \
-                           (#{@pending_divestment.organization.places.first&.phone})."
-                       else
-                         org_names = formatted_organization_names_and_phones
-                         @duplicate_alert = format_duplicate_alert_string(org_names)
-                       end
-  end
-
-  def org_names_with_custom_label(custom_label)
-    @duplicate_convict.organizations.map do |org|
-      org == current_organization ? custom_label : org.name
-    end
-  end
-
-  def format_duplicate_alert_string(org_names)
-    return org_names.first.to_s if org_names.length <= 1
-
-    last = org_names.pop
-    "#{@duplicate_convict.full_name} suivi par #{org_names.join(', ')} ainsi que #{last}"
-  end
-
-  def formatted_organization_names_and_phones
-    @duplicate_convict.organizations.map do |org|
-      name_and_phone = org.name
-      name_and_phone += " (#{org.places.first&.phone})" if org.places.first&.phone.present?
-      name_and_phone
-    end
-  end
-
-  def pending_divestment_and_future_appointments
-    @pending_divestment = @duplicate_convict.divestments.find_by(state: 'pending')
-    @future_appointments = @duplicate_convict.future_appointments
-  end
-
   def convict_params
     params.require(:convict).permit(
       :first_name, :last_name, :phone, :no_phone,
