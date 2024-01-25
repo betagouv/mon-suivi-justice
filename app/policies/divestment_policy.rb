776f35db6a21013e77ab632473fc01054e6c4da0
Charles Marcoin <charles.marcoin@gmail.com>
2024-01-25 15:19:20 +0100

    removed divestments policy scope which was not used

--- app/policies/divestment_policy.rb
+++ app/policies/divestment_policy.rb
@@ -1,14 +1,4 @@
 class DivestmentPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      if user.admin?
-        scope.all
-      elsif user.local_admin?
-        scope.where(users: user)
-      end
-    end
-  end
-
   def create?
     record.organization == user.organization
   end
