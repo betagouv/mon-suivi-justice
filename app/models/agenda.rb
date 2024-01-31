141be69040daa8466391c6148efb4cc6d18e2142
Matthieu Faugere <matthieu.faugere@gmail.com>
2024-01-31 15:19:47 +0100

    test

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -29,7 +29,7 @@ class Agenda < ApplicationRecord
 
   scope :with_open_slots, lambda { |appointment_type|
     joins(:slots).where('slots.appointment_type_id = ?', appointment_type.id)
-                 .where('slots.date >= ?', Date.today).uniq
+                 .where('slots.date >= ?', Time.zone.today).uniq
   }
 
   def self.ransackable_attributes(_auth_object = nil)
