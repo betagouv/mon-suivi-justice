2fd519e9dcb5b2e7e64beb70a3a41b3a4daeb37a
Matthieu Faugere <matthieu.faugere@gmail.com>
2024-02-06 16:31:24 +0100

    fix alias_attribute deprecation, devise use of secrets and mailer port

--- app/models/convict.rb
+++ app/models/convict.rb
@@ -22,8 +22,8 @@ class Convict < ApplicationRecord
   belongs_to :city, optional: true
   belongs_to :creating_organization, class_name: 'Organization', optional: true
 
-  alias_attribute :cpip, :user
-  alias_attribute :agent, :user
+  alias_method :cpip, :user
+  alias_method :agent, :user
 
   attr_accessor :place_id, :duplicates, :current_user
 
