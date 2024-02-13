aa005b0fa511fdfcdf5ce77909dfbb455e46fd7c
Damien Le Thiec <damien@dlet.fr>
2024-02-13 17:31:56 +0100

    Fix UTC problem papertrail update

--- scripts/change_versions_type.rb
+++ scripts/change_versions_type.rb
@@ -11,18 +11,16 @@ versions.each do |version|
   p "Handling version id: #{version.id}"
   begin
     version.update_columns old_object: nil, object: YAML.safe_load(version.old_object, permitted_classes: Rails.application.config.active_record.yaml_column_permitted_classes, aliases: true) if version.old_object.present?
-    if version.old_object_changes.present?
-      old_object_changes = YAML.safe_load(version.old_object_changes, permitted_classes: Rails.application.config.active_record.yaml_column_permitted_classes, aliases: true)
-      starting_time = old_object_changes['starting_time'][1] if old_object_changes['starting_time'].present?
-      if starting_time.is_a?(ActiveRecord::Type::Time::Value)
-        old_object_changes['starting_time'][1] = starting_time.strftime('%H:%M')
-      end
-      version.update_columns old_object_changes: nil, object_changes: old_object_changes
-    end
+    version.update_columns old_object_changes: nil, object_changes: YAML.safe_load(version.old_object_changes, permitted_classes: Rails.application.config.active_record.yaml_column_permitted_classes, aliases: true) if version.old_object_changes.present?
     progress.increment
-  rescue => e
-    p "Error: #{e.message}"
-    next
+  rescue StandardError => e
+    if e.message.include?("undefined method `utc?' for nil:NilClass")
+      modified_old_object_changes = version.old_object_changes.gsub('  ', '').gsub('!ruby/object:ActiveRecord::Type::Time::Value', 'Sat, 01 Jan 2000 05:00:00.000000000 UTC +00:00')
+      version.update_columns old_object_changes: nil, object_changes: YAML.safe_load(modified_old_object_changes, permitted_classes: Rails.application.config.active_record.yaml_column_permitted_classes, aliases: true) if version.old_object_changes.present?
+    else
+      p "Error: #{e.message}"
+      next
+    end
   end
 end
 
