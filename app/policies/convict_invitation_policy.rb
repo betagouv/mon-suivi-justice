b302bad625107b53186d3315c5b1576a096270f5
Damien Le Thiec <damien@dlet.fr>
2024-02-15 17:42:20 +0100

    Improve feature

--- app/policies/convict_invitation_policy.rb
+++ app/policies/convict_invitation_policy.rb
@@ -1,5 +1,9 @@
 class ConvictInvitationPolicy < ApplicationPolicy
   def create?
-    record.invitable_to_convict_interface? && user.can_invite_to_convict_interface?(record)
+    if record.persisted?
+      record.invitable_to_convict_interface? && user.can_invite_to_convict_interface?(record)
+    else
+      user.can_invite_to_convict_interface?
+    end
   end
 end
