72c9304f4f573774039fe9658c98e55345857eec
Damien Le Thiec <damien@dlet.fr>
2024-02-16 17:04:57 +0100

    Remove create organizations via administrate to make sure notification types are created

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
         get '/impersonate' => "users#impersonate"
       end
       resources :convicts
-      resources :organizations do
+      resources :organizations, except: %i[new create] do
         put '/link_convict_from_linked_orga' => "organizations#link_convict_from_linked_orga"
       end
       resources :departments
