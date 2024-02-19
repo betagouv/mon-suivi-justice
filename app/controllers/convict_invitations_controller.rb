f1244a9039fb641787817da6581b9fb47feab8eb
Damien Le Thiec <damien@dlet.fr>
2024-02-19 20:56:23 +0100

    Remove ConvictInvitationNotification

--- app/controllers/convict_invitations_controller.rb
+++ app/controllers/convict_invitations_controller.rb
@@ -1,14 +1,12 @@
 class ConvictInvitationsController < ApplicationController
   before_action :authenticate_user!
 
-  def create # rubocop:disable Metrics/AbcSize
+  def create
     @convict = Convict.find(params[:convict_id])
     authorize @convict, policy_class: ConvictInvitationPolicy
     params = { phone: @convict.phone, msj_id: @convict.id, first_name: @convict.first_name,
                last_name: @convict.last_name }
 
-    ConvictInvitationNotification.with(invitation_params: params, status: :pending,
-                                       type: :info).deliver(current_user)
     InviteConvictJob.perform_later(@convict.id, current_user)
 
     redirect_to convict_path(@convict), notice: t('.invitation_pending')
