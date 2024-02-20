94db016382c9df9bec0bde0750818942d0610fb1
Damien Le Thiec <damien@dlet.fr>
2024-02-20 10:50:14 +0100

    Change URL for update phone convict interface

--- app/services/mon_suivi_justice_public_api/convict.rb
+++ app/services/mon_suivi_justice_public_api/convict.rb
@@ -2,7 +2,7 @@ module MonSuiviJusticePublicApi
   class Convict < Base
     class << self
       def update_phone(params = {})
-        connection.patch("#{BASE_URL}/api/users/#{params[:msj_id]}/update-phone", params.to_json,
+        connection.patch("#{BASE_URL}/api/users/update-phone", params.to_json,
                          'Content-Type': 'application/json')
       end
 
