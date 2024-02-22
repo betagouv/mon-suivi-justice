0d3d512e40b5ef4ddd83878087a26039dd35d4b6
Matthieu Faugere <matthieu.faugere@gmail.com>
2024-02-22 15:12:36 +0100

    lint

--- app/services/cleanup/delete_unactive_convicts.rb
+++ app/services/cleanup/delete_unactive_convicts.rb
@@ -4,13 +4,14 @@ module Cleanup
 
     @delay = 18.months.ago
     def initialize
-      @convicts = Convict.where('NOT EXISTS (
-        SELECT 1 FROM appointments
-        INNER JOIN slots ON slots.id = appointments.slot_id
-        WHERE appointments.convict_id = convicts.id
-        AND slots.date > ?
-      )', @delay)
-                         .where('convicts.created_at < ?', @delay)
+      @convicts = Convict
+                  .where('NOT EXISTS (
+                    SELECT 1 FROM appointments
+                    INNER JOIN slots ON slots.id = appointments.slot_id
+                    WHERE appointments.convict_id = convicts.id
+                    AND slots.date > ?
+                  )', @delay)
+                  .where('convicts.created_at < ?', @delay)
     end
 
     def call
