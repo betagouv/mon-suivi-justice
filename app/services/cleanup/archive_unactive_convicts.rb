408d52d01fb895952d25750de924d70893deaacd
Matthieu Faugere <matthieu.faugere@gmail.com>
2024-02-22 15:10:27 +0100

    init archive and delete scripts

---
+++ app/services/cleanup/archive_unactive_convicts.rb
@@ -0,0 +1,21 @@
+class ArchiveUnactiveConvicts
+  attr_reader :convicts
+
+  @delay = 12.months.ago
+  def initialize
+    @convicts = Convict.where('NOT EXISTS (
+      SELECT 1 FROM appointments
+      INNER JOIN slots ON slots.id = appointments.slot_id
+      WHERE appointments.convict_id = convicts.id
+      AND slots.date > ?
+    )', @delay)
+                       .where('convicts.created_at < ?', @delay)
+  end
+
+  def call
+    @convicts.each do |convict|
+      convict.discard
+      HistoryItemFactory.perform(convict:, event: 'archive_convict', category: 'convict')
+    end
+  end
+end
---
+++ app/services/cleanup/delete_unactive_convicts.rb
@@ -0,0 +1,20 @@
+module Cleanup
+  class DeleteUnactiveConvicts
+    attr_reader :convicts
+
+    @delay = 18.months.ago
+    def initialize
+      @convicts = Convict.where('NOT EXISTS (
+        SELECT 1 FROM appointments
+        INNER JOIN slots ON slots.id = appointments.slot_id
+        WHERE appointments.convict_id = convicts.id
+        AND slots.date > ?
+      )', @delay)
+                         .where('convicts.created_at < ?', @delay)
+    end
+
+    def call
+      @convicts.delete_all
+    end
+  end
+end
