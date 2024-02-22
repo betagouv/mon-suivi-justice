b51ecdc9aae8c7b316232fda5f29040b399d85ed
Matthieu Faugere <matthieu.faugere@gmail.com>
2024-02-22 16:16:53 +0100

    test delete service

--- app/services/cleanup/delete_unactive_convicts.rb
+++ app/services/cleanup/delete_unactive_convicts.rb
@@ -2,20 +2,26 @@ module Cleanup
   class DeleteUnactiveConvicts
     attr_reader :convicts
 
-    @delay = 18.months.ago
     def initialize
+      @delay = 18.months.ago
       @convicts = Convict
-                  .where('NOT EXISTS (
-                    SELECT 1 FROM appointments
-                    INNER JOIN slots ON slots.id = appointments.slot_id
-                    WHERE appointments.convict_id = convicts.id
-                    AND slots.date > ?
-                  )', @delay)
-                  .where('convicts.created_at < ?', @delay)
+        .where('NOT EXISTS (
+        SELECT 1 FROM appointments
+        INNER JOIN slots ON slots.id = appointments.slot_id
+        WHERE appointments.convict_id = convicts.id
+        AND slots.date > ?
+        )', @delay)
+        .where('created_at < ?', @delay)
     end
 
     def call
-      @convicts.delete_all
+      Convict.transaction do
+        @convicts.find_each do |convict|
+          # Delete or handle appointments before deleting the convict
+          convict.appointments.delete_all
+          convict.delete
+        end
+      end
     end
   end
 end
