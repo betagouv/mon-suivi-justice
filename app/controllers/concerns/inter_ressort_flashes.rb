668cee8623bb4e986d487b3b7c20dd098f337d8f
Damien Le Thiec <damien@dlet.fr>
2024-03-14 17:39:37 +0100

    Escape unsafe html safe (#698)
    Fixe
    https://github.com/betagouv/mon-suivi-justice/security/code-scanning/4
    et toutes les autres occurences du problème

--- app/controllers/concerns/inter_ressort_flashes.rb
+++ app/controllers/concerns/inter_ressort_flashes.rb
@@ -1,6 +1,7 @@
 module InterRessortFlashes
   extend ActiveSupport::Concern
 
+  # rubocop:disable Metrics/AbcSize
   def set_inter_ressort_flashes
     link_to_edit = "<a href='/convicts/#{@convict.id}/edit'>en cliquant ici</a>"
     change_city = I18n.t('convicts.set_inter_ressort_flashes.change_city')
@@ -8,12 +9,14 @@ module InterRessortFlashes
     set_city = @convict.city_id ? change_city : add_city
 
     available_organizations = I18n.t('convicts.set_inter_ressort_flashes.available_organizations')
-    organizations_list = @convict.organizations.pluck(:name).join(', ')
-    how_to_add_organizations = I18n.t('convicts.set_inter_ressort_flashes.how_to_add_organizations', msg: set_city)
+    organizations_list = ERB::Util.html_escape(@convict.organizations.pluck(:name).join(', '))
+    how_to_add_organizations = I18n.t('convicts.set_inter_ressort_flashes.how_to_add_organizations',
+                                      msg: ERB::Util.html_escape(set_city))
 
     warning_message = "#{available_organizations} #{organizations_list}. <br/>
       #{how_to_add_organizations} #{link_to_edit}".html_safe
 
     flash.now[:info] = warning_message
   end
+  # rubocop:enable Metrics/AbcSize
 end
