9407fda50d8e5c4263cf0abff04948ee60a335d8
Damien Le Thiec <damien@dlet.fr>
2024-03-20 17:48:36 +0100

    Fix tests

--- app/jobs/delete_contact_in_brevo_job.rb
+++ app/jobs/delete_contact_in_brevo_job.rb
@@ -1,12 +1,11 @@
 class DeleteContactInBrevoJob < ApplicationJob
   queue_as :default
 
-  def perform(user_id, admin = nil)
+  def perform(user_id, user_email, admin = nil)
     adapter = BrevoAdapter.new
-    user = User.find(user_id)
 
     begin
-      adapter.delete_user_contact(user.email)
+      adapter.delete_user_contact(user_email)
     rescue StandardError => e
       AdminMailer.with(admin:, user_id:, error: e).brevo_sync_failure.deliver_now
     end
