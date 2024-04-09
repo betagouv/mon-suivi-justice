b12dcfe4a71081d81e742e7dd4139033e425b860
Matthieu Faugere <matthieu.faugere@gmail.com>
2024-04-09 11:53:42 +0200

    lint

--- spec/services/divestment_stalled_service_spec.rb
+++ spec/services/divestment_stalled_service_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe DivestmentStalledService do
   describe '#call' do
-    let(:convict) { instance_double("Convict", archived?: false, last_appointment_at_least_3_months_old?: false) }
-    let(:divestment) { instance_double("Divestment", convict: convict) }
-    let(:organization_divestment) { instance_double("OrganizationDivestment", divestment: divestment) }
-    let(:state_service) { instance_double("DivestmentStateService", accept: nil, ignore: nil) }
+    let(:convict) { instance_double('Convict', archived?: false, last_appointment_at_least_3_months_old?: false) }
+    let(:divestment) { instance_double('Divestment', convict:) }
+    let(:organization_divestment) { instance_double('OrganizationDivestment', divestment:) }
+    let(:state_service) { instance_double('DivestmentStateService', accept: nil, ignore: nil) }
 
     before do
       allow(OrganizationDivestment).to receive(:old_pending).and_return([organization_divestment])
@@ -14,7 +14,7 @@ RSpec.describe DivestmentStalledService do
       allow(organization_divestment).to receive(:convict).and_return(convict)
     end
 
-    context 'when convict is divestmentable' do 
+    context 'when convict is divestmentable' do
       it 'calls accept on DivestmentStateService' do
         allow(convict).to receive_messages(archived?: true)
         expect(state_service).to receive(:accept)
