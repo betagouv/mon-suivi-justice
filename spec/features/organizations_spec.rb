4c6f07ad5086e090e1c195761881cda108c8ec5a
Damien Le Thiec <damien@dlet.fr>
2024-04-15 21:42:06 +0200

    Fix tests

--- spec/features/organizations_spec.rb
+++ spec/features/organizations_spec.rb
@@ -18,7 +18,8 @@ RSpec.feature 'Organizations', type: :feature, logged_in_as: 'admin' do
     fill_in :organization_name, with: 'SPIP 75'
     expect { click_button 'Enregistrer' }.to change(Organization, :count).from(1).to(2)
                                          .and change { NotificationType.count }.by(5)
-    expect(page).to have_content('SPIP 75')
+    expect(page).to have_content('Modifier le service')
+    expect(page).to have_field('Nom', with: 'SPIP 75')
   end
 
   scenario 'An admin updates an organization' do
@@ -29,7 +30,8 @@ RSpec.feature 'Organizations', type: :feature, logged_in_as: 'admin' do
     end
     fill_in :organization_name, with: 'SPIP 75'
     click_button 'Enregistrer'
-    expect(page).to have_content('SPIP 75')
+    expect(page).to have_content('Modifier le service')
+    expect(page).to have_field('Nom', with: 'SPIP 75')
   end
 
   describe 'update' do
