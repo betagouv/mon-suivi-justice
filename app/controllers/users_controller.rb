c5b784268a1d7bb03d6725e4370607df4a44868c
Damien Le Thiec <damien@dlet.fr>
2024-05-31 11:00:09 +0200

    Update app/controllers/users_controller.rb
    Co-authored-by: Matthieu Faugere <matthieu.faugere@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -108,10 +108,11 @@ class UsersController < ApplicationController
   end
 
   def user_role_params
-    return {} unless params.dig(:user, :role).present? && params.dig(:user, :role) != 'admin'
-    return {} unless current_user.admin? || current_user.local_admin?
+  user_role = params.dig(:user, :role)
+  return {} unless user_role.present? && user_role != 'admin'
+  return {} unless current_user.admin? || current_user.local_admin?
 
-    { role: params.dig(:user, :role) }
+  { role: user_role }
   end
 
   def remove_linked_convicts(user, mutation: false)
