3051c807711aaa228e49dd1f7c9dca495d85a97b
Damien Le Thiec <damien@dlet.fr>
2024-06-17 21:02:10 +0200

    Lint ruby

--- app/services/divestment_stalled_service.rb
+++ app/services/divestment_stalled_service.rb
@@ -16,13 +16,17 @@ class DivestmentStalledService
 
     reminders_due.each do |organization|
       UserMailer.notify_local_admins_of_divestment(organization).deliver_later
-      organization.organization_divestments.reminders_due.each do |od|
-        od.update!(last_reminder_email_at: Time.zone.now)
-      end
+      update_reminders_due(organization)
     end
   end
 
   private
+
+  def update_reminders_due(organization)
+    organization.organization_divestments.reminders_due.each do |od|
+      od.update!(last_reminder_email_at: Time.zone.now)
+    end
+  end
 
   def divestmentable?(convict)
     convict.archived? || convict.last_appointment_at_least_3_months_old?
