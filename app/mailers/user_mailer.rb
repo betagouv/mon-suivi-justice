3c324af722f398213751f3ef96b768a5df8a9e8e
Damien Le Thiec <damien@dlet.fr>
2024-06-17 18:20:33 +0200

    Fix organization divestment reminder update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -20,8 +20,6 @@ class UserMailer < ApplicationMailer
     return if @admins.blank?
 
     mail(to: @admins.map(&:email), subject: 'Vous avez des demandes de dessaisissement en attente')
-    # je sais pas si c'est une bonne idee de faire ca la
-    organization_divestment.update!(last_reminder_email_at: Time.zone.now)
   end
 
   def divestment_accepted
