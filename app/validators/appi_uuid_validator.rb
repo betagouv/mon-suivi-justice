9f2f3275ed7e4a691b379832e51211684983e1fa
Damien Le Thiec <damien@dlet.fr>
2024-06-18 18:28:42 +0200

    Fix specs

--- app/validators/appi_uuid_validator.rb
+++ app/validators/appi_uuid_validator.rb
@@ -1,8 +1,8 @@
 class AppiUuidValidator < ActiveModel::Validator
   def validate(record)
     is_valid, error_message = appi_format(record.appi_uuid)
-    return if is_valid
 
+    return if is_valid
     record.errors.add(:appi_uuid, error_message)
   end
 
@@ -13,7 +13,7 @@ class AppiUuidValidator < ActiveModel::Validator
     return true unless appi_uuid.present?
 
     # Add error if appi_uuid doesn't match any valid format
-    return true if /(200|201|202)\d{9}([A-Za-z]?)$|^(199|200)\d{5}([A-Za-z]?)/.match?(appi_uuid)
+    return true if /^(200|201|202)\d{9}([A-Za-z]?)$|^(199|200)\d{5}([A-Za-z]?)$/.match?(appi_uuid)
 
     [false, I18n.t('activerecord.errors.models.convict.attributes.appi_uuid.invalid')]
   end
