8e30f250e6eec2726e2c70af07bec2ce4be389c3
Damien Le Thiec <damien@dlet.fr>
2024-06-21 13:04:04 +0200

    Improve I18n, helpers and mails (#820)

--- app/helpers/organization_divestment_helper.rb
+++ app/helpers/organization_divestment_helper.rb
@@ -4,4 +4,20 @@ module OrganizationDivestmentHelper
 
     'décision prise sans commentaire'
   end
+
+  def organization_divestment_state_badge(organization_divestment)
+    od_id = organization_divestment.id
+    content_tag(:p, organization_divestment.organization_name,
+                class: "fr-my-0-5v fr-badge fr-badge--no-icon #{badge_class(organization_divestment.state)}",
+                aria: { describedby: "tooltip-od-#{od_id}" },
+                id: "link-od-#{od_id}")
+  end
+
+  def organization_divestment_tooltip(organization_divestment)
+    content_tag(:span, comment_text(organization_divestment.comment),
+                class: 'fr-tooltip fr-placement',
+                id: "tooltip-od-#{organization_divestment.id}",
+                role: 'tooltip',
+                aria: { hidden: true })
+  end
 end
