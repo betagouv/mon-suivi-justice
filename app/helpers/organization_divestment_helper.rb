85a327629be637385e93f90b72ee4ea91e9f07b9
Damien Le Thiec <damien@dlet.fr>
2024-06-21 20:55:04 +0200

    Fix divestment tooltips

--- app/helpers/organization_divestment_helper.rb
+++ app/helpers/organization_divestment_helper.rb
@@ -1,20 +1,16 @@
 module OrganizationDivestmentHelper
-  def comment_text(comment)
-    return comment if comment.present?
-
-    'décision prise sans commentaire'
-  end
-
   def organization_divestment_state_badge(organization_divestment)
     od_id = organization_divestment.id
     content_tag(:p, organization_divestment.organization_name,
-                class: "fr-my-0-5v fr-badge fr-badge--no-icon #{badge_class(organization_divestment.state)}",
+                class: "fr-my-0-5v fr-badge fr-badge--no-icon #{badge_class(organization_divestment.state)} hyphens-auto",
                 aria: { describedby: "tooltip-od-#{od_id}" },
                 id: "link-od-#{od_id}")
   end
 
   def organization_divestment_tooltip(organization_divestment)
-    content_tag(:span, comment_text(organization_divestment.comment),
+    return unless organization_divestment.comment.present?
+
+    content_tag(:span, organization_divestment.comment,
                 class: 'fr-tooltip fr-placement',
                 id: "tooltip-od-#{organization_divestment.id}",
                 role: 'tooltip',
