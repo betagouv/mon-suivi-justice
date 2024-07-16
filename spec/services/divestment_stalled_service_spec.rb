52612b42680445a9b26f9ee025882f74a0b5cc06
Damien Le Thiec <damien@dlet.fr>
2024-07-16 15:56:13 +0200

    Fix tests

--- spec/services/divestment_stalled_service_spec.rb
+++ spec/services/divestment_stalled_service_spec.rb
@@ -5,13 +5,17 @@ RSpec.describe DivestmentStalledService do
     let(:convict) { instance_double('Convict', archived?: false, last_appointment_at_least_3_months_old?: false) }
     let(:divestment) { instance_double('Divestment', convict:) }
     let(:organization_divestment) { instance_double('OrganizationDivestment', divestment:) }
+    let(:organization_divestment2) { instance_double('OrganizationDivestment', divestment:) }
     let(:state_service) { instance_double('DivestmentStateService', accept: nil) }
 
     before do
-      allow(OrganizationDivestment).to receive(:old_pending).and_return([organization_divestment])
+      allow(OrganizationDivestment).to receive(:old_pending).and_return([organization_divestment,
+                                                                         organization_divestment2])
       allow(DivestmentStateService).to receive(:new).with(organization_divestment, nil).and_return(state_service)
+      allow(DivestmentStateService).to receive(:new).with(organization_divestment2, nil).and_return(state_service)
 
       allow(organization_divestment).to receive(:convict).and_return(convict)
+      allow(organization_divestment2).to receive(:convict).and_return(convict)
 
       allow(UserMailer).to receive(:admin_divestment_action_needed).and_call_original
     end
