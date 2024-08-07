f0d28deb03b4cffba243b32875ffbfd79dc19419
Damien Le Thiec <damien@dlet.fr>
2024-08-07 16:53:50 +0200

    Include admin in user_service_switch_policy

--- app/policies/user_service_switch_policy.rb
+++ app/policies/user_service_switch_policy.rb
@@ -2,6 +2,8 @@ class UserServiceSwitchPolicy < ApplicationPolicy
   def create?
     return false unless user.security_charter_accepted?
 
+    return true if user.admin?
+
     (user.local_admin_spip? || user.overseer? || user.psychologist?) && user.headquarter.present?
   end
 end
