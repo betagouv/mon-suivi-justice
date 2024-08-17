f311ceba201a9462b8eaa93ed239f054970a941d
Damien Le Thiec <damien@dlet.fr>
2024-08-17 09:00:27 +0200

    Remove alert development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,6 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.after_initialize do
     Bullet.enable        = true
-    Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console       = true
     Bullet.rails_logger  = true
