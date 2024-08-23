80efe52277c11fefaace48d7630b1a25bd7659b5
Matthieu Faugere <matthieu.faugere@gmail.com>
2024-08-23 15:30:46 +0200

    last one

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -19,7 +19,7 @@ class UserMailer < ApplicationMailer
     mails = [*organization.email, *admins&.map(&:email)]
     return if mails.blank?
 
-    mail(to: @admins.map(&:email), subject: 'Action requise : Dessaisissements en attente de réponse de votre service')
+    mail(to: mails, subject: 'Action requise : Dessaisissements en attente de réponse de votre service')
   end
 
   def admin_divestment_action_needed
