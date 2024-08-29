7a09d8df64d595ba234da4c73f152bcd4f146485
Damien Le Thiec <damien@dlet.fr>
2024-08-29 11:03:55 +0200

    Do not archive convicts with pending divestments (#928)

--- app/services/cleanup/archive_unactive_convicts.rb
+++ app/services/cleanup/archive_unactive_convicts.rb
@@ -4,15 +4,18 @@ module Cleanup
 
     def initialize
       delay = Convict.archive_delay
-      @convicts = Convict
-                  .where('NOT EXISTS (
+      @convicts = Convict.where('NOT EXISTS (
                     SELECT 1 FROM appointments
                     INNER JOIN slots ON slots.id = appointments.slot_id
                     WHERE appointments.convict_id = convicts.id
                     AND slots.date > ?
                   )', delay)
-                  .where('convicts.created_at < ?', delay)
-                  .where(discarded_at: nil)
+                         .where(created_at: ..delay)
+                         .where(discarded_at: nil)
+                         .where.not(id: Divestment.where(state: :pending).select(:convict_id))
+                         .joins(:convicts_organizations_mappings)
+                         .where.not(convicts_organizations_mappings: { created_at: delay.. }).distinct
+      # Pas de divestment en cours ni de changement de service dans le delay
     end
 
     def call
