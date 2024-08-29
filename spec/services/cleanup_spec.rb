2b889b9e2d557afdfc500d372af8477a996985d6
Damien Le Thiec <damien@dlet.fr>
2024-08-29 17:12:42 +0200

    Remove specs

--- spec/services/cleanup_spec.rb
+++ spec/services/cleanup_spec.rb
@@ -1,43 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Cleanup do
-  describe 'DeleteUnactiveConvicts' do
-    describe 'call' do
-      let!(:inactive_old_convict) { create(:convict, created_at: 20.months.ago) }
-      let!(:active_old_convict) { create(:convict, created_at: 19.months.ago) }
-      let!(:active_recent_convict) { create(:convict, created_at: 17.months.ago) }
-      let!(:inactive_recent_convict) { create(:convict, created_at: 17.months.ago) }
-
-      let!(:old_slot) { create(:slot, :without_validations, date: 19.months.ago) }
-      let!(:new_slot) { create(:slot, :without_validations, date: 16.months.ago) }
-      let!(:new_slot2) { create(:slot, :without_validations, date: 6.months.ago) }
-
-      let!(:old_appointment) { create(:appointment, :skip_validate, slot: old_slot, convict: inactive_old_convict) }
-      let!(:new_appointment) { create(:appointment, :skip_validate, slot: new_slot, convict: active_recent_convict) }
-      let!(:new_appointment2) { create(:appointment, :skip_validate, slot: new_slot2, convict: active_old_convict) }
-      before do
-        # Call the service once before all tests
-        Cleanup::DeleteUnactiveConvicts.new.call
-      end
-
-      it 'ensures active old convict still exists' do
-        expect(Convict.exists?(active_old_convict.id)).to be true
-      end
-
-      it 'ensures inactive old convict does not exist' do
-        expect(Convict.exists?(inactive_old_convict.id)).to be false
-        expect(Appointment.exists?(old_appointment.id)).to be false
-      end
-
-      it 'ensures active recent convict still exists' do
-        expect(Convict.exists?(active_recent_convict.id)).to be true
-      end
-
-      it 'ensures inactive recent convict still exists' do
-        expect(Convict.exists?(inactive_recent_convict.id)).to be true
-      end
-    end
-  end
   describe 'ArchiveUnactiveConvicts' do
     describe 'call' do
       let(:discarded_date) { Time.parse('2024-02-02') }
