12c4b78dce435f6940a17ab1dadf36f0a6fcf5e9
Damien Le Thiec <damien@dlet.fr>
2024-09-04 09:09:53 +0200

    Use destroy_by (#940)

--- app/jobs/archived_convicts_destroy_job.rb
+++ app/jobs/archived_convicts_destroy_job.rb
@@ -1,6 +1,5 @@
 class ArchivedConvictsDestroyJob < ApplicationJob
   def perform
-    convicts = Convict.where('discarded_at < ?', Convict::ARCHIVE_DURATION.ago)
-    convicts.find_each(&:destroy!)
+    Convict.destroy_by('discarded_at < ?', Convict::ARCHIVE_DURATION.ago)
   end
 end
