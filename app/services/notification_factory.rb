de31df29b212adb15fa840df9020cdd36943ec9a
Matthieu Faugere <matthieu.faugere@gmail.com>
2024-10-09 15:23:28 +0200

    Handle delivery time before delay

--- app/services/notification_factory.rb
+++ app/services/notification_factory.rb
@@ -2,7 +2,7 @@ module NotificationFactory
   class << self
     def perform(appointment, roles)
       @appointment = appointment
-      roles = Array(roles)
+      roles = cleanup Array(roles)
 
       @appointment.transaction do
         roles.each do |role|
@@ -28,6 +28,8 @@ module NotificationFactory
     end
 
     def delivery_time(notif_type)
+      return Time.zone.now if before_hours_delay?(notif_type)
+
       app_date = @appointment.slot.date
       app_time = @appointment.localized_starting_time
 
@@ -39,6 +41,28 @@ module NotificationFactory
 
     def hour_delay(notif_type)
       { 'one_day' => 24, 'two_days' => 48 }.fetch(notif_type.reminder_period)
+    end
+
+    def before_hours_delay?(notif_type)
+      return false unless notif_type.reminder?
+
+      delay = hour_delay(notif_type)
+      datetime = @appointment.slot.datetime
+
+      datetime < delay.hours.from_now
+    end
+
+    def cleanup(roles)
+      return roles unless contains_all(roles, %w[reminder summon])
+
+      reminder_notif_type = @appointment.reminder_notification_type
+      return roles - ['reminder'] if before_hours_delay? reminder_notif_type
+
+      roles
+    end
+
+    def contains_all(array, values)
+      (values - array).empty?
     end
   end
 end
