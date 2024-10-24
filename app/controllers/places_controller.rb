de4715122d101705de6ed09287aa817be958a8cb
Matthieu Faugere <matthieu.faugere@gmail.com>
2024-10-24 11:48:11 +0200

    Prevent create and update of places without appointment type

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,11 +14,13 @@ class PlacesController < ApplicationController
   def create
     @place = Place.new(place_params)
     authorize @place
-
+    p @place
     if @place.save
       create_agenda(@place)
       redirect_to places_path
     else
+      p @place
+      p @place.errors
       render :new, status: :unprocessable_entity
     end
   end
