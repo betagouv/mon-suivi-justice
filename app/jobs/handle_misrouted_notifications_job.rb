fc1f4ce94fca5820b44386bed34b0365625c8fb3
Damien Le Thiec <damien@dlet.fr>
2024-11-11 09:06:56 +0100

    Do not send email to sentry when no phone to fix

--- app/jobs/handle_misrouted_notifications_job.rb
+++ app/jobs/handle_misrouted_notifications_job.rb
@@ -2,12 +2,14 @@ class HandleMisroutedNotificationsJob < ApplicationJob
   queue_as :default
 
   def perform
+    return if phones_to_reroute.empty?
+
     AdminMailer.with(phones: phones_to_reroute).warn_link_mobility_for_misrouted_notifications.deliver_later
   end
 
   private
 
   def phones_to_reroute
-    Notification.to_reroute_link_mobility.pluck(:target_phone).uniq
+    @phones_to_reroute ||= Notification.to_reroute_link_mobility.pluck(:target_phone).uniq
   end
 end
