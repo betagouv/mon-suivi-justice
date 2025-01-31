e1d5745a38070189b71bf4dfb78b25fd8b17338f
Damien Le Thiec <damien@dlet.fr>
2025-01-31 15:44:04 +0100

    Schedule SMS delivery job every minute to avoid LinkMobility Rate Limiting (#1118)

---
+++ spec/jobs/sms_schedule_job_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe SmsScheduleJob, type: :job do
+  describe '#perform' do
+    let(:current_time) { Time.current }
+
+    let!(:notification_ready) { create(:notification, delivery_time: current_time, state: 'created') }
+
+    let!(:notification_past) { create(:notification, delivery_time: 2.minutes.ago, state: 'created') }
+
+    let!(:notification_future) { create(:notification, delivery_time: 2.minutes.from_now, state: 'created') }
+
+    let!(:notification_sent) { create(:notification, delivery_time: current_time, state: 'sent', external_id: '123') }
+
+    it 'appelle program_now uniquement sur les notifications prêtes à être envoyées' do
+      expect(Notification).to receive(:ready_to_send).and_return([notification_ready])
+      expect(notification_ready).to receive(:program_now)
+      expect(notification_past).not_to receive(:program_now)
+      expect(notification_future).not_to receive(:program_now)
+      expect(notification_sent).not_to receive(:program_now)
+      described_class.perform_now
+    end
+  end
+end
